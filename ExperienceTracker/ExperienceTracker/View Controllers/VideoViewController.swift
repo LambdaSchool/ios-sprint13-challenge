d07c29240dcde0e9a9b2aae190b58ff9f1516475
Jonathan T. Miles <jonathan.t.miles@gmail.com>
2018-10-19 09:46:59 -0700

    video viewcontroller set up

--- ExperienceTracker/ExperienceTracker/View Controllers/VideoViewController.swift
+++ ExperienceTracker/ExperienceTracker/View Controllers/VideoViewController.swift
@@ -7,16 +7,50 @@
 //
 
 import UIKit
+import AVFoundation
 
-class VideoViewController: UIViewController {
+class VideoViewController: UIViewController, AVCaptureFileOutputRecordingDelegate {
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        captureSession.startRunning()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupCapture()
     }
     
-
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        captureSession.stopRunning()
+    }
+    
+    @IBAction func toggleRecording(_ sender: Any) {
+        if recordOutput.isRecording {
+            recordOutput.stopRecording()
+        } else {
+            recordOutput.startRecording(to: newRecordingURL(), recordingDelegate: self)
+        }
+    }
+    
+    // MARK: - AVCaptureFileOutputRecordingDelegate
+    
+    func fileOutput(_ output: AVCaptureFileOutput, didStartRecordingTo fileURL: URL, from connections: [AVCaptureConnection]) {
+        DispatchQueue.main.async {
+            self.updateViews()
+        }
+    }
+    
+    func fileOutput(_ output: AVCaptureFileOutput, didFinishRecordingTo outputFileURL: URL, from connections: [AVCaptureConnection], error: Error?) {
+        DispatchQueue.main.async {
+            self.updateViews()
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
@@ -26,5 +60,59 @@ class VideoViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    // MARK: - Private
+    
+    private func updateViews() {
+        guard isViewLoaded else { return }
+        
+        let recordButtonImageName = recordOutput.isRecording ? "Stop" : "Record"
+        recordingButton.setImage(UIImage(named: recordButtonImageName), for: .normal)
+    }
+    
+    private func setupCapture() {
+        let captureSession = AVCaptureSession()
+        let device = bestCamera()
+        guard let videoDeviceInput = try? AVCaptureDeviceInput(device: device), captureSession.canAddInput(videoDeviceInput) else {
+            fatalError()
+        }
+        captureSession.addInput(videoDeviceInput)
+        
+        let fileOutput = AVCaptureMovieFileOutput()
+        guard captureSession.canAddOutput(fileOutput) else { fatalError() }
+        captureSession.addOutput(fileOutput)
+        recordOutput = fileOutput
+        
+        captureSession.sessionPreset = .hd1920x1080
+        captureSession.commitConfiguration()
+        
+        self.captureSession = captureSession
+        cameraView.videoPreviewLayer.session = captureSession
+    }
+    
+    private func bestCamera() -> AVCaptureDevice {
+        if let device = AVCaptureDevice.default(.builtInDualCamera, for: .video, position: .back) {
+            return device
+        } else if let device = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) {
+            return device
+        } else {
+            fatalError("Missing expected back camera device")
+        }
+        
+    }
+    
+    private func newRecordingURL() -> URL {
+        let fm = FileManager.default
+        let documentsDir = try! fm.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+        
+        return documentsDir.appendingPathComponent(UUID().uuidString).appendingPathExtension("mov")
+    }
+    
+    // AMRK: - Properties
+    
+    private var captureSession: AVCaptureSession!
+    private var recordOutput: AVCaptureMovieFileOutput!
 
+    @IBOutlet weak var recordingButton: UIButton!
+    @IBOutlet weak var cameraView: CameraPreviewView!
 }
