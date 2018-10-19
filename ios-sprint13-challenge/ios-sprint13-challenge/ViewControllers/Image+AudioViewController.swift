4502614d01f5d42c726bfde63aa3f4d43304c424
stdemicheli <stdemicheli@gmail.com>
2018-10-19 18:30:14 +0200

    Users can now choose a filtered image from the photo library.

--- ios-sprint13-challenge/ios-sprint13-challenge/ViewControllers/Image+AudioViewController.swift
+++ ios-sprint13-challenge/ios-sprint13-challenge/ViewControllers/Image+AudioViewController.swift
@@ -7,11 +7,13 @@
 //
 
 import UIKit
+import Photos
 
 class Image_AudioViewController: UIViewController {
 
     // MARK: - Navigation
     var experienceController: ExperienceController!
+    private let context = CIContext(options: nil)
     @IBOutlet weak var recordAudioButton: UIButton!
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var titleTextField: UITextField!
@@ -28,8 +30,39 @@ class Image_AudioViewController: UIViewController {
     }
     
     @IBAction func addImage(_ sender: Any) {
-        
+        presentImagePicker()
         addImageButton.isHidden = true
+    }
+    
+    // MARK: - Private
+    
+    private func presentImagePicker() {
+        guard UIImagePickerController.isSourceTypeAvailable(.photoLibrary) else {
+            NSLog("No Photo Library available")
+            return
+        }
+        
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    private func image(byFiltering image: UIImage) -> UIImage? {
+        guard let cgImage = image.cgImage else { return nil }
+        
+        let ciImage = CIImage(cgImage: cgImage)
+        
+        let filter = CIFilter(name: "CIPhotoEffectMono")
+        filter?.setValue(ciImage, forKey: kCIInputImageKey)
+        
+        guard let outputCIImage = filter?.outputImage,
+            let outputCGImage = context.createCGImage(outputCIImage, from: outputCIImage.extent) else {
+                return nil
+        }
+        
+        return UIImage(cgImage: outputCGImage)
     }
     
     // MARK: - Navigation
@@ -41,4 +74,24 @@ class Image_AudioViewController: UIViewController {
         }
     }
 
+}
+
+extension Image_AudioViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        picker.dismiss(animated: true, completion: nil)
+        
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else { return }
+        
+        if let filteredImage = self.image(byFiltering: image) {
+            imageView?.image = filteredImage
+        }
+        
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
 }
