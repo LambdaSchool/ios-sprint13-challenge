7672555acbc2de77516a51e30673ab37ab34c178
Jonathan T. Miles <jonathan.t.miles@gmail.com>
2018-10-19 11:04:43 -0700

    adds folders for model and model controller files

---
+++ ExperienceTracker/ExperienceTracker/Model Controller/ExperienceController.swift
@@ -0,0 +1,9 @@
+//
+//  ExperienceController.swift
+//  ExperienceTracker
+//
+//  Created by Jonathan T. Miles on 10/19/18.
+//  Copyright © 2018 Jonathan T. Miles. All rights reserved.
+//
+
+import Foundation
