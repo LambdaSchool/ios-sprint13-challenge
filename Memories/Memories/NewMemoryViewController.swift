521e1dbb604fa175e1596a0826d0d35bca212a52
Samantha Gatt <smfgatt@gmail.com>
2018-10-19 13:01:08 -0400

    Implements adding an audio recording and playing it back

--- Memories/Memories/NewMemoryViewController.swift
+++ Memories/Memories/NewMemoryViewController.swift
@@ -7,13 +7,17 @@
 //
 
 import UIKit
+import AVFoundation
 
-class NewMemoryViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class NewMemoryViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, AVAudioPlayerDelegate {
     
     // MARK: - Properties
     
     private let filter = CIFilter(name: "CISepiaTone")!
     private let context = CIContext(options: nil)
+    
+    var player: AVAudioPlayer?
+    var recorder: AVAudioRecorder?
     
     
     // MARK: - Outlets
@@ -23,6 +27,27 @@ class NewMemoryViewController: UIViewController, UIImagePickerControllerDelegate
     @IBOutlet private weak var addPhotoButton: UIButton!
     @IBOutlet private weak var recordStopButton: UIButton!
     @IBOutlet private weak var playbackButton: UIButton!
+    
+    
+    // MARK: - Private functions
+    
+    private func updateViews() {
+        guard isViewLoaded else { return }
+        
+        let isRecording = recorder?.isRecording ?? false
+        let recordButtonTitle = isRecording ? "Stop" : "Record"
+        recordStopButton.setTitle(recordButtonTitle, for: .normal)
+        
+        let isPlaying = player?.isPlaying ?? false
+        let playButtonTitle = isPlaying ? "Pause" : "Play Back"
+        playbackButton.setTitle(playButtonTitle, for: .normal)
+    }
+    
+    private func newRecordingURL() -> URL {
+        let fm = FileManager.default
+        let documentsDir = try! fm.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+        return documentsDir.appendingPathComponent(UUID().uuidString).appendingPathExtension("caf")
+    }
     
     
     // MARK: - Actions
@@ -37,6 +62,35 @@ class NewMemoryViewController: UIViewController, UIImagePickerControllerDelegate
         imagePicker.delegate = self
         imagePicker.sourceType = .photoLibrary
         present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func toggleRecord(_ sender: Any) {
+        let isRecording = recorder?.isRecording ?? false
+        if isRecording {
+            recorder?.stop()
+            if let url = recorder?.url {
+                player = try! AVAudioPlayer(contentsOf: url)
+                player?.delegate = self
+            }
+        } else {
+            let format = AVAudioFormat(standardFormatWithSampleRate: 44100.0, channels: 1)!
+            recorder = try! AVAudioRecorder(url: newRecordingURL(), format: format)
+            recorder?.record()
+        }
+        updateViews()
+    }
+    
+    @IBAction func playBack(_ sender: Any) {
+        let isPlaying = player?.isPlaying ?? false
+        if isPlaying {
+            player?.pause()
+        } else {
+            if player == nil {
+                return
+            }
+            player?.play()
+        }
+        updateViews()
     }
     
     
@@ -59,5 +113,12 @@ class NewMemoryViewController: UIViewController, UIImagePickerControllerDelegate
     
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         picker.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    // MARK: - AVAudioPlayerDelegate
+    
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        updateViews()
     }
 }
