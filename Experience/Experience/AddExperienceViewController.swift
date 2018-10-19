7cc2c1641d1b4a5b6a473aa79dc80c77a7a5d2cf
Carolyn Lea <carolynrlea@gmail.com>
2018-10-19 11:10:32 -0600

    added basic audio functionality

--- Experience/Experience/AddExperienceViewController.swift
+++ Experience/Experience/AddExperienceViewController.swift
@@ -9,11 +9,13 @@
 import UIKit
 import AVFoundation
 
-class AddExperienceViewController: UIViewController
+class AddExperienceViewController: UIViewController, AVAudioPlayerDelegate
 {
     @IBOutlet var addImageButton: UIButton!
-    
     @IBOutlet var audioRecordingButton: UIButton!
+    
+    var player: AVAudioPlayer?
+    var recorder: AVAudioRecorder?
     
     override func viewDidLoad()
     {
@@ -28,7 +30,44 @@ class AddExperienceViewController: UIViewController
     
     @IBAction func addAudio(_ sender: Any)
     {
+        let isRecording = recorder?.isRecording ?? false
+        if isRecording
+        {
+            recorder?.stop()
+            if let url = recorder?.url {
+                player = try! AVAudioPlayer(contentsOf: url)
+                player?.delegate = self
+            }
+        }
+        else
+        {
+            let format = AVAudioFormat(standardFormatWithSampleRate: 44100.0, channels: 1)!
+            recorder = try! AVAudioRecorder(url: newRecordingURL(), format: format)
+            recorder?.record()
+        }
+        updateViews()
+    }
+    
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool)
+    {
+        updateViews()
+    }
+    
+    private func updateViews()
+    {
+        guard isViewLoaded else {return}
         
+        let isRecording = recorder?.isRecording ?? false
+        let recordButtonTitle = isRecording ? "Stop Recording" : "Add Audio Recording"
+        audioRecordingButton.setTitle(recordButtonTitle, for: .normal)
+    }
+    
+    private func newRecordingURL() -> URL
+    {
+        let fm = FileManager.default
+        let documentsDir = try! fm.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+        
+        return documentsDir.appendingPathComponent(UUID().uuidString).appendingPathExtension("caf")
     }
     
     // MARK: - Navigation
