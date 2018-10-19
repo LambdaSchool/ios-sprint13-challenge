2c83e3ca9dbe6e2b9d1af7d2e7a24296ef13464c
Samantha Gatt <smfgatt@gmail.com>
2018-10-19 12:15:33 -0400

    Starts map view controller

--- Memories/Memories/ViewController.swift
+++ Memories/Memories/ViewController.swift
@@ -7,14 +7,50 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class MapViewController: UIViewController, MKMapViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.requestWhenInUseAuthorization()
+        if CLLocationManager.authorizationStatus() != .authorizedWhenInUse {
+            NSLog("You need to alllow access")
+        }
+        
+        userTrackingButton = MKUserTrackingButton(mapView: mapView)
+        userTrackingButton.translatesAutoresizingMaskIntoConstraints = false
+        mapView.addSubview(userTrackingButton)
+        userTrackingButton.leftAnchor.constraint(equalTo: mapView.leftAnchor, constant: 20).isActive = true
+        userTrackingButton.bottomAnchor.constraint(equalTo: mapView.bottomAnchor, constant: -20).isActive = true
+        
+        mapView.register(MKMarkerAnnotationView.self, forAnnotationViewWithReuseIdentifier: "QuakeAnnotationView")
     }
-
-
+    
+    // MARK: - Properties
+    
+    private let locationManager = CLLocationManager()
+    private var userTrackingButton: MKUserTrackingButton!
+    
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
+    // MARK: - MKMapViewDelegate
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "QuakeAnnotationView") as! MKMarkerAnnotationView
+        annotationView.markerTintColor = .gray
+        annotationView.glyphTintColor = .black
+        annotationView.glyphImage = UIImage(named: "QuakeIcon")!
+        
+        annotationView.canShowCallout = true
+        
+        return annotationView
+    }
 }
 
