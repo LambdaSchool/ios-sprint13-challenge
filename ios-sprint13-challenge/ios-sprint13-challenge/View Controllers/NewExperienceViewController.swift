1cb1bcb05c8d7463cfc96fbf9c2e52210056a65a
Conner Alegre <cga0@pm.me>
2018-10-19 10:55:54 -0600

    Add audio recording to new experience

--- ios-sprint13-challenge/ios-sprint13-challenge/View Controllers/NewExperienceViewController.swift
+++ ios-sprint13-challenge/ios-sprint13-challenge/View Controllers/NewExperienceViewController.swift
@@ -14,65 +14,27 @@ class NewExperienceViewController: UIViewController {
     @IBOutlet var titleTextField: UITextField!
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var choosePhotoButton: UIButton!
+    @IBOutlet var recordAudioButton: UIButton!
+    
+    private var recorder: AVAudioRecorder?
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-    private func presentInformationalAlertController(title: String?, message: String?, dismissActionCompletion: ((UIAlertAction) -> Void)? = nil, completion: (() -> Void)? = nil) {
-        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        let dismissAction = UIAlertAction(title: "Dismiss", style: .cancel, handler: dismissActionCompletion)
-
-        alertController.addAction(dismissAction)
-
-        present(alertController, animated: true, completion: completion)
-    }
-
-    private func presentImagePickerController() {
-
-        guard UIImagePickerController.isSourceTypeAvailable(.photoLibrary) else {
-            presentInformationalAlertController(title: "Error", message: "The photo library is unavailable")
-            return
+    
+    @IBAction func recordAudioTapped(_ sender: Any) {
+        let isRecording = recorder?.isRecording ?? false
+        
+        if isRecording {
+            endRecording()
+        } else {
+            beginRecording()
         }
-
-        let imagePicker = UIImagePickerController()
-        imagePicker.delegate = self
-        imagePicker.sourceType = .photoLibrary
-        present(imagePicker, animated: true, completion: nil)
     }
-
-    @IBAction func choosePhoto(_ sender: Any) {
-        let authorizationStatus = PHPhotoLibrary.authorizationStatus()
-
-        switch authorizationStatus {
-        case .authorized:
-            presentImagePickerController()
-        case .notDetermined:
-
-            PHPhotoLibrary.requestAuthorization { (status) in
-
-                guard status == .authorized else {
-                    NSLog("User did not authorize access to the photo library")
-                    self.presentInformationalAlertController(title: "Error", message: "In order to access the photo library, you must allow this application access to it.")
-                    return
-                }
-
-                self.presentImagePickerController()
-            }
-
-        case .denied:
-            self.presentInformationalAlertController(title: "Error", message: "In order to access the photo library, you must allow this application access to it.")
-        case .restricted:
-            self.presentInformationalAlertController(title: "Error", message: "Unable to access the photo library. Your device's restrictions do not allow access.")
-
-        }
-        presentImagePickerController()
-    }
-
 }
 
+// MARK: - Adding Photo
 extension NewExperienceViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
         choosePhotoButton.setTitle("", for: [])
         picker.dismiss(animated: true, completion: nil)
@@ -83,5 +45,93 @@ extension NewExperienceViewController: UIImagePickerControllerDelegate, UINaviga
 
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         picker.dismiss(animated: true, completion: nil)
+    }
+    
+    private func presentInformationalAlertController(title: String?, message: String?, dismissActionCompletion: ((UIAlertAction) -> Void)? = nil, completion: (() -> Void)? = nil) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let dismissAction = UIAlertAction(title: "Dismiss", style: .cancel, handler: dismissActionCompletion)
+        
+        alertController.addAction(dismissAction)
+        
+        present(alertController, animated: true, completion: completion)
+    }
+    
+    private func presentImagePickerController() {
+        
+        guard UIImagePickerController.isSourceTypeAvailable(.photoLibrary) else {
+            presentInformationalAlertController(title: "Error", message: "The photo library is unavailable")
+            return
+        }
+        
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func choosePhoto(_ sender: Any) {
+        let authorizationStatus = PHPhotoLibrary.authorizationStatus()
+        
+        switch authorizationStatus {
+        case .authorized:
+            presentImagePickerController()
+        case .notDetermined:
+            
+            PHPhotoLibrary.requestAuthorization { (status) in
+                
+                guard status == .authorized else {
+                    NSLog("User did not authorize access to the photo library")
+                    self.presentInformationalAlertController(title: "Error", message: "In order to access the photo library, you must allow this application access to it.")
+                    return
+                }
+                
+                self.presentImagePickerController()
+            }
+            
+        case .denied:
+            self.presentInformationalAlertController(title: "Error", message: "In order to access the photo library, you must allow this application access to it.")
+        case .restricted:
+            self.presentInformationalAlertController(title: "Error", message: "Unable to access the photo library. Your device's restrictions do not allow access.")
+            
+        }
+        presentImagePickerController()
+    }
+}
+
+// MARK: - Adding Audio Recording
+extension NewExperienceViewController {
+    private func newRecordingURL() -> URL {
+        let fm = FileManager.default
+        let documentsDir = try! fm.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+        return documentsDir.appendingPathComponent(UUID().uuidString).appendingPathExtension("caf")
+    }
+    
+    private func beginRecording() {
+        let isRecording = recorder?.isRecording ?? false
+        
+        if isRecording {
+            recorder?.stop()
+        } else {
+            do {
+                let format = AVAudioFormat(standardFormatWithSampleRate: 44100.0, channels: 1)!
+                recorder = try AVAudioRecorder(url: newRecordingURL(), format: format)
+                recorder?.record()
+                recordAudioButton.setTitle("Recording...", for: .normal)
+            } catch {
+                NSLog("Error beginning the recording...")
+            }
+        }
+    }
+    
+    private func endRecording() {
+        let isRecording = recorder?.isRecording ?? false
+        
+        if isRecording {
+            recorder?.stop()
+            recordAudioButton.setTitle("Record Audio", for: .normal)
+            if let url = recorder?.url {
+                // Add recording URL to Experience
+            }
+        }
     }
 }
