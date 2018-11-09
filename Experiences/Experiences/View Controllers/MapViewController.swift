7eb48eddbf346793a57aa7627e18c1ff81962b3f
Scott Bennett <stt.btt@gmail.com>
2018-11-09 14:01:43 -0500

    Submitted

--- Experiences/Experiences/View Controllers/MapViewController.swift
+++ Experiences/Experiences/View Controllers/MapViewController.swift
@@ -12,17 +12,59 @@ import CoreLocation
 
 class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
+    // MARK: - Properties
+    
     @IBOutlet weak var mapView: MKMapView!
     
     var locationManager = CLLocationManager()
     private var userTrackingButton = MKUserTrackingButton()
     
+    let newPin = MKPointAnnotation()
+    
+    let newExperienceViewController = NewExperienceViewController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         
+        newPin.title? = newExperienceViewController.titleTextField.text ?? ""
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
         
-        mapView.showsUserLocation = true
+        navigationController?.isNavigationBarHidden = false
+    }
+    
+    //MARK:- CLLocationManager Delegates
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        self.locationManager.stopUpdatingLocation()
+        
+        let location = locations.last! as CLLocation
+        
+        let region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude), span: MKCoordinateSpan(latitudeDelta: 1.0, longitudeDelta: 1.0))
+        self.mapView.setRegion(region, animated: true)
+        
+        newPin.coordinate = location.coordinate
+        mapView.addAnnotation(newPin)
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Unable to access your current location")
+    }
+    
+
+    @IBAction func addExperienceButton(_ sender: Any) {
+        
+        mapView.showsUserLocation = false
         if CLLocationManager.locationServicesEnabled() == true {
             
             if CLLocationManager.authorizationStatus() == .restricted || CLLocationManager.authorizationStatus() == .denied ||  CLLocationManager.authorizationStatus() == .notDetermined {
@@ -35,33 +77,6 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
             print("PLease turn on location services or GPS")
         }
         
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        navigationController?.isNavigationBarHidden = true
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        
-        navigationController?.isNavigationBarHidden = false
-    }
-    
-    //MARK:- CLLocationManager Delegates
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        self.locationManager.stopUpdatingLocation()
-        let region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude), span: MKCoordinateSpan(latitudeDelta: 1.0, longitudeDelta: 1.0))
-        self.mapView.setRegion(region, animated: true)
-    }
-    
-    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        print("Unable to access your current location")
-    }
-    
-
-    @IBAction func addExperienceButton(_ sender: Any) {
         performSegue(withIdentifier: "NewExperience", sender: self)
     }
     
