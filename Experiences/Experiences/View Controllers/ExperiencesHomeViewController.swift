d60b3f52f03f3d994916f9f9474a98290846f5df
Moin Uddin <U_Moin75@yahoo.com>
2018-11-09 12:14:43 -0500

    Setup ExperiencesHomeController

--- Experiences/Experiences/View Controllers/ExperiencesHomeViewController.swift
+++ Experiences/Experiences/View Controllers/ExperiencesHomeViewController.swift
@@ -7,14 +7,101 @@
 //
 
 import UIKit
-
-class ExperiencesHomeViewController: UIViewController {
+import AVFoundation
+import Photos
+class ExperiencesHomeViewController: UIViewController, AVAudioRecorderDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        requestMicrophonePermission()
+    }
+    
+    func updateViews() {
+        
+    }
+    
+    func requestMicrophonePermission() {
+        let session = AVAudioSession.sharedInstance()
+        
+        session.requestRecordPermission { (granted) in
+            guard granted else {
+                NSLog("Please give the application permission to record in Settings.")
+                return
+            }
+            
+            do {
+                try session.setCategory(.playAndRecord, mode: .default, options: [])
+                try session.setActive(true, options: [])
+            } catch {
+                NSLog("Error setting up AVAudiosSession: \(error)")
+            }
+        }
+    }
+    
+    private func updateButtons() {
+        isRecording ? recordButton.setTitle("Stop", for: .normal) : recordButton.setTitle("Record", for: .normal)
+    }
+    
+    private func newRecordingURL() -> URL {
+        let documentsDirectory = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+        return documentsDirectory.appendingPathComponent(UUID().uuidString).appendingPathExtension("caf")
+    }
+    
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        recordingURL = recorder.url
+        self.recorder = nil
+        updateButtons()
+    }
+    
+    private var recordingURL: URL?
+    private var recorder: AVAudioRecorder?
+    private var isRecording: Bool {
+        return recorder?.isRecording ?? false
+    }
+    
+    private func presentImagePickerController() {
+        
+        guard UIImagePickerController.isSourceTypeAvailable(.photoLibrary) else {
+            presentInformationalAlertController(title: "Error", message: "The photo library is unavailable")
+            return
+        }
+        
+        let imagePicker = UIImagePickerController()
+        
+        imagePicker.delegate = self
+        
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil)
     }
     
     @IBAction func addPoster(_ sender: Any) {
+        
+        let authorizationStatus = PHPhotoLibrary.authorizationStatus()
+        
+        switch authorizationStatus {
+        case .authorized:
+            presentImagePickerController()
+        case .notDetermined:
+            
+            PHPhotoLibrary.requestAuthorization { (status) in
+                
+                guard status == .authorized else {
+                    NSLog("User did not authorize access to the photo library")
+                    self.presentInformationalAlertController(title: "Error", message: "In order to access the photo library, you must allow this application access to it.")
+                    return
+                }
+                
+                self.presentImagePickerController()
+            }
+            
+        case .denied:
+            self.presentInformationalAlertController(title: "Error", message: "In order to access the photo library, you must allow this application access to it.")
+        case .restricted:
+            self.presentInformationalAlertController(title: "Error", message: "Unable to access the photo library. Your device's restrictions do not allow access.")
+            
+        }
+        presentImagePickerController()
     }
     
     
@@ -23,8 +110,55 @@ class ExperiencesHomeViewController: UIViewController {
     
     
     @IBAction func record(_ sender: Any) {
+        defer { updateButtons() }
+        guard !isRecording else {
+            recorder?.stop()
+            return
+        }
+        
+        do {
+            let format = AVAudioFormat(standardFormatWithSampleRate: 44100.0, channels: 2)!
+            recorder = try AVAudioRecorder(url: newRecordingURL(), format: format)
+            recorder?.delegate = self
+            recorder?.record()
+        } catch {
+            NSLog("Unable to start recording: \(error)")
+        }
     }
     
+    
+    var originalImage: UIImage? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var recordButton: UIButton!
     
+}
+
+extension ExperiencesHomeViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        addPosterButton.setTitle("", for: [])
+        
+        picker.dismiss(animated: true, completion: nil)
+        
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else { return }
+        
+        originalImage = image
+        
+    }
+    private func presentInformationalAlertController(title: String?, message: String?, dismissActionCompletion: ((UIAlertAction) -> Void)? = nil, completion: (() -> Void)? = nil) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let dismissAction = UIAlertAction(title: "Dismiss", style: .cancel, handler: dismissActionCompletion)
+        alertController.addAction(dismissAction)
+        present(alertController, animated: true, completion: completion)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
 }
