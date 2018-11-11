7e7bbfc323d108b97c8b410d35a024d5bb211296
Farhan Farooqui <farhan@farooqui.net>
2018-11-11 15:53:01 -0500

    Allow user to record and play voice caption

--- Experiences/Experiences/View Controllers/AddExperienceViewController.swift
+++ Experiences/Experiences/View Controllers/AddExperienceViewController.swift
@@ -9,18 +9,127 @@
 import UIKit
 import AVFoundation
 
-class AddExperienceViewController: UIViewController {
+class AddExperienceViewController: UIViewController, AVAudioRecorderDelegate, AVAudioPlayerDelegate {
     
     // MARK: Properties
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleField: UITextField!
+    
+    var recordingSession : AVAudioSession!
+    var audioRecorder    :AVAudioRecorder!
+    var settings         = [String : Int]()
+    
+    var audioPlayer : AVAudioPlayer!
    
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var playButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        recordingSession = AVAudioSession.sharedInstance()
+        do {
+            try? recordingSession.setMode(.spokenAudio)
+            try? recordingSession.setActive(true)
+            recordingSession.requestRecordPermission { (granted) in
+                if granted {
+                    NSLog("Allowed to record")
+                }else {
+                    NSLog("Not allowed to record")
+                }
+            }
+        
+        // Audio Settings
+        
+        settings = [
+            AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+            AVSampleRateKey: 12000,
+            AVNumberOfChannelsKey: 1,
+            AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+        ]    }
     }
     
      // MARK: Local Methods
     
+    func directoryURL() -> URL? {
+        let fileManager = FileManager.default
+        let urls = fileManager.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentDirectory = urls[0] as URL
+        let soundURL = documentDirectory.appendingPathComponent("sound.m4a")
+        print(soundURL)
+        return soundURL
+    }
+    
+    func startRecording() {
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            audioRecorder = try AVAudioRecorder(url: self.directoryURL()! as URL,
+                                                settings: settings)
+            audioRecorder.delegate = self
+            audioRecorder.prepareToRecord()
+        } catch {
+            finishRecording(success: false)
+        }
+        do {
+            try audioSession.setActive(true)
+            audioRecorder.record()
+        } catch {
+        }
+    }
+    
+    func finishRecording(success: Bool) {
+        audioRecorder.stop()
+        if success {
+            print(success)
+        } else {
+            audioRecorder = nil
+            print("Somthing Went Wrong with Recording.")
+        }
+    }
+    
+    @IBAction func clickedAudioRecord(_ sender: AnyObject) {
+        if audioRecorder == nil {
+            self.recordButton.setTitle("Stop Recording", for: UIControl.State.normal)
+            self.startRecording()
+        } else {
+            self.recordButton.setTitle("Record Voice", for: UIControl.State.normal)
+            self.finishRecording(success: true)
+        }
+    }
+    
+    @IBAction func doPlay(_ sender: AnyObject) {
+        if !audioRecorder.isRecording {
+            self.audioPlayer = try! AVAudioPlayer(contentsOf: audioRecorder.url)
+            self.audioPlayer.prepareToPlay()
+            self.audioPlayer.delegate = self
+            self.audioPlayer.play()
+        }
+    }
+    
+    // Audio Record/Play Delegates
+    
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        if !flag {
+            finishRecording(success: false)
+        }
+    }
+    
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        print(flag)
+    }
+    func audioPlayerDecodeErrorDidOccur(_ player: AVAudioPlayer, error: Error?){
+        print(error.debugDescription)
+    }
+    internal func audioPlayerBeginInterruption(_ player: AVAudioPlayer){
+        print(player.debugDescription)
+    }
+    
+  
+    
+    
+    // MARK: - Navigation
     
     @IBAction func showCamera(_ sender: Any) {
         
@@ -42,14 +151,12 @@ class AddExperienceViewController: UIViewController {
     
         
     }
-    /*
-    // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-    */
+    
 
 }
