a4f12a347ff33bad3a64a0ff2cbe311b32c1169b
OmegaWulf <nikcxz@gmail.com>
2019-01-18 11:35:51 -0500

    MapVC will add annotations + remove old ones

--- Experience/Experience/View Controllers/MapVC.swift
+++ Experience/Experience/View Controllers/MapVC.swift
@@ -13,22 +13,28 @@ class MapVC: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
     
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    let experienceCont = ExperienceController()
+    var experiences: [Experience] = []
+  
 
-        // Do any additional setup after loading the view.
+    // Done in viewWillAppear so we can popToRoot and load new annotations
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Remove the old annotations so we're not creating duplicates of the old ones
+        mapView.removeAnnotations(mapView.annotations)
+        
+        // Add all annotations which will include the new ones
+        mapView.addAnnotations(experienceCont.experiences)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+ 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "toNewExperience" {
+            let dest = segue.destination as! NewExperienceVC
+            dest.experienceCont = experienceCont
+        }
     }
-    */
+    
 
 }
