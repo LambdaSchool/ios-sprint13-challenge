5081d2a78195dd0ab32f3fb6faa371c9035f40b4
yzhukovs <yzhukovs@bumnetworks.com>
2019-01-18 14:26:08 -0500

    added rest of the project

--- MediaProgramming/MediaProgramming/ExperiencesViewController.swift
+++ MediaProgramming/MediaProgramming/ExperiencesViewController.swift
@@ -10,46 +10,145 @@ import MapKit
 import UIKit
 
 
-class ExperiencesViewController: UIViewController {
+class ExperiencesViewController: UIViewController,UITextFieldDelegate {
 
+    @IBOutlet var titleText: UITextField!
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var addImageButton: UIButton!
+    @IBOutlet var recordButton: UIButton!
+    
+    private var recorder: AVAudioRecorder?
+    var experiencesController: ExperiencesController?
+    var coordinate: CLLocationCoordinate2D?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        titleText.delegate = self
     }
     
-
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
     
-    @IBOutlet weak var titleTextField: UITextField!
-    
-    @IBOutlet weak var recordButton: UIButton!
-    
-    @IBOutlet weak var imageView: UIImageView!
-    
-    @IBOutlet weak var addImageButton: UIButton!
-    
-    @IBAction func addImage(_ sender: Any) {
+    func updateViews() {
+        guard isViewLoaded else { return }
+        
+        let isRecording = recorder?.isRecording ?? false
+        let recordButtonTitle = isRecording ? "Stop recording" : "Record Audio"
+        recordButton.setTitle(recordButtonTitle, for: .normal)
     }
     
     @IBAction func recording(_ sender: Any) {
+        let isRecording = recorder?.isRecording ?? false
+        if isRecording {
+            endRecording()
+        } else {
+            beginRecording()
+        }
     }
     
+    private func newRecordingURL() -> URL {
+        let fm = FileManager.default
+        let documentsDir = try! fm.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+        return documentsDir.appendingPathComponent(UUID().uuidString).appendingPathExtension("caf")
+    }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    private func beginRecording() {
+        do {
+            let format = AVAudioFormat(standardFormatWithSampleRate: 44100.0, channels: 1)!
+            recorder = try AVAudioRecorder(url: newRecordingURL(), format: format)
+            recorder?.record()
+            updateViews()
+        } catch {
+            NSLog("Error beginning the recording...")
+        }
+    }
+    
+    private func endRecording() {
+        recorder?.stop()
+        updateViews()
+    }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "VideoRecordSegue" {
+            guard let url = recorder?.url,
+                let imageData = imageView.image?.jpegData(compressionQuality: 0.1),
+                let coordinate = self.coordinate else { return }
+            
+            if let vc = segue.destination as? VideoViewController {
+                vc.audioURL = url
+                vc.image = imageData
+                vc.experience = titleText.text
+                vc.experienceController = experiencesController
+                vc.coordinate = coordinate
+            }
+        }
     }
-    */
-
-    
-    
-     var experiencesController: ExperiencesController?
-   var coordinate: CLLocationCoordinate2D?
-    
-    
-    
 }
+
+extension ExperiencesViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+        addImageButton.setTitle("", for: [])
+        picker.dismiss(animated: true, completion: nil)
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else { return }
+        
+        imageView?.image = image
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    private func presentInformationalAlertController(title: String?, message: String?, dismissActionCompletion: ((UIAlertAction) -> Void)? = nil, completion: (() -> Void)? = nil) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: dismissActionCompletion)
+        
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true, completion: completion)
+    }
+    
+    private func presentImagePickerController() {
+        
+        guard UIImagePickerController.isSourceTypeAvailable(.photoLibrary) else {
+            presentInformationalAlertController(title: "Error", message: "The photo library is unavailable")
+            return
+        }
+        
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func choosePhoto(_ sender: Any) {
+        let authorizationStatus = PHPhotoLibrary.authorizationStatus()
+        
+        switch authorizationStatus {
+        case .authorized:
+            presentImagePickerController()
+        case .notDetermined:
+            
+            PHPhotoLibrary.requestAuthorization { (status) in
+                
+                guard status == .authorized else {
+                    NSLog("User did not authorize access to the photo library")
+                    self.presentInformationalAlertController(title: "Error", message: "In order to access the photo library, you must allow this application access to it.")
+                    return
+                }
+                
+                self.presentImagePickerController()
+            }
+            
+        case .denied:
+            self.presentInformationalAlertController(title: "Error", message: "In order to access the photo library, you must allow this application access to it.")
+        case .restricted:
+            self.presentInformationalAlertController(title: "Error", message: "Unable to access the photo library. Your device's restrictions do not allow access.")
+            
+        }
+        presentImagePickerController()
+    }
+}
+
+
--- MediaProgramming/MediaProgramming/VideoViewController.swift
+++ MediaProgramming/MediaProgramming/VideoViewController.swift
@@ -5,24 +5,35 @@
 //  Created by Yvette Zhukovsky on 1/18/19.
 //  Copyright © 2019 Yvette Zhukovsky. All rights reserved.
 //
-
+import AVFoundation
+import Photos
 import UIKit
 
-class VideoViewController: UIViewController {
+class VideoViewController: UIViewController, AVCaptureFileOutputRecordingDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+     setupRecording()
+      
     }
     
     @IBAction func saveVideo(_ sender: Any) {
+        guard let experienceTitle = experience,
+            let audioURL = audioURL,
+            let videoURL = videoURL,
+            let imageData = image,
+            let coordinate = coordinate else { return }
+        experienceController?.makingExperiences(coordinate: coordinate, title: experienceTitle, image: imageData, audioURL: audioURL, videoURL: videoURL)
+        self.navigationController?.popToRootViewController(animated: true)
+        
     }
-  
-   
-    
-    
+ 
     @IBAction func recordingVideo(_ sender: Any) {
+        if recordOutput.isRecording {
+            recordOutput.stopRecording()
+        } else {
+            recordOutput.startRecording(to: newRecordingURL(), recordingDelegate: self)
+        }
     }
     
     
@@ -30,17 +41,106 @@ class VideoViewController: UIViewController {
     
     @IBOutlet weak var recordButton: UIButton!
  
-    @IBOutlet weak var videoView: UIView!
+    @IBOutlet weak var videoView: CameraPreviewView!
     
-    
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    var experienceController: ExperiencesController?
+    private var captureSession: AVCaptureSession!
+    private var recordOutput: AVCaptureMovieFileOutput!
+   var image: Data?
+    var experience: String?
+    var audioURL: URL?
+    var coordinate: CLLocationCoordinate2D?
+    var videoURL: URL?
+    
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        captureSession.startRunning()
     }
-    */
-
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        captureSession.stopRunning()
+    }
+   
+    
+    
+    
+   
+    func fileOutput(_ output: AVCaptureFileOutput, didStartRecordingTo fileURL: URL, from connections: [AVCaptureConnection]) {
+        DispatchQueue.main.async {
+            self.updateViews()
+        }
+    }
+    
+    func fileOutput(_ output: AVCaptureFileOutput, didFinishRecordingTo outputFileURL: URL, from connections: [AVCaptureConnection], error: Error?) {
+        DispatchQueue.main.async {
+            self.updateViews()
+            PHPhotoLibrary.requestAuthorization({ (status) in
+                if status != .authorized {
+                    NSLog("Please give Experiences access to your photos in settings.")
+                    return
+                }
+                
+                PHPhotoLibrary.shared().performChanges({
+                    self.videoURL = outputFileURL
+                    PHAssetChangeRequest.creationRequestForAssetFromVideo(atFileURL: outputFileURL)
+                }, completionHandler: { (success, error) in
+                    if let error = error {
+                        NSLog("Error saving video to photo library: \(error)")
+                    }
+                })
+            })
+        }
+    }
+    
+   
+    private func setupRecording() {
+        let captureSession = AVCaptureSession()
+        let device = bestCamera()
+        
+      
+        guard let videoDeviceInput = try? AVCaptureDeviceInput(device: device),
+            captureSession.canAddInput(videoDeviceInput) else {
+                fatalError()
+        }
+        captureSession.addInput(videoDeviceInput)
+        
+        let fileOutput = AVCaptureMovieFileOutput()
+        guard captureSession.canAddOutput(fileOutput) else { fatalError() }
+        captureSession.addOutput(fileOutput)
+        recordOutput = fileOutput
+        
+        captureSession.sessionPreset = .hd1920x1080
+        captureSession.commitConfiguration()
+        
+        self.captureSession = captureSession
+         videoView.videoPreviewLayer.session = captureSession
+    }
+    
+    private func bestCamera() -> AVCaptureDevice {
+        if let device = AVCaptureDevice.default(.builtInDualCamera, for: .video, position: .back) {
+            return device
+        } else if let device = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) {
+            return device
+        } else {
+            fatalError("Missing back camera device")
+        }
+    }
+    
+    private func newRecordingURL() -> URL {
+        let fm = FileManager.default
+        let documentsDir = try! fm.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+        
+        return documentsDir.appendingPathComponent(UUID().uuidString).appendingPathExtension("mov")
+    }
+    
+    private func updateViews() {
+        guard isViewLoaded else { return }
+        let recordButtonImageName = recordOutput.isRecording ? "Stop" : "Record"
+        recordButton.setImage(UIImage(named: recordButtonImageName)!, for: .normal)
+    }
+   
 }
