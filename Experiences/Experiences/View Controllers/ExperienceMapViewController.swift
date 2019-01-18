ba8d6604d6a0763543b18be0fac4e8e915459a0d
Jason Modisett <jason@Jasons-MacBook-Pro.local>
2019-01-18 11:08:08 -0600

    Added ExperienceMapViewController

--- Experiences/Experiences/View Controllers/ExperienceMapViewController.swift
+++ Experiences/Experiences/View Controllers/ExperienceMapViewController.swift
@@ -11,12 +11,36 @@ import MapKit
 
 class ExperienceMapViewController: UIViewController, MKMapViewDelegate {
     
-    let locationManager = CLLocationManager()
+    // MARK:- Types, properties, & IBOutlets
     
-
+    let locationManager = CLLocationManager()
+    let experienceController = ExperienceController()
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
+    // MARK:- View lifecycle methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.requestWhenInUseAuthorization()
+        
+        mapView.delegate = self
+        mapView.register(MKMarkerAnnotationView.self, forAnnotationViewWithReuseIdentifier: "ExperienceAnnotationView")
+    }
+    
+    
+    // MARK:- MapViewDelegate methods
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard let experience = annotation as? Experience else { return nil }
+        
+        let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "ExperienceAnnotationView", for: experience) as! MKMarkerAnnotationView
+        
+        annotationView.glyphText = "E"
+        
+        return annotationView
     }
 
 }
