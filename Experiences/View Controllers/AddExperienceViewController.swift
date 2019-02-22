2c9d9a5faebb53faa4e3ad65a1bb8fabcb9380a6
jkaunert <44586402+jkaunert@users.noreply.github.com>
2019-02-22 14:13:01 -0600

    (feat) Implements VC convenience extensions, photoLibrary picker functionality, photo library access permisssions, and apply photo filter functionality.

--- Experiences/View Controllers/AddExperienceViewController.swift
+++ Experiences/View Controllers/AddExperienceViewController.swift
@@ -8,8 +8,31 @@
 
 import UIKit
 import AVFoundation
+import Photos
 
-class AddExperienceViewController: UIViewController {
+extension AddExperienceViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        chooseImageButton.setTitle("", for: [])
+        
+        originalImage = info[.originalImage] as? UIImage
+        picker.dismiss(animated: true, completion: nil)
+        
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else { return }
+        
+        imageView.image = image
+        
+        //setImageViewHeight(with: image.ratio)
+    
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+}
+
+class AddExperienceViewController: ShiftableViewController {
 
     @IBAction func nextButtonTapped(_ sender: Any) {
         
@@ -37,9 +60,109 @@ class AddExperienceViewController: UIViewController {
     }
     */
     
+    //MARK: - IBOutlets
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var chooseImageButton: UIButton!
+    @IBOutlet weak var nextButton: UIBarButtonItem!
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var recordAudioButton: UIButton!
+    @IBOutlet weak var canceButton: UIBarButtonItem!
+    
+    //MARK: - IBActions
+    
+    @IBAction func cancelButtonTapped(_ sender: Any) {
+        self.dismiss(animated: true
+            , completion: nil)
+    }
+    @IBAction func recordAudioButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func chooseImageButtonTapped(_ sender: Any) {
+        
+        let authorizationStatus = PHPhotoLibrary.authorizationStatus()
+        
+        switch authorizationStatus {
+        case .authorized:
+            presentImagePickerController()
+        case .notDetermined:
+            
+            PHPhotoLibrary.requestAuthorization { (status) in
+                
+                guard status == .authorized else {
+                    NSLog("User did not authorize access to the photo library")
+                    self.presentInformationalAlertController(title: "Error", message: "In order to access the photo library, you must allow this application access to it.")
+                    return
+                }
+                
+                self.presentImagePickerController()
+            }
+            
+        case .denied:
+            self.presentInformationalAlertController(title: "Error", message: "In order to access the photo library, you must allow this application access to it.")
+        case .restricted:
+            self.presentInformationalAlertController(title: "Error", message: "Unable to access the photo library. Your device's restrictions do not allow access.")
+            
+        }
+        presentImagePickerController()
+    }
+    
+    //MARK: - Properties
+    private let context = CIContext(options: nil)
+    private var appliedFilter: CIFilter = CIFilter(name: "CIPhotoEffectInstant")!
+    private var originalImage: UIImage? {
+        didSet {
+            
+            updateImageView()
+            
+        }
+    }
+    
     //MARK: - Private Methods
-    private func showCamera() {
-        performSegue(withIdentifier: "ShowCamera", sender: self)
+    private func presentImagePickerController() {
+        
+        guard UIImagePickerController.isSourceTypeAvailable(.photoLibrary) else {
+            presentInformationalAlertController(title: "Error", message: "The photo library is unavailable")
+            return
+        }
+        
+        let imagePicker = UIImagePickerController()
+        
+        imagePicker.delegate = self
+        
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    private func updateImageView() {
+        guard let image = originalImage else { return }
+        DispatchQueue.main.async {
+            self.imageView?.image = self.applyFilterToImage(to: image)
+        }
+    }
+    
+    private func applyFilterToImage(to image: UIImage) -> UIImage {
+        let inputImage: CIImage
+        
+        if let ciImage = image.ciImage {
+            inputImage = ciImage
+        }else if let cgImage = image.cgImage {
+            inputImage = CIImage(cgImage: cgImage)
+        }else {
+            return image
+        }
+        appliedFilter.setValue(inputImage, forKey: kCIInputImageKey)
+        
+        
+        guard let outputImage = appliedFilter.outputImage else {
+            return image
+        }
+        
+        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else {
+            return image
+        }
+        
+        return UIImage(cgImage: cgImage)
     }
 
 }
