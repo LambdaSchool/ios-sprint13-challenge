05671daf7f88f06edb7cd9c9600b9e8a78413da4
Sergey Osipyan <osipyansergey@gmail.com>
2019-02-22 08:55:11 -0800

    add new project, create UI frome storyboard

---
+++ Sprint13/Sprint13/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sprint13
+//
+//  Created by Sergey Osipyan on 2/22/19.
+//  Copyright © 2019 Sergey Osipyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
