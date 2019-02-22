bd750c365b35fe1d7a442ad015eccc9cc6becbb8
Dillon McElhinney <dillon.mce@gmail.com>
2019-02-22 10:13:22 -0700

    Get basic model set up

---
+++ Experiences/Experiences/Models/ExperienceController.swift
@@ -0,0 +1,26 @@
+//
+//  ExperienceController.swift
+//  Experiences
+//
+//  Created by Dillon McElhinney on 2/22/19.
+//  Copyright © 2019 Dillon McElhinney. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+class ExperienceController {
+    
+    private var experiences: [Experience] = []
+    
+    func createExperience(title: String, audioURL: URL, videoURL: URL, imageURL: URL, geotag: CLLocationCoordinate2D? = nil ) {
+        let newExperience = Experience(title: title, audioURL: audioURL, videoURL: videoURL, imageURL: imageURL, geotag: geotag)
+        experiences.append(newExperience)
+    }
+    
+    func experiencesWithAnnotations() -> Set<Experience> {
+        let geotagged = experiences.filter() { $0.geotag != nil }
+        return Set(geotagged)
+    }
+    
+}
