4cb0cbcf260674d516813ea5011e366237ee2d5e
Audrey Welch <audreywelch@Audreys-MacBook-Pro.local>
2019-02-22 12:16:16 -0600

    Recorder and Player set up and functioning

--- Experiences/Experiences/View Controllers/NewExperienceViewController.swift
+++ Experiences/Experiences/View Controllers/NewExperienceViewController.swift
@@ -3,7 +3,7 @@ import UIKit
 import CoreImage
 
 // In order to be delegate of image picker controller, need to conform
-class NewExperienceViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class NewExperienceViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, PlayerDelegate, RecorderDelegate {
     
     // MARK: - Properties
     
@@ -17,6 +17,9 @@ class NewExperienceViewController: UIViewController, UIImagePickerControllerDele
     private let filter = CIFilter(name: "CIPhotoEffectTonal")
     private let context = CIContext(options: nil)
     
+    private let player = Player()
+    private let recorder = Recorder()
+    
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var addImageButton: UIButton!
@@ -27,10 +30,20 @@ class NewExperienceViewController: UIViewController, UIImagePickerControllerDele
     @IBOutlet weak var timerSlider: UISlider!
     @IBOutlet weak var saveAudioButton: UIButton!
     
+    private lazy var timeFormatter: DateComponentsFormatter = {
+        let f = DateComponentsFormatter()
+        f.unitsStyle = .positional
+        f.zeroFormattingBehavior = .pad
+        f.allowedUnits = [.minute, .second]
+        return f
+    }()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        player.delegate = self
+        recorder.delegate = self
         
     }
     
@@ -125,11 +138,36 @@ class NewExperienceViewController: UIViewController, UIImagePickerControllerDele
     // MARK: - Audio Methods
     
     @IBAction func recordAudio(_ sender: Any) {
-        
-        
+        recorder.toggleRecording()
     }
     
     @IBAction func tappedPlayButton(_ sender: Any) {
+        player.playPause(file: recorder.currentFile)
+    }
+    
+    func playerDidChangeState(_ player: Player) {
+        updateViews()
+    }
+    
+    func recorderDidChangeState(_ recorder: Recorder) {
+        updateViews()
+    }
+    
+    private func updateViews() {
+        let isPlaying = player.isPlaying
+        playButton.setTitle(isPlaying ? "Pause" : "Play", for: .normal)
+        
+        let isRecording = recorder.isRecording
+        recordButton.setTitle(isRecording ? "Stop" : "Record", for: .normal)
+        
+        let elapsedTime = player.elapsedTime
+        elapsedTimeLabel.text = timeFormatter.string(from: player.elapsedTime)
+        
+        remainingTimeLabel.text = timeFormatter.string(from: player.remainingTime)
+        
+        timerSlider.minimumValue = 0
+        timerSlider.maximumValue = Float(player.totalTime)
+        timerSlider.value = Float(player.elapsedTime)
     }
     
     @IBAction func saveAudioButton(_ sender: Any) {
