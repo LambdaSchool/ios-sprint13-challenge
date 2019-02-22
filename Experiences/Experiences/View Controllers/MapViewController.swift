1ec64adb4fa54ba8f2f7a5379e162cb2691b143d
Dillon McElhinney <dillon.mce@gmail.com>
2019-02-22 11:02:23 -0700

    Fill out Map View Controller and add ability to pick and filter an image.

--- Experiences/Experiences/View Controllers/MapViewController.swift
+++ Experiences/Experiences/View Controllers/MapViewController.swift
@@ -7,24 +7,70 @@
 //
 
 import UIKit
+import MapKit
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, MKMapViewDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    // MARK: - Properties
+    @IBOutlet weak var mapView: MKMapView!
+    
+    private let experienceController = ExperienceController()
+    private var experiences: Set<Experience> = [] {
+        didSet { updateExperienceAnnotations(oldValue) }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Lifecycle Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        LocationHelper.shared.requestAccess(completion: { granted in
+            DispatchQueue.main.async {
+                if granted {
+                    let userTrackingButton = MKUserTrackingButton(mapView: self.mapView)
+                    userTrackingButton.translatesAutoresizingMaskIntoConstraints = false
+                    self.mapView.addSubview(userTrackingButton)
+                    
+                    userTrackingButton.leftAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leftAnchor, constant: 20).isActive = true
+                    userTrackingButton.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+                }
+            }
+        })
+        
+        mapView.register(MKMarkerAnnotationView.self, forAnnotationViewWithReuseIdentifier: "ExperienceAnnotationView")
+        mapView.delegate = self
+        fetchExperiences()
     }
-    */
+    
+    // MARK: - MK Map View Delegate
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard annotation is Experience else { return nil }
+        
+        let experienceView = mapView.dequeueReusableAnnotationView(withIdentifier: "ExperienceAnnotationView", for: annotation) as! MKMarkerAnnotationView
+        
+        return experienceView
+    }
+    
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destinationVC = segue.destination as? UINavigationController {
+            let newExperienceVC = destinationVC.children.first! as! NewExperienceViewController
+            newExperienceVC.experienceController = experienceController
+        }
+    }
+    
+    // MARK: - Utility Methods
+    private func fetchExperiences() {
+        experiences = experienceController.experiencesWithAnnotations()
+    }
 
+    private func updateExperienceAnnotations(_ oldValue: Set<Experience>) {
+        
+        let addedExperiences = experiences.subtracting(oldValue)
+        let removeExperiences = oldValue.subtracting(experiences)
+        
+        DispatchQueue.main.async {
+            self.mapView.removeAnnotations(Array(removeExperiences))
+            self.mapView.addAnnotations(Array(addedExperiences))
+        }
+    }
 }
--- Experiences/Experiences/View Controllers/NewExperienceViewController.swift
+++ Experiences/Experiences/View Controllers/NewExperienceViewController.swift
@@ -8,23 +8,101 @@
 
 import UIKit
 
-class NewExperienceViewController: UIViewController {
+class NewExperienceViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    // MARK: - Properties
+    var experienceController: ExperienceController!
+    
+    private let context = CIContext(options: nil)
+    private var originalImage: UIImage? {
+        didSet { updateImageView() }
+    }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var addPhotoButton: UIButton!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var playButton: UIButton!
+    
+    // MARK: - UI Actions
+    
+    @IBAction func addPhoto(_ sender: Any) {
+        PhotoLibraryHelper.shared.checkAuthorizationStatus { (alertController) in
+            if let alertController = alertController {
+                self.present(alertController, animated: true)
+            } else {
+                self.presentImagePickerController()
+            }
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func playAudio(_ sender: Any) {
+        
     }
-    */
-
+    
+    @IBAction func recordAudio(_ sender: Any) {
+        
+    }
+    
+    @IBAction func cancel(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
+    // MARK: - UI Image Picker Controller Delegate
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        addPhotoButton.setTitle("", for: .normal)
+        picker.dismiss(animated: true)
+        
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else { return }
+        originalImage = image
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true)
+    }
+    
+    // MARK: - Private Utility Methods
+    private func presentImagePickerController() {
+        guard UIImagePickerController.isSourceTypeAvailable(.photoLibrary) else {
+            let errorAlert = UIAlertController.informationalAlertController(message: "The photo library is unavailable")
+            present(errorAlert, animated: true)
+            return
+        }
+        
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    private func updateImageView() {
+        guard let image = originalImage else { return }
+        
+        imageView.image = reduceAndFilter(image)
+    }
+    
+    private func reduceAndFilter(_ image: UIImage?, longEdge resolution: Int = 1000) -> UIImage? {
+        guard var inputImage: CIImage = image?.getCIImage() else { return image }
+        
+        let scale = CGFloat(resolution) / max(inputImage.extent.size.width, inputImage.extent.size.height)
+        if scale < 1 {
+            let scaleFilter = CIFilter(name: "CILanczosScaleTransform")!
+            scaleFilter.setValue(inputImage, forKey: kCIInputImageKey)
+            scaleFilter.setValue(scale, forKey: kCIInputScaleKey)
+            if let outputImage = scaleFilter.outputImage { inputImage = outputImage}
+        }
+        
+        let monochromeFilter = CIFilter(name: "CIColorMonochrome")!
+        let color = CIColor(red: 0.5, green: 0.5, blue: 0.5)
+        monochromeFilter.setValue(inputImage, forKey: kCIInputImageKey)
+        monochromeFilter.setValue(color, forKey: kCIInputColorKey)
+        monochromeFilter.setValue(1.0, forKey: kCIInputIntensityKey)
+        
+        guard let outputImage = monochromeFilter.outputImage,
+            let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return image }
+        
+        return UIImage(cgImage: cgImage)
+    }
 }
