351f14391f2a52aaa5ff9aa58a4763d86d744091
Lotanna <sugabelly@gmail.com>
2019-02-28 19:45:39 -0500

    set up saving to Core Data

---
+++ Experiences/Experiences/Core Data/ExperienceConvenience.swift
@@ -0,0 +1,32 @@
+//
+//  ExperienceConvenience.swift
+//  Experiences
+//
+//  Created by Lotanna Igwe-Odunze on 2/28/19.
+//  Copyright © 2019 Sugabelly LLC. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension Experience { //This should be extending the Entity in the data model.
+    
+    convenience init(
+        title: String,
+        date: Date,
+        longitude: Double,
+        latitude: Double,
+        picture: String,
+        video: String,
+        
+        context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        self.init(context: context)
+        self.title = title //Attribute from the Entity
+        self.date = date //Attribute from the Entity
+        self.longitude = longitude //Attribute from the Entity
+        self.latitude = latitude //Attribute from the Entity
+        self.picture = picture //Attribute from the Entity
+        self.video = video //Attribute from the Entity
+
+    }
+}
