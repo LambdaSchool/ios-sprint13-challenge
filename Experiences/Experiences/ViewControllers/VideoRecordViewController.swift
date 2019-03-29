2c6fac3e93ff94033cf633d69b04eca1c3c97b43
Frulwinn <frulwinn@gmail.com>
2019-03-29 10:35:09 -0500

    added objects to storyboard and classed view controllers

---
+++ Experiences/Experiences/ViewControllers/VideoRecordViewController.swift
@@ -0,0 +1,30 @@
+//
+//  VideoRecordViewController.swift
+//  Experiences
+//
+//  Created by Lambda_School_Loaner_34 on 3/29/19.
+//  Copyright © 2019 Frulwinn. All rights reserved.
+//
+
+import UIKit
+
+class VideoRecordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
