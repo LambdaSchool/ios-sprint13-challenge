7cc6441e60d67832404587f495b133c0b7fcccd8
Frulwinn <frulwinn@gmail.com>
2019-03-29 10:53:04 -0500

    added recorder, UIImage+Scaling, and CameraPreviewView files to project

---
+++ Experiences/Experiences/CameraPreviewView.swift
@@ -0,0 +1,26 @@
+//
+//  CameraPreviewView.swift
+//  Experiences
+//
+//  Created by Lambda_School_Loaner_34 on 3/29/19.
+//  Copyright © 2019 Frulwinn. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class CameraPreviewView: UIView {
+    
+    override class var layerClass: AnyClass {
+        return AVCaptureVideoPreviewLayer.self
+    }
+    
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer {
+        return layer as! AVCaptureVideoPreviewLayer
+    }
+    
+    var session: AVCaptureSession? {
+        get { return videoPreviewLayer.session }
+        set { videoPreviewLayer.session = newValue }
+    }
+}
