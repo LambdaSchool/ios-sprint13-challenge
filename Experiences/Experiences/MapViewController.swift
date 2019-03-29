7487d63aa9e9beb6d403a13f10aff026c6398f6b
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2019-03-29 12:03:49 -0400

    build out UI

--- Experiences/Experiences/MapViewController.swift
+++ Experiences/Experiences/MapViewController.swift
@@ -9,13 +9,31 @@
 import UIKit
 import MapKit
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, MKMapViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        mapView.delegate = self
+        requestPermission()
+    }
+    
+    func requestPermission() {
+        locationManager.requestWhenInUseAuthorization()
+        
     }
 
-
+    @IBAction func addExperience(_ sender: Any) {
+        
+    }
+    
+    
+    
+    // MARK: - Properties
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var addButton: UIButton!
+    let locationManager = CLLocationManager()
+    var location: CLLocationCoordinate2D!
+    
+    
 }
 
