b8047b5145ed2037abb5b199aafb700f2fcd598e
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2019-03-29 12:12:46 -0400

    add recorderviewcontroller file to handle recording functionality

---
+++ Experiences/Experiences/RecorderViewController.swift
@@ -0,0 +1,28 @@
+//
+//  RecorderViewController.swift
+//  Experiences
+//
+//  Created by Julian A. Fordyce on 3/29/19.
+//  Copyright © 2019 Julian A. Fordyce. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Photos
+import AVFoundation
+
+
+
+class RecorderViewController: UIViewController {
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
