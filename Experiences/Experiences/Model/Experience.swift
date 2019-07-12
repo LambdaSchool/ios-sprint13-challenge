ffe3b24cebdafe1d088fbf235976c3563b44d816
Mitchell Budge <mitchellbudge@gmail.com>
2019-07-12 11:46:37 -0600

    Update Experience.swift

--- Experiences/Experiences/Model/Experience.swift
+++ Experiences/Experiences/Model/Experience.swift
@@ -11,58 +11,21 @@ import Foundation
 import CoreLocation
 import MapKit
 
-class Experience: NSObject {
+class Experience: NSObject, MKAnnotation {
     
-    /*
+    var coordinate: CLLocationCoordinate2D
+    var title: String?
+    var image: UIImage?
+    var audio: URL?
+    var video: URL?
     
-    var coordinate: CLLocationCoordinate2D?
-    
-    var title: String? {
-        return properties.place
+    init(coordinate: CLLocationCoordinate2D, title: String?, image: UIImage?, audio: URL?, video: URL?) {
+        self.coordinate = coordinate
+        self.title = title
+        self.image = image
+        self.audio = audio
+        self.video = video
     }
     
-    let properties: Properties
-    let geometry: Geometry
-    
-    struct Properties: Decodable {
-        
-        
-        let mag: Double
-        let place: String
-        let time: Date
-        
-        enum PropertiesCodingKeys: String, CodingKey {
-            case mag
-            case place
-            case time
-        }
-        
-        init(from decoder: Decoder) throws {
-            let container = try decoder.container(keyedBy: PropertiesCodingKeys.self)
-            
-            mag = (try? container.decode(Double.self, forKey: .mag)) ?? 0
-            place = try container.decode(String.self, forKey: .place)
-            time = try container.decode(Date.self, forKey: .time)
-        }
-    }
-    
-    struct Geometry: Decodable {
-        let location: CLLocationCoordinate2D
-        
-        enum GeometryCodingKeys: String, CodingKey {
-            case coordinates
-        }
-        
-        init(from decoder: Decoder) throws {
-            let container = try decoder.container(keyedBy: GeometryCodingKeys.self)
-            var coordinatesContainer = try container.nestedUnkeyedContainer(forKey: .coordinates)
-            
-            let longitude = try coordinatesContainer.decode(Double.self)
-            let latitude = try coordinatesContainer.decode(Double.self)
-            
-            location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
-        }
-    }
-    */
 }
 
