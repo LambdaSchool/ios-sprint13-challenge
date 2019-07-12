1ffafb3e79e5b3565e1073c9111e91a69e2af26b
victor <ruizdvictor@gmail.com>
2019-07-12 11:58:30 -0400

    added experience controller functionality

---
+++ Experiences/Experiences/Model Controllers/ExperienceController.swift
@@ -0,0 +1,15 @@
+//
+//  ExperienceController.swift
+//  Experiences
+//
+//  Created by Victor  on 7/12/19.
+//  Copyright © 2019 Victor . All rights reserved.
+//
+
+import Foundation
+
+class ExperienceController {
+    // MARK: - Properties
+    
+    var experiences = [Experience]()
+}
