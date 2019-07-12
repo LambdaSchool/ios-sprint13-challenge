e4e96f408a1105ca75eacf22cda33f7a0c43e794
hectorsvill <hectorstevenvillasano@gmail.com>
2019-07-12 08:29:16 -0700

    PostView setup

--- experiences/experiences/View Controller/PostViewController.swift
+++ experiences/experiences/View Controller/PostViewController.swift
@@ -9,22 +9,18 @@
 import UIKit
 
 class PostViewController: UIViewController {
-
+	@IBOutlet var titleTextField: UITextField!
+	@IBOutlet var imageView: UIImageView!
+	
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+	
+	@IBAction func addPosterButtonPressed(_ sender: Any) {
+		print("addPosterButtonPressed")
+	}
+	
+	@IBAction func recordButtonPressed(_ sender: Any) {
+		print("recordButtonPressed")
+	}
 }
