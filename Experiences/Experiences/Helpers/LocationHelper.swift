27e358cd51bd201d12c46d7f42a549cad5b257ac
Hayden Hastings <haydenhastings32@gmail.com>
2019-07-12 09:33:05 -0600

    Added LocationHelper to get the current users location.

---
+++ Experiences/Experiences/Helpers/LocationHelper.swift
@@ -0,0 +1,53 @@
+//
+//  LocationHelper.swift
+//  Experiences
+//
+//  Created by Hayden Hastings on 7/12/19.
+//  Copyright © 2019 Hayden Hastings. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationHelper: NSObject, CLLocationManagerDelegate {
+    
+    static var shared: LocationHelper = LocationHelper()
+    
+    lazy var locationManager: CLLocationManager = {
+        let result = CLLocationManager()
+        result.desiredAccuracy = kCLLocationAccuracyKilometer
+        result.pausesLocationUpdatesAutomatically = true
+        result.delegate = self
+        return result
+    }()
+    
+    // MARK: - Get the users location
+    var currentLocation: CLLocation? {
+        return locationManager.location
+    }
+    
+    override init() {
+        super.init()
+        
+    }
+    
+    func requestUserAuthorization() {
+        locationManager.requestAlwaysAuthorization()
+        locationManager.requestWhenInUseAuthorization()
+        startTrackingLocation()
+    }
+    
+    func startTrackingLocation() {
+        locationManager.startUpdatingLocation()
+    }
+    
+    func stopTrackingLocation() {
+        locationManager.stopUpdatingLocation()
+    }
+    
+    // MARK: - Delegate Function
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let _: CLLocationCoordinate2D = manager.location?.coordinate else { return }
+        stopTrackingLocation()
+    }
+}
