5478e621a15c2cdc686d2b2f6b2d09184d535e50
Christopher Aronson <christopheraronson@icloud.com>
2019-07-12 08:14:49 -0700

    Init Commit

---
+++ Experiences/Experiences/View Controllers/MapViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MapViewController.swift
+//  Experiences
+//
+//  Created by Christopher Aronson on 7/12/19.
+//  Copyright © 2019 Christopher Aronson. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
