c11ba2d07a171738f066ee008434402661cf69f7
Marlon <marmoneymar@icloud.com>
2019-10-04 19:18:18 -0400

    Basic model setup

--- Experiences/Experiences/Model/Experience.swift
+++ Experiences/Experiences/Model/Experience.swift
@@ -9,11 +9,26 @@
 import Foundation
 import MapKit
 
-struct Experience {
-    let title: String
-    let image: Data?
+class Experience: NSObject, MKAnnotation {
+
+    var coordinate: CLLocationCoordinate2D {
+        return CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+
+    var title: String?
+    let imageURL: URL?
     let audioURL: URL?
     let videoURL: URL?
     let latitude: Double
     let longitude: Double
+
+    init(title: String, imageURL: URL?, audioURL: URL?, videoURL: URL?, latitude: Double, longitude: Double) {
+        self.title = title
+        self.imageURL = imageURL
+        self.audioURL = audioURL
+        self.videoURL = videoURL
+        self.latitude = latitude
+        self.longitude = longitude
+
+    }
 }
