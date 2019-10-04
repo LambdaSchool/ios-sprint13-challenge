68cfc142b413db581776d7f29da45f75b9171afa
Michael Redig <mredig@gmail.com>
2019-10-04 12:01:32 -0500

    added map view and new item button

--- Experiences/Experiences/ViewController.swift
+++ Experiences/Experiences/ViewController.swift
@@ -9,12 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+	@IBOutlet var newExperienceButton: UIButton!
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
+		newExperienceButton.layer.cornerRadius = 10
 	}
 
-
+	@IBAction func catalogNewExperienceButtonPressed(_ sender: UIButton) {
+	}
+	
 }
 
