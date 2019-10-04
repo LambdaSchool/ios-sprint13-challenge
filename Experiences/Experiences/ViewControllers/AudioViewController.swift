8a2294066f5ed5dd058da8d1edb545c6f96f79aa
Michael Redig <mredig@gmail.com>
2019-10-04 14:06:52 -0500

    added audio preview

--- Experiences/Experiences/ViewControllers/AudioViewController.swift
+++ Experiences/Experiences/ViewControllers/AudioViewController.swift
@@ -9,9 +9,11 @@
 import UIKit
 
 class AudioViewController: UIViewController {
-	@IBOutlet var recordButton: UIButton!
-	@IBOutlet var titleTextField: UITextField!
-	@IBOutlet var recordDurationLabel: UILabel!
+	@IBOutlet private var recordButton: UIButton!
+	@IBOutlet private var titleTextField: UITextField!
+	@IBOutlet private var recordDurationLabel: UILabel!
+	@IBOutlet private var previewButton: UIButton!
+	@IBOutlet private var saveButton: UIBarButtonItem!
 
 	private lazy var timeFormatter: DateComponentsFormatter = {
 		let formatting = DateComponentsFormatter()
@@ -26,9 +28,14 @@ class AudioViewController: UIViewController {
 		return recorder
 	}()
 
+	var audioPlayer: AudioPlayer?
+
 	private func updateViews() {
 		recordButton.isSelected = audioRecorder.isRecording
+		recordButton.isEnabled = audioPlayer?.isPlaying != true
+		previewButton.isEnabled = audioPlayer != nil
 		updateTimeLabel()
+		saveButtonEnableLogic()
 	}
 
 	private func updateTimeLabel() {
@@ -38,16 +45,28 @@ class AudioViewController: UIViewController {
 		recordDurationLabel.text = "\(minSec ?? "").\(milliStr)"
 	}
 
+	private func saveButtonEnableLogic() {
+		if audioPlayer != nil && titleTextField.text?.isEmpty == false {
+			saveButton.isEnabled = true
+		} else {
+			saveButton.isEnabled = false
+		}
+	}
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
 		setupRecordLabel()
 		updateViews()
     }
-
 	private func setupRecordLabel() {
 		let font = UIFont.monospacedDigitSystemFont(ofSize: 48, weight: .medium)
 		recordDurationLabel.font = font
+	}
+
+
+	@IBAction func textFieldChanged(_ sender: UITextField) {
+		saveButtonEnableLogic()
 	}
 
 	@IBAction func recordButton(_ sender: UIButton) {
@@ -57,18 +76,44 @@ class AudioViewController: UIViewController {
 	@IBAction func saveButtonPressed(_ sender: UIBarButtonItem) {
 
 	}
+
+	@IBAction func previewButtonPressed(_ sender: UIButton) {
+		audioPlayer?.playPause()
+	}
+
+	// MARK: - playback setup
+	private func setupPreview(playbackURL: URL) {
+		audioPlayer = nil
+		do {
+			audioPlayer = try AudioPlayer(with: playbackURL)
+			audioPlayer?.delegate = self
+		} catch {
+			NSLog("Error creating audio player: \(error)")
+		}
+	}
 }
 
+// MARK: - recording delegate
 extension AudioViewController: AudioRecorderDelegate {
 	func recorderDidChangeState(_ recorder: AudioRecorder) {
 		updateViews()
 	}
 
 	func recorderDidFinishSavingFile(_ recorder: AudioRecorder, url: URL) {
+		setupPreview(playbackURL: url)
 		updateViews()
 	}
 
 	func recorderLoopUpdated(_ recorder: AudioRecorder) {
 		updateTimeLabel()
 	}
+}
+
+// MARK: - playback delegate
+extension AudioViewController: AudioPlayerDelegate {
+	func playerDidChangeState(_ player: AudioPlayer) {
+		updateViews()
+	}
+
+	func playerPlaybackLoopUpdated(_ player: AudioPlayer) {}
 }
