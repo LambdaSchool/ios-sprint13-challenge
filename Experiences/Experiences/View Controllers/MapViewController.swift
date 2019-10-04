969249d4ba90691a9a3c77f6c58563f0433ca27f
Marlon <marmoneymar@icloud.com>
2019-10-04 19:20:45 -0400

    Getting current user location with mapkit

--- Experiences/Experiences/View Controllers/MapViewController.swift
+++ Experiences/Experiences/View Controllers/MapViewController.swift
@@ -8,15 +8,46 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class MapViewController: UIViewController {
-
     
+    @IBOutlet weak var mapView: MKMapView!
+
+    let locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        locationManager.requestWhenInUseAuthorization()
 
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+        }
 
     }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        getUserCurrentLocation()
+    }
+
+    @IBAction func currentLocationTapped(_ sender: UIButton) {
+        getUserCurrentLocation()
+    }
+
+    func getUserCurrentLocation() {
+        guard let userCurrentLocation = mapView.userLocation.location else { return }
+        let coordinate = userCurrentLocation.coordinate
+        let coordinateCenter = CLLocationCoordinate2D(latitude: coordinate.latitude, longitude: coordinate.longitude)
+        let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        let region = MKCoordinateRegion(center: coordinateCenter, span: span)
+        mapView.setRegion(region, animated: true)
+    }
+
+}
+
+extension MapViewController: CLLocationManagerDelegate {
 
 }
