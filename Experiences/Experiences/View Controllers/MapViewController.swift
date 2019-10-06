a72e02a3cd3fb6fc92b2d0c2123d39dc64a4cb0e
Marlon <marmoneymar@icloud.com>
2019-10-06 17:14:30 -0400

    Getting mapKit working without extension. Added custom tableview cell

--- Experiences/Experiences/View Controllers/MapViewController.swift
+++ Experiences/Experiences/View Controllers/MapViewController.swift
@@ -28,45 +28,46 @@ class MapViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         locationManager.requestWhenInUseAuthorization()
-        mapView.delegate = self
+//        mapView.delegate = self
 //        mapView.register(MKMarkerAnnotationView.self, forAnnotationViewWithReuseIdentifier: "ExperienceView")
 //        loadExperiences()
 //        let myLocation = MKPointAnnotation()
 //        myLocation.coordinate = CLLocationCoordinate2D(latitude: 41.02142333984375, longitude: -80.74500288720755)
 //        mapView.addAnnotation(myLocation)
-        createAnnotations(locations: experienceController.experiences)
+//        createAnnotations(locations: experienceController.experiences)
     }
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        getUserCurrentLocation()
+//        getUserCurrentLocation()
+        loadExperiences()
     }
 
     @IBAction func currentLocationTapped(_ sender: UIButton) {
         getUserCurrentLocation()
     }
 
-    func createAnnotations(locations: [ExperienceTemp]) {
-        mapView.removeAnnotations(experienceController.experiences)
-        for location in locations {
-            let annotations = MKPointAnnotation()
-            annotations.title = location.header
-            annotations.subtitle = dateFormatter.string(from: location.timestamp)
-            annotations.coordinate = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
-            mapView.addAnnotation(annotations)
+//    func createAnnotations(locations: [ExperienceTemp]) {
+//        mapView.removeAnnotations(experienceController.experiences)
+//        for location in locations {
+//            let annotations = MKPointAnnotation()
+//            annotations.title = location.header
+//            annotations.subtitle = dateFormatter.string(from: location.timestamp)
+//            annotations.coordinate = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+//            mapView.addAnnotation(annotations)
+//        }
+//    }
+
+    func loadExperiences() {
+        mapView.removeAnnotations(mapView.annotations)
+        mapView.addAnnotations(experienceController.experiences)
+        if let firstExperience = experienceController.experiences.first {
+            let span = MKCoordinateSpan(latitudeDelta: 2, longitudeDelta: 2)
+            let region = MKCoordinateRegion(center: firstExperience.coordinate, span: span)
+
+            mapView.setRegion(region, animated: true)
         }
     }
-
-//    func loadExperiences() {
-//        mapView.removeAnnotations(experienceController.experiences)
-//        if let firstExperience = experienceController.experiences.first {
-//            let span = MKCoordinateSpan(latitudeDelta: 2, longitudeDelta: 2)
-//            let region = MKCoordinateRegion(center: firstExperience.coordinate, span: span)
-//
-//            mapView.setRegion(region, animated: true)
-//        }
-//        mapView.addAnnotations(experienceController.experiences)
-//    }
 
     func getUserCurrentLocation() {
         guard let userCurrentLocation = mapView.userLocation.location else { return }
@@ -79,20 +80,20 @@ class MapViewController: UIViewController {
 
 }
 
-extension MapViewController: MKMapViewDelegate {
-    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+//extension MapViewController: MKMapViewDelegate {
+//    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
 
-        guard annotation is MKPointAnnotation else { return nil }
-
-        let identifier = "ExperienceView"
-        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
-        if annotationView == nil {
-            annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
-            annotationView?.canShowCallout = true
-        } else {
-            annotationView?.annotation = annotation
-        }
-        return annotationView
+//        guard annotation is MKPointAnnotation else { return nil }
+//
+//        let identifier = "ExperienceView"
+//        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+//        if annotationView == nil {
+//            annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+//            annotationView?.canShowCallout = true
+//        } else {
+//            annotationView?.annotation = annotation
+//        }
+//        return annotationView
 
 //        guard let experience = annotation as? ExperienceTemp else { fatalError("Invalid type") }
 //        guard let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "ExperienceView") as? MKMarkerAnnotationView else { fatalError("Incorrect view is registered") }
@@ -106,8 +107,8 @@ extension MapViewController: MKMapViewDelegate {
 //        annotationView.detailCalloutAccessoryView = detailView
 //
 //        return annotationView
-    }
-}
+//    }
+//}
 
 //extension MapViewController: CLLocationManagerDelegate {
 //    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
---
+++ Experiences/Experiences/Views/ExperienceTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ExperienceTableViewCell.swift
+//  Experiences
+//
+//  Created by Marlon Raskin on 10/6/19.
+//  Copyright © 2019 Marlon Raskin. All rights reserved.
+//
+
+import UIKit
+
+class ExperienceTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+
+}
