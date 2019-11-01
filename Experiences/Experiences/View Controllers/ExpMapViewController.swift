3dbc970f4178314136d5dc3a306ac4c22edeaa89
ladybeitel <ciarabeitel@gmail.com>
2019-11-01 11:06:35 -0400

    Add Map View and Exp View Controller.

---
+++ Experiences/Experiences/View Controllers/ExpMapViewController.swift
@@ -0,0 +1,80 @@
+//
+//  ExpMapViewController.swift
+//  Experiences
+//
+//  Created by Ciara Beitel on 11/1/19.
+//  Copyright © 2019 Ciara Beitel. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class ExpMapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
+    var expController: ExpController!
+    let annotationReuseIdentifier = "ExpAnnotation"
+    
+    @IBOutlet weak var experiencesMapView: MKMapView!
+    
+    
+    @IBAction func addNewExpButton(_ sender: UIButton) {
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mapView.delegate = self
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        let status = CLLocationManager.authorizationStatus()
+        switch status {
+            case .notDetermined:
+                locationManager.requestWhenInUseAuthorization()
+            case .authorizedWhenInUse:
+                locationManager.requestLocation()
+            default:
+            break
+        }
+        mapView.register(MKMarkerAnnotationView.self, forAnnotationViewWithReuseIdentifier: annotationReuseIdentifier)
+    }
+    
+    override func viewWillAppear() {
+        super.viewWillAppear(animated)
+        mapView.removeAnnotations(mapView.annotations)
+        let annotations = mapView.annotations.compactMap({ $0 as? (your model object type) })
+            mapView.addAnnotations(annotations)
+        mapView.addAnnotations(annotations)
+    }
+        
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation is MKUserLocation {
+            return nil
+        }
+        guard let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: annotationReuseIdentifier, for: annotation) as? MKMarkerAnnotationView else { return nil }
+        annotationView.titleVisibility = .adaptive
+        annotationView.subtitleVisibility = .adaptive
+        return annotationView
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        NSLog(“Location manager failed with error: \(error)”)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        guard status == .authorizedWhenInUse else { return }
+        locationManager.requestLocation()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ShowNewExpSegue" {
+            let destinationVC = segue.destination as? ExperienceViewController
+            destinationVC?.expController = expController
+            
+        }
+    }
+}
---
+++ Experiences/Experiences/View Controllers/ExperienceViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ExperienceViewController.swift
+//  Experiences
+//
+//  Created by Ciara Beitel on 11/1/19.
+//  Copyright © 2019 Ciara Beitel. All rights reserved.
+//
+
+import UIKit
+
+class ExperienceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
