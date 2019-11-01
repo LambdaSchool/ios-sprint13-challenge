772e29f888dbafc18886df7167cc716df6296038
Jordan Christensen <jordan.c4922@gmail.com>
2019-11-01 23:37:58 +0800

    Add xcode project, setup storyboard and view controllers

---
+++ Experiences/Experiences/AddExperienceViewController.swift
@@ -0,0 +1,49 @@
+//
+//  AddExperienceViewController.swift
+//  Experiences
+//
+//  Created by Jordan Christensen on 11/1/19.
+//  Copyright © 2019 Mazjap Co. All rights reserved.
+//
+
+import UIKit
+
+class AddExperienceViewController: UIViewController {
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var experienceImageView: UIImageView!
+    @IBOutlet weak var playAudioButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func addImageTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func recordAudio(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func toggleAudio(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func nextButtonTapped(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Experiences/Experiences/MapExperiencesViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MapExperiencesViewController.swift
+//  Experiences
+//
+//  Created by Jordan Christensen on 11/1/19.
+//  Copyright © 2019 Mazjap Co. All rights reserved.
+//
+
+import UIKit
+
+class MapExperiencesViewController: UIViewController {
+
+    @IBOutlet weak var mapView: UIMapKitView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
