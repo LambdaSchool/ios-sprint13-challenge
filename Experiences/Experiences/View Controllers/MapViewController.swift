cf1ecc74cc6e9ba6e5686ad5bfe6a1b183a102b9
Isvvc <lyons@tuta.io>
2019-12-06 10:18:37 -0700

    Show experiences on map view Randomly offset location when creating experience for testing purposes

--- Experiences/Experiences/View Controllers/MapViewController.swift
+++ Experiences/Experiences/View Controllers/MapViewController.swift
@@ -7,26 +7,31 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
     
     var experienceController: ExperienceController?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        mapView.register(MKMarkerAnnotationView.self, forAnnotationViewWithReuseIdentifier: "ExperienceView")
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        loadPosts()
     }
-    */
+    
+    private func loadPosts() {
+        
+        mapView.removeAnnotations(mapView.annotations)
+        mapView.addAnnotations(experienceController?.experiences ?? [])
+        
+        print(mapView.annotations.count)
+    }
 
 }
