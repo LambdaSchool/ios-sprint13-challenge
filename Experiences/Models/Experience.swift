1d495c5c39d69810490df2f5908e2c0c6d67b366
Wilma <kima_92@fastmail.com>
2019-12-06 11:18:53 -0500

    Added Experience Model

---
+++ Experiences/Models/Experience.swift
@@ -0,0 +1,21 @@
+//
+//  Experience.swift
+//  Experiences
+//
+//  Created by macbook on 12/6/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Experience {
+    var audio: URL
+    var video: URL
+    var image: UIImage
+    var title: String
+    var description : String
+    var latitude: String
+    var longitude: String
+    
+}
