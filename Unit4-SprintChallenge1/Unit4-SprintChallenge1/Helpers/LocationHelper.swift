5b66a6a66e773bcfdbfc12db0cf1165732d125cb
Skyler Suzuki <skylersuzuk1@gmail.com>
2020-01-17 11:23:06 -0800

    Added LocationHelper class to get location of user.

---
+++ Unit4-SprintChallenge1/Unit4-SprintChallenge1/Helpers/LocationHelper.swift
@@ -0,0 +1,64 @@
+//
+//  LocationHelper.swift
+//  Unit4-SprintChallenge1
+//
+//  Created by Lambda_School_Loaner_204 on 1/17/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import CoreLocation
+import MapKit
+
+class LocationHelper: NSObject {
+
+    static let shared = LocationHelper()
+
+    private let locationManager = CLLocationManager()
+    var group: DispatchGroup?
+
+    override init() {
+        super.init()
+        locationManager.delegate = self
+
+        requestLocationAuthorization()
+    }
+
+    func requestLocationAuthorization() {
+
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedWhenInUse:
+            return
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        default:
+            break
+        }
+    }
+
+    func getCurrentLocation(completion: @escaping (CLLocationCoordinate2D?) -> Void) {
+        requestLocationAuthorization()
+
+        group = DispatchGroup()
+
+        group?.enter()
+
+        locationManager.requestLocation()
+
+        group?.notify(queue: .main) {
+            let coordinate = self.locationManager.location?.coordinate
+
+            self.group = nil
+            completion(coordinate)
+        }
+    }
+}
+
+extension LocationHelper: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+         group?.leave()
+     }
+
+     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+         print("Failed getting location \(error)")
+     }
+}
