bd780fa3ec86719f8fba8ac7c0c8124a02a49c12
Dan M <djmthedev@gmail.com>
2020-01-17 08:21:12 -0800

    added PropertyKeys

---
+++ MorseWeek13SprintChallenge/MorseWeek13SprintChallenge/Helpers/PropertyKeys.swift
@@ -0,0 +1,13 @@
+//
+//  PropertyKeys.swift
+//  MorseWeek13SprintChallenge
+//
+//  Created by morse on 1/17/20.
+//  Copyright © 2020 morse. All rights reserved.
+//
+
+import Foundation
+
+struct PropertyKeys {
+    static let createExperienceSegue = "ShowCreateExperienceSegue"
+}
