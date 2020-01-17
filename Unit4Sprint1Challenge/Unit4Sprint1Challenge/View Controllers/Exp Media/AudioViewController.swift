aa779bc827359c00d9720beb1a5d7468f11a5e9e
Jon Bash <jonbash@protonmail.com>
2020-01-17 10:39:39 -0800

    set up audioVC

--- Unit4Sprint1Challenge/Unit4Sprint1Challenge/View Controllers/Exp Media/AudioViewController.swift
+++ Unit4Sprint1Challenge/Unit4Sprint1Challenge/View Controllers/Exp Media/AudioViewController.swift
@@ -6,4 +6,70 @@
 //  Copyright © 2020 Jon Bash. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import AVFoundation
+
+protocol AudioVCDelegate: AnyObject {
+    func audioVCDidFinishRecording(withData: Data?)
+}
+
+class AudioViewController: UIViewController {
+
+    var audioData: Data? { audioRecordererControl.audioData }
+
+    weak var delegate: AudioVCDelegate?
+
+    // MARK: - Outlets
+
+    @IBOutlet var audioRecordererControl: AudioRecorderControl!
+    @IBOutlet var audioPlayerControl: AudioPlayerControl!
+    @IBOutlet var reRecordButton: UIButton!
+
+    // MARK: - View Lifecycle / Update
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        audioRecordererControl.delegate = self
+        updateViews()
+    }
+
+    private func updateViews() {
+        let hasRecordedData = (audioData != nil)
+        audioRecordererControl.isHidden = hasRecordedData
+        audioPlayerControl.isHidden = !hasRecordedData
+        reRecordButton.isHidden = !hasRecordedData
+    }
+
+    // MARK: - Actions
+
+    @IBAction func reRecordButtonTapped(_ sender: UIButton) {
+        clearRecordedData()
+    }
+
+    // MARK: - Methods
+
+    private func clearRecordedData() {
+        audioPlayerControl.unloadAudio()
+        updateViews()
+    }
+
+    private func setUpPlayer() {
+        if let data = audioData {
+            audioPlayerControl.loadAudio(from: data)
+        }
+        updateViews()
+    }
+
+}
+
+// MARK: - AudioRecorderControlDelegate
+
+extension AudioViewController: AudioRecorderControlDelegate {
+    func audioRecorderControl(
+        _ recorderControl: AudioRecorderControl,
+        didFinishRecordingSucessfully didFinishRecording: Bool
+    ) {
+        delegate?.audioVCDidFinishRecording(withData: recorderControl.audioData)
+        setUpPlayer()
+    }
+}
