29790c55c10dc5d0e156ed00b1d8b610653c2deb
Brandi <blubrandi0@gmail.com>
2020-01-17 15:34:36 -0700

    Model & Map
    Added Experience Model.  Set Map region and location to be set to Disneyland upon app start.

--- Experiences/Experiences/View Controllers/MapViewViewController.swift
+++ Experiences/Experiences/View Controllers/MapViewViewController.swift
@@ -11,14 +11,25 @@ import MapKit
 import CoreLocation
 
 class MapViewViewController: UIViewController {
-
+    
     @IBOutlet weak var mapView: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let location = CLLocationCoordinate2D(latitude: 33.812794,
+            longitude: -117.9190981)
 
-        // Do any additional setup after loading the view.
+        let span = MKCoordinateSpan(latitudeDelta: 0.008, longitudeDelta: 0.008)
+        let region = MKCoordinateRegion(center: location, span: span)
+            mapView.setRegion(region, animated: true)
+            
     }
+    
+    
+}
+
+extension MapViewViewController: MKMapViewDelegate {
     
 
 }
