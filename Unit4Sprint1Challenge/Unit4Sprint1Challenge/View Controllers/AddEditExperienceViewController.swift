3562ad2de1f829cc662c30c8eab87fbc2d176e14
Jon Bash <jonbash@protonmail.com>
2020-01-17 10:33:15 -0800

    set up expVC

--- Unit4Sprint1Challenge/Unit4Sprint1Challenge/View Controllers/AddEditExperienceViewController.swift
+++ Unit4Sprint1Challenge/Unit4Sprint1Challenge/View Controllers/AddEditExperienceViewController.swift
@@ -6,12 +6,46 @@
 //  Copyright © 2020 Jon Bash. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
-class NewPostViewController: ShiftableViewController {
+class AddEditExperienceViewController: ShiftableViewController {
 
     var experienceController: ExperienceController!
 
     var experience: Experience?
+
+    var imageData: Data?
+    var videoData: Data?
+    var audioData: Data?
+
+    var locationHelper = LocationHelper()
+
+    var hasExperienceData: Bool {
+        (descriptionTextView.text != nil && !descriptionTextView.text.isEmpty) ||
+            imageData != nil ||
+            videoData != nil ||
+            audioData != nil
+    }
     
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var descriptionTextView: UITextView!
+
+    @IBAction func saveButtonTapped(_ sender: Any) {
+        saveExperience()
+    }
+
+    func saveExperience() {
+        guard
+            let title = titleTextField.text, !title.isEmpty,
+            hasExperienceData
+            else { return }
+        let experience = Experience(title: title)
+        experience.imageData = imageData
+        experience.videoData = videoData
+        experience.audioData = audioData
+        experience.geotag = locationHelper.currentLocation
+
+        experienceController.makeModel(experience)
+        navigationController?.popToRootViewController(animated: true)
+    }
 }
