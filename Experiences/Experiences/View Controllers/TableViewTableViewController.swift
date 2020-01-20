63e86168755189800569107e2301ae223b836283
Brandi <blubrandi0@gmail.com>
2020-01-20 16:04:35 -0700

    TableViewTableViewController
    Experiences displaying in TableView

--- Experiences/Experiences/View Controllers/TableViewTableViewController.swift
+++ Experiences/Experiences/View Controllers/TableViewTableViewController.swift
@@ -7,75 +7,42 @@
 //
 
 import UIKit
+import CoreData
 
 class TableViewTableViewController: UITableViewController {
+    
+    var experience: Experience?
+    var experiences: [Experience] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        fetchExperiences()
     }
+    
+    
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return experiences.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ExperienceCell", for: indexPath)
 
-        // Configure the cell...
+        let experience = experiences[indexPath.row]
+        cell.textLabel?.text = experience.title
 
         return cell
     }
-    */
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
 
     /*
     // MARK: - Navigation
@@ -86,5 +53,17 @@ class TableViewTableViewController: UITableViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    func fetchExperiences() {
+        
+        let fetchRequest: NSFetchRequest<Experience> = Experience.fetchRequest()
+        do {
+            experiences = try CoreDataStack.context.fetch(fetchRequest)
+            print(experiences)
+        } catch {
+            print(error)
+        }
+        
+    }
 
 }
