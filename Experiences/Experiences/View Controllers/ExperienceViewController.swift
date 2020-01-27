c1bc408d517848a6eaf01ace0d0c7744c584c2d9
Robert Keffury <bobby.keffury@me.com>
2020-01-27 15:04:38 -0800

    Added Video Functionality.

--- Experiences/Experiences/View Controllers/ExperienceViewController.swift
+++ Experiences/Experiences/View Controllers/ExperienceViewController.swift
@@ -10,12 +10,13 @@ import UIKit
 import CoreImage
 import CoreImage.CIFilterBuiltins
 import Photos
+import AVFoundation
 
 class ExperienceViewController: UIViewController {
     
     //MARK: - Properties
     
-    //Photos
+    // Photos
     private let context = CIContext()
     private let noirFilter = CIFilter.photoEffectNoir()
     var blackWhite: Bool = false
@@ -32,20 +33,47 @@ class ExperienceViewController: UIViewController {
         }
     }
     
+    // Videos
+    lazy private var captureSession = AVCaptureSession()
+    lazy private var fileOutput = AVCaptureMovieFileOutput()
+    var player: AVPlayer!
+    
     //MARK: - Outlets
     
     @IBOutlet weak var titleTextField: UITextField!
+    
+    // Photos
     @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var cameraView: CameraPreviewView!
     @IBOutlet weak var noirButton: UIButton!
     @IBOutlet weak var chooseImageButton: UIButton!
+    
+    // Videos
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var cameraView: CameraPreviewView!
     
     //MARK: - Views
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //Photos
         originalImage = imageView.image
+        
+        //Videos
+        cameraView.videoPlayerView.videoGravity = .resize
+        requestPermissionAndShowCamera()
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTapGesture(_:)))
+               view.addGestureRecognizer(tapGesture)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        captureSession.startRunning()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        captureSession.stopRunning()
     }
     
     //MARK: - Methods
@@ -83,6 +111,144 @@ class ExperienceViewController: UIViewController {
         }
     }
     
+    // Videos
+    private func updateViews() {
+        recordButton.isSelected = fileOutput.isRecording
+    }
+    func playMovie(url: URL) {
+        player = AVPlayer(url: url)
+        let playerLayer = AVPlayerLayer(player: player)
+        var topRect = self.cameraView.bounds
+        topRect.size.height = topRect.height / 2
+        topRect.size.width = topRect.width / 4
+        topRect.origin.y = cameraView.layoutMargins.top
+        
+        playerLayer.frame = topRect
+        cameraView.layer.addSublayer(playerLayer)
+        
+        player.play()
+    }
+    func playRecording() {
+        if let player = player {
+            player.seek(to: CMTime.zero)
+            player.play()
+        }
+    }
+    private func newRecordingURL() -> URL {
+        let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        
+        let formatter = ISO8601DateFormatter()
+        formatter.formatOptions = [.withInternetDateTime]
+        
+        let name = formatter.string(from: Date())
+        let fileURL = documentsDirectory.appendingPathComponent(name).appendingPathExtension("mov")
+        return fileURL
+    }
+    private func setupCamera() {
+        let camera = bestCamera()
+        
+        captureSession.beginConfiguration()
+        guard let cameraInput = try? AVCaptureDeviceInput(device: camera) else {
+            fatalError("Can't create an input from the camera.")
+        }
+        
+        // Video input
+        guard captureSession.canAddInput(cameraInput) else {
+            fatalError("This session can't handle this type of input: \(cameraInput)")
+        }
+        
+        captureSession.addInput(cameraInput)
+        
+        if captureSession.canSetSessionPreset(.hd1920x1080) {
+            captureSession.sessionPreset = .hd1920x1080
+        }
+        
+        // Audio input
+        
+        let microphone = bestAudio()
+        guard let audioInput = try? AVCaptureDeviceInput(device: microphone) else {
+            fatalError("Can't create input from microphone")
+        }
+        guard captureSession.canAddInput(audioInput) else {
+            fatalError("Can't add audio input")
+        }
+        captureSession.addInput(audioInput)
+        
+        
+        //Outputs
+        guard captureSession.canAddOutput(fileOutput) else {
+            fatalError("Cannot record to disk")
+        }
+        captureSession.addOutput(fileOutput)
+        
+        captureSession.commitConfiguration()
+        cameraView.session = captureSession
+    }
+    private func bestCamera() -> AVCaptureDevice {
+        if let device = AVCaptureDevice.default(.builtInUltraWideCamera, for: .video, position: .back) {
+            return device
+        }
+        if let device = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) {
+            return device
+        }
+        
+        fatalError("No cameras on the device (Or you are running it on a simulator)")
+    }
+    private func bestAudio() -> AVCaptureDevice {
+        if let device = AVCaptureDevice.default(for: .audio) {
+            return device
+        }
+        fatalError("No audio")
+    }
+    private func toggleRecord() {
+        if fileOutput.isRecording {
+            fileOutput.stopRecording()
+        } else {
+            fileOutput.startRecording(to: newRecordingURL(), recordingDelegate: self)
+        }
+    }
+    @objc func handleTapGesture(_ tapGesture: UITapGestureRecognizer) {
+        print("tap")
+        switch tapGesture.state {
+        case .ended:
+            playRecording()
+        default:
+            print("Handled other states: \(tapGesture)")
+        }
+    }
+    private func requestPermissionAndShowCamera() {
+        let status = AVCaptureDevice.authorizationStatus(for: .video)
+        
+        switch status {
+            
+        case .notDetermined:
+            // First time user.
+            requestPermission()
+        case .restricted:
+            // parental controls
+            fatalError("Video is disabled for parental controls")
+        case .denied:
+            // No
+            fatalError("Tell user they need to enable Privacy for Video/Camera/Microphone")
+        case .authorized:
+            // Yes
+            setupCamera()
+        default:
+            fatalError("A new status was added that we need to handle")
+        }
+    }
+    private func requestPermission() {
+        AVCaptureDevice.requestAccess(for: .video) { (granted) in
+            if granted {
+                DispatchQueue.main.async {
+                    self.setupCamera()
+                }
+            } else {
+                fatalError("Tell user they need to enable Privacy for Video/Camera/Microphone")
+            }
+        }
+    }
+    
     //MARK: - Actions
     
     @IBAction func saveTapped(_ sender: Any) {
@@ -100,6 +266,7 @@ class ExperienceViewController: UIViewController {
     
     // Videos
     @IBAction func recordTapped(_ sender: Any) {
+        toggleRecord()
     }
     
     
@@ -128,4 +295,21 @@ extension ExperienceViewController: UIImagePickerControllerDelegate, UINavigatio
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         picker.dismiss(animated: true, completion: nil)
     }
+}
+
+extension ExperienceViewController: AVCaptureFileOutputRecordingDelegate {
+    
+    func fileOutput(_ output: AVCaptureFileOutput, didStartRecordingTo fileURL: URL, from connections: [AVCaptureConnection]) {
+        updateViews()
+    }
+    
+    func fileOutput(_ output: AVCaptureFileOutput, didFinishRecordingTo outputFileURL: URL, from connections: [AVCaptureConnection], error: Error?) {
+        if let error = error {
+            print("Error saving video: \(error)")
+        }
+        print("Video: \(outputFileURL.path)")
+        updateViews()
+        playMovie(url: outputFileURL)
+    }
+    
 }
