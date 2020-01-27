8a58c2f5b68fc98485ba6c0f8f7066806c5c9beb
Vici Shaweddy <vshaweddy@gmail.com>
2020-01-26 20:45:01 -0500

    filter image works

--- Experiences/Experiences/View Controllers/ImageViewController.swift
+++ Experiences/Experiences/View Controllers/ImageViewController.swift
@@ -11,6 +11,123 @@ import Photos
 import CoreImage.CIFilterBuiltins
 
 class ImageViewController: UIViewController {
+
+    // MARK: - Outlets and Properties
+    
+    @IBOutlet weak var saturationSlider: UISlider!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var saturationStackView: UIStackView!
+    
     var post: Post?
-    var originalImage: UIImage?
+    
+    var originalImage: UIImage? {
+        didSet {
+            guard let originalImage = originalImage else { return }
+            
+            var scaledSize = imageView.bounds.size
+            let scale = CGFloat(0.5)
+            
+            scaledSize = CGSize(width: scaledSize.width * scale,
+                                height: scaledSize.height * scale)
+            
+            let scaledUIImage = originalImage.imageByScaling(toSize: scaledSize)
+            guard let scaledCGImage = scaledUIImage?.cgImage else { return }
+            
+            scaledImage = CIImage(cgImage: scaledCGImage)
+        }
+    }
+    
+    var scaledImage: CIImage? {
+        didSet {
+            updateImage()
+        }
+    }
+    
+    private let colorControlsFilter = CIFilter.colorControls()
+    private let context = CIContext(options: nil)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        originalImage = imageView.image
+        
+        // hide the filter
+        self.saturationStackView.isHidden = true
+    }
+    
+    // MARK: - Functions
+    
+    private func image(byFiltering inputImage: CIImage) -> UIImage {
+
+        colorControlsFilter.inputImage = inputImage
+        colorControlsFilter.saturation = saturationSlider.value
+        
+        guard let outputImage = colorControlsFilter.outputImage else { return UIImage(ciImage: inputImage) }
+        
+        guard let renderedImage = context.createCGImage(outputImage, from: outputImage.extent) else { return UIImage(ciImage: inputImage) }
+        
+        return UIImage(cgImage: renderedImage)
+    }
+    
+    
+    private func updateImage() {
+        if let scaledImage = scaledImage {
+            imageView.image = image(byFiltering: scaledImage)
+        } else {
+            imageView.image = nil
+        }
+    }
+    
+    func presentImagePickerController () {
+        guard UIImagePickerController.isSourceTypeAvailable(.photoLibrary) else
+        {
+            // make as an alert
+            print("The photo library isn't available.")
+            return
+        }
+        
+        let imagePicker = UIImagePickerController()
+        
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.delegate = self
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    // MARK: Actions
+    
+    @IBAction func addImageFromLibraryPressed(_ sender: Any) {
+        self.presentImagePickerController()
+    }
+    
+    @IBAction func savePressed(_ sender: Any) {
+    }
+    
+    // MARK: Slider events
+    
+    @IBAction func saturationChanged(_ sender: Any) {
+        self.updateImage()
+    }
+}
+
+extension ImageViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    // if user cancels the action so dismiss
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        // check if there is original image or not
+        if let image = info[.editedImage] as? UIImage {
+            originalImage = image
+        } else if let image = info[.originalImage] as? UIImage {
+            originalImage = image
+        }
+        
+        // show saturation filter
+        self.saturationStackView.isHidden = false
+        
+        picker.dismiss(animated: true, completion: nil)
+    }
 }
