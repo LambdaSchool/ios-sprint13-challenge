ab1c8fb499206fc0ca10a3178b714d4563b21d6c
Chad Rutherford <chad_rutherford@outlook.com>
2020-02-14 12:04:07 -0500

    Created Custom Table View Cell and wired up TableView

--- Experiences/Experiences/Controller/DocumentsTableViewController.swift
+++ Experiences/Experiences/Controller/DocumentsTableViewController.swift
@@ -6,11 +6,76 @@
 //  Copyright © 2020 Chad Rutherford. All rights reserved.
 //
 
+import CoreData
 import UIKit
 
 class DocumentsTableViewController: UITableViewController {
     
+    private lazy var fetchedResultsController: NSFetchedResultsController<Experience> = {
+        let fetchRequest: NSFetchRequest<Experience> = Experience.fetchRequest()
+        let moc = CoreDataStack.shared.mainContext
+        var frc = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: moc, sectionNameKeyPath: nil, cacheName: nil)
+        try? frc.performFetch()
+        return frc
+    }()
+    
     override func viewDidLoad() {
-        view.backgroundColor = .systemGreen
+        setupTableView()
+    }
+    
+    private func setupTableView() {
+        tableView.register(DocumentCell.self, forCellReuseIdentifier: DocumentCell.reuseID)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fetchedResultsController.fetchedObjects?.count ?? 0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: DocumentCell.reuseID, for: indexPath) as? DocumentCell else { return UITableViewCell()}
+        let experience = fetchedResultsController.object(at: indexPath)
+        cell.experience = experience
+        return cell
+    }
+}
+
+extension DocumentsTableViewController: NSFetchedResultsControllerDelegate {
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
+        switch type {
+        case .insert:
+            tableView.insertSections(IndexSet(integer: sectionIndex), with: .automatic)
+        case .delete:
+            tableView.deleteSections(IndexSet(integer: sectionIndex), with: .automatic)
+        default:
+            break
+        }
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        switch type {
+        case .insert:
+            guard let newIndexPath = newIndexPath else { return }
+            tableView.insertRows(at: [newIndexPath], with: .automatic)
+        case .delete:
+            guard let indexPath = indexPath else { return }
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        case .update:
+            guard let indexPath = indexPath else { return }
+            tableView.reloadRows(at: [indexPath], with: .automatic)
+        case .move:
+            guard let fromIndexPath = indexPath, let toIndexPath = newIndexPath else { return }
+            tableView.deleteRows(at: [fromIndexPath], with: .automatic)
+            tableView.insertRows(at: [toIndexPath], with: .automatic)
+        @unknown default:
+            break
+        }
     }
 }
---
+++ Experiences/Experiences/View/Cells/DocumentCell.swift
@@ -0,0 +1,91 @@
+//
+//  DocumentCell.swift
+//  Experiences
+//
+//  Created by Chad Rutherford on 2/14/20.
+//  Copyright © 2020 Chad Rutherford. All rights reserved.
+//
+
+import UIKit
+
+class DocumentCell: UITableViewCell {
+    static let reuseID = "DocumentCell"
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.preferredFont(forTextStyle: .title1)
+        return label
+    }()
+    let timestampLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.preferredFont(forTextStyle: .body)
+        return label
+    }()
+    let documentImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        imageView.layer.cornerRadius = 8
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.timeStyle = .short
+        formatter.dateStyle = .short
+        return formatter
+    }()
+    
+    var experience: Experience? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configure()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func updateViews() {
+        guard let experience = experience,
+            let image = experience.image,
+            let imageURL = URL(string: image),
+            let data = try? Data(contentsOf: imageURL),
+            let timestamp = experience.timestamp
+            else { return }
+        documentImageView.image = UIImage(data: data)
+        titleLabel.text = experience.title
+        timestampLabel.text = dateFormatter.string(from: timestamp)
+        
+    }
+    
+    private func configure() {
+        addSubview(documentImageView)
+        addSubview(titleLabel)
+        addSubview(timestampLabel)
+        let padding: CGFloat = 12
+        NSLayoutConstraint.activate([
+            documentImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            documentImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: padding),
+            documentImageView.heightAnchor.constraint(equalToConstant: 120),
+            documentImageView.widthAnchor.constraint(equalTo: documentImageView.heightAnchor),
+            
+            titleLabel.topAnchor.constraint(equalTo: documentImageView.topAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: documentImageView.trailingAnchor, constant: 24),
+            titleLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -padding),
+            titleLabel.heightAnchor.constraint(equalToConstant: 40),
+            
+            timestampLabel.bottomAnchor.constraint(equalTo: documentImageView.bottomAnchor),
+            timestampLabel.leadingAnchor.constraint(equalTo: documentImageView.trailingAnchor, constant: 24),
+            timestampLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -padding),
+            timestampLabel.heightAnchor.constraint(equalToConstant: 40),
+        ])
+    }
+}
