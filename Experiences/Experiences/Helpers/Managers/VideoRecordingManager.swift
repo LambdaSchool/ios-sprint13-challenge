fea507bc80df4afbccffc559dc99741bef3e9a8c
Chad Rutherford <chad_rutherford@outlook.com>
2020-02-16 12:16:32 -0500

    Added Detail View Controller and refactored VideoRecording into manager

---
+++ Experiences/Experiences/Helpers/Managers/VideoRecordingManager.swift
@@ -0,0 +1,9 @@
+//
+//  VideoRecordingManager.swift
+//  Experiences
+//
+//  Created by Chad Rutherford on 2/16/20.
+//  Copyright © 2020 Chad Rutherford. All rights reserved.
+//
+
+import Foundation
