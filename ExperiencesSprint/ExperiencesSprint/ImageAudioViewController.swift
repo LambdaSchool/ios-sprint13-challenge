3685b8d44e4b0a905caa78b0c64c688d924829ec
Jorge Alvarez <jorgelalvarez52@gmail.com>
2020-03-13 09:49:22 -0700

    image filter and library work, just not added to experience yet

--- ExperiencesSprint/ExperiencesSprint/ImageAudioViewController.swift
+++ ExperiencesSprint/ExperiencesSprint/ImageAudioViewController.swift
@@ -7,22 +7,49 @@
 //
 
 import UIKit
+import CoreImage
+import Photos
+import CoreImage.CIFilterBuiltins // iOS 13 *
+import AVFoundation
 
 class ImageAudioViewController: UIViewController {
+    
+    // MARK: - Properties
 
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var recordAudioButton: UIButton!
     @IBOutlet weak var addImageButton: UIButton!
     
+    private var originalImage: UIImage? {
+        didSet {
+            guard let originalImage = originalImage else {return}
+            var scaledSize = imageView.bounds.size
+            let scale = UIScreen.main.scale // 1x 2x or 3x or more
+            scaledSize = CGSize(width: scaledSize.width * scale, height: scaledSize.height * scale)
+            scaledImage = originalImage.imageByScaling(toSize: scaledSize)
+        }
+    }
+    
+    private var scaledImage: UIImage? {
+        didSet {
+            updateImage()
+        }
+    }
+    
+    /// Allows us to render the image (like an oven for baking bread)
+    private let context = CIContext(options: nil)
+    
     @IBAction func addImageTapped(_ sender: UIButton) {
         print("addImageTapped")
+        presentImagePickerController()
     }
     
     @IBAction func recordAudioTapped(_ sender: UIButton) {
         print("recordAudioTapped")
     }
     
+    // MARK: - View Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,7 +57,74 @@ class ImageAudioViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    func filterImage(_ image: UIImage) -> UIImage? {
+        // UIImage -> CGImage -> CIImage
+        guard let cgImage = image.cgImage else { return nil }
+        let ciImage = CIImage(cgImage: cgImage)
+        
+        /// Should these be properties???
+        let filter = CIFilter.colorControls()
+        
+        /// Input
+        filter.inputImage = ciImage
+        filter.saturation = 0 // black and white
+        
+        /// Output
+        guard let outputCIImage = filter.outputImage else { return nil }
+        
+        // CIImage -> CGImage -> UIImage
+        // Render the image (apply the filter to the image). Baking the cookies in the oven
+        guard let outputCGImage = context.createCGImage(outputCIImage, from: CGRect(origin: .zero, size: image.size)) else { return nil }
+        return UIImage(cgImage: outputCGImage)
+    }
+    
+    private func updateImage() {
+        if let scaledImage = scaledImage {
+            imageView.image = filterImage(scaledImage)
+        } else {
+            imageView.image = nil // allows us to clear out the image
+        }
+    }
+    
+    private func presentImagePickerController() {
+        guard UIImagePickerController.isSourceTypeAvailable(.photoLibrary) else {
+            print("Error: the photo library is unavailable")
+            return
+        }
+        
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.delegate = self
+        present(imagePicker, animated: true)
+    }
 
+    private func savePhoto() {
+        guard let originalImage = originalImage else { return }
+        guard let processedImage = self.filterImage(originalImage.flattened) else { return }
+        PHPhotoLibrary.requestAuthorization { (status) in
+            guard status == .authorized else { return }
+            // Let the library know we are going to make changes
+            PHPhotoLibrary.shared().performChanges({
+                // Make a new photo creation request
+                PHAssetCreationRequest.creationRequestForAsset(from: processedImage)
+            }, completionHandler: { (success, error) in
+                if let error = error {
+                    NSLog("Error saving photo: \(error)")
+                    return
+                }
+                DispatchQueue.main.async {
+                    print("Saved image to Photo Library")
+                    let alertController = UIAlertController(title: "Image Saved",
+                                                            message: "Saved image to photo library",
+                                                            preferredStyle: .alert)
+                    let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+                    alertController.addAction(alertAction)
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            })
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
@@ -41,4 +135,55 @@ class ImageAudioViewController: UIViewController {
     }
     */
 
+}
+
+// MARK: - Extensions
+
+/// For accessing the photo library
+extension ImageAudioViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        print("Picked Image")
+        
+        if let image = info[.originalImage] as? UIImage {
+            originalImage = image
+        }
+        picker.dismiss(animated: true)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        print("Cancel")
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+}
+
+/// So images don't flip
+extension UIImage {
+    
+    /// Resize the image to a max dimension from size parameter
+    func imageByScaling(toSize size: CGSize) -> UIImage? {
+        guard size.width > 0 && size.height > 0 else { return nil }
+        
+        let originalAspectRatio = self.size.width/self.size.height
+        var correctedSize = size
+        
+        if correctedSize.width > correctedSize.width*originalAspectRatio {
+            correctedSize.width = correctedSize.width*originalAspectRatio
+        } else {
+            correctedSize.height = correctedSize.height/originalAspectRatio
+        }
+        
+        return UIGraphicsImageRenderer(size: correctedSize, format: imageRendererFormat).image { context in
+            draw(in: CGRect(origin: .zero, size: correctedSize))
+        }
+    }
+    
+    /// Renders the image if the pixel data was rotated due to orientation of camera
+    var flattened: UIImage {
+        if imageOrientation == .up { return self }
+        return UIGraphicsImageRenderer(size: size, format: imageRendererFormat).image { context in
+            draw(at: .zero)
+        }
+    }
 }
