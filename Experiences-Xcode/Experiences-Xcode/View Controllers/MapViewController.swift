d0211824ed644d9ede098b3b009abd6b35a8d77a
AustinPotts <pottsevolvements@gmail.com>
2020-03-13 15:55:31 -0400

    Configured MapView + VideoView Controllers

--- Experiences-Xcode/Experiences-Xcode/View Controllers/MapViewController.swift
+++ Experiences-Xcode/Experiences-Xcode/View Controllers/MapViewController.swift
@@ -12,21 +12,24 @@ import MapKit
 class MapViewController: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
+    var experienceController: ExperienceController?
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        mapView.register(MKMarkerAnnotationView.self, forAnnotationViewWithReuseIdentifier: "ExperienceView")
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        loadExperiences()
     }
-    */
+    
+    private func loadExperiences() {
+        mapView.removeAnnotations(mapView.annotations)
+        mapView.addAnnotations(experienceController?.experiences ?? [])
+        
+        print(mapView.annotations.count)
+    }
 
 }
