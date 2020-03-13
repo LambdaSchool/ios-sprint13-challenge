42f144b16fcd30a4c05b05fe39222ca20a596ffe
Jorge Alvarez <jorgelalvarez52@gmail.com>
2020-03-13 08:50:15 -0700

    scenes hooked up

---
+++ ExperiencesSprint/ExperiencesSprint/ImageAudioViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ImageAudioViewController.swift
+//  ExperiencesSprint
+//
+//  Created by Jorge Alvarez on 3/13/20.
+//  Copyright © 2020 Jorge Alvarez. All rights reserved.
+//
+
+import UIKit
+
+class ImageAudioViewController: UIViewController {
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var recordAudioButton: UIButton!
+    @IBOutlet weak var addImageButton: UIButton!
+    
+    @IBAction func addImageTapped(_ sender: UIButton) {
+        print("addImageTapped")
+    }
+    
+    @IBAction func recordAudioTapped(_ sender: UIButton) {
+        print("recordAudioTapped")
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ExperiencesSprint/ExperiencesSprint/VideoViewController.swift
@@ -0,0 +1,38 @@
+//
+//  VideoViewController.swift
+//  ExperiencesSprint
+//
+//  Created by Jorge Alvarez on 3/13/20.
+//  Copyright © 2020 Jorge Alvarez. All rights reserved.
+//
+
+import UIKit
+
+class VideoViewController: UIViewController {
+
+    @IBOutlet weak var cameraView: UIView!
+    
+    @IBOutlet weak var recordVideoButton: UIButton!
+    
+    @IBAction func recordVideoTapped(_ sender: UIButton) {
+        print("recordVideoTapped")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
