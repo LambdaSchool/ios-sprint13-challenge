3bb701b036406f8737854f4df40e4ad017b8193a
dillonp23 <dillonp.lambda@gmail.com>
2020-03-22 20:18:25 -0700

    Added ability to record audio comment. Audio is converted to data upon recording completion. Audio will playback immediately after recording stops.

--- Experiences/Experiences/View Controllers/AddExperienceViewController.swift
+++ Experiences/Experiences/View Controllers/AddExperienceViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 import Photos
 import CoreImage
 import CoreImage.CIFilterBuiltins
+import AVFoundation
 
 class AddExperienceViewController: UIViewController {
     
@@ -22,37 +23,155 @@ class AddExperienceViewController: UIViewController {
     @IBOutlet weak var recordAudioButton: UIButton!
     
     
-    // MARK: - Properties
-    
-    var originalImage: UIImage? {
-        didSet {
-            imageView.image = makeImageBW(byFiltering: originalImage!)
-        }
-    }
-    
-    
     //MARK: - View Set-Up
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+         audioPlayer = AVAudioPlayer()
     }
+    
+    private func updateViews() {
+        recordAudioButton.isSelected = isRecording
+    }
+    
+    
+    // MARK: - Image Properties
+    
+    var originalImage: UIImage? {
+        didSet {
+            imageView.image = makeImageBW(byFiltering: originalImage!)
+            imageData = imageView.image?.jpegData(compressionQuality: 0.1)
+        }
+    }
+    
+    var imageData: Data?
+    
+    
+    //MARK: - Audio Properties
+    
+    var audioPlayer: AVAudioPlayer? {
+        didSet {
+            guard let audioPlayer = audioPlayer else { return }
+            audioPlayer.delegate = self
+            updateViews()
+        }
+    }
+    
+    var audioRecorder: AVAudioRecorder?
+    var recordingURL: URL?
+    
+    var isRecording: Bool {
+        audioRecorder?.isRecording ?? false
+    }
+    
+    var audioData: Data?
+    
+    
+    //MARK: - Audio Player & Recorder Methods
+    
+    func createNewRecordingURL() -> URL {
+        let documents = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        
+        let name = ISO8601DateFormatter.string(from: Date(), timeZone: .current, formatOptions: .withInternetDateTime)
+        let file = documents.appendingPathComponent(name, isDirectory: false).appendingPathExtension("caf")
+        
+    return file
+    }
+    
+    func prepareAudioSession() throws {
+        let session = AVAudioSession.sharedInstance()
+        try session.setCategory(.playAndRecord, options: [.defaultToSpeaker])
+        try session.setActive(true, options: []) // can fail if on a phone call, for instance
+    }
+    
+    func requestPermissionOrStartRecording() {
+        switch AVAudioSession.sharedInstance().recordPermission {
+        case .undetermined:
+            AVAudioSession.sharedInstance().requestRecordPermission { granted in
+                guard granted == true else {
+                    print("We need microphone access")
+                    return
+                }
+                
+                print("Recording permission has been granted!")
+                // NOTE: Invite the user to tap record again, since we just interrupted them, and they may not have been ready to record
+            }
+        case .denied:
+            print("Microphone access has been blocked.")
+            
+            let alertController = UIAlertController(title: "Microphone Access Denied", message: "Please allow this app to access your Microphone.", preferredStyle: .alert)
+            
+            alertController.addAction(UIAlertAction(title: "Open Settings", style: .default) { (_) in
+                UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
+            })
+            
+            alertController.addAction(UIAlertAction(title: "Cancel", style: .default, handler: nil))
+            
+            present(alertController, animated: true, completion: nil)
+        case .granted:
+            startRecording()
+        @unknown default:
+            break
+        }
+    }
+    
+        
+    func startRecording() {
+        // try to prepare audio session
+        do {
+            try prepareAudioSession()
+        } catch {
+            print("We could not record audio: \(error)")
+            return
+        }
+        
+        recordingURL = createNewRecordingURL()
+        
+        let format = AVAudioFormat(standardFormatWithSampleRate: 44_100, channels: 1)!
+        
+        do {
+            audioRecorder = try AVAudioRecorder(url: recordingURL!, format: format)
+            audioRecorder?.record()
+            audioRecorder?.delegate = self
+            updateViews()
+        } catch {
+            preconditionFailure("The audio recorder could not be created with \(recordingURL!) and \(format)")
+        }
+    }
+    
+    func stopRecording() {
+        audioRecorder?.stop()
+        updateViews()
+    }
+    
+    private func play() {
+        do {
+            try prepareAudioSession()
+            audioPlayer?.play()
+            updateViews()
+        } catch {
+            print("Error preparing audio session: \(error)")
+            return
+        }
+    }
+
     
     // MARK: - Image Picker Methods
     
     private func presentImagePickerController() {
-        
         guard UIImagePickerController.isSourceTypeAvailable(.photoLibrary) else {
             preconditionFailure("Photo Library not available")
         }
-        
-        let imagePicker = UIImagePickerController()
-        
-        imagePicker.delegate = self
-        
-        imagePicker.sourceType = .photoLibrary
+        DispatchQueue.main.async {
+            let imagePicker = UIImagePickerController()
+            
+            imagePicker.delegate = self
+            
+            imagePicker.sourceType = .photoLibrary
 
-        present(imagePicker, animated: true, completion: nil)
+            self.present(imagePicker, animated: true, completion: nil)
+        }
     }
     
     // MARK: - Image Filtering Methods
@@ -119,7 +238,7 @@ class AddExperienceViewController: UIViewController {
             let alert = UIAlertController(title: "Error", message: "In order to access the photo library, you must allow this application access to it.", preferredStyle: .alert)
             self.present(alert, animated: true)
         @unknown default:
-            return
+            break
         }
         presentImagePickerController()
     }
@@ -130,6 +249,12 @@ class AddExperienceViewController: UIViewController {
     }
     
     @IBAction func recordAudioButtonTapped(_ sender: Any) {
+        
+        if isRecording {
+            stopRecording()
+        } else {
+            requestPermissionOrStartRecording()
+        }
     }
     
 }
@@ -153,4 +278,40 @@ extension AddExperienceViewController: UIImagePickerControllerDelegate, UINaviga
         picker.dismiss(animated: true, completion: nil)
     }
     
+}
+
+
+//MARK: - Audio Player & Recorder Delegate Extension
+
+extension AddExperienceViewController: AVAudioPlayerDelegate {
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        updateViews()
+    }
+    
+    func audioPlayerDecodeErrorDidOccur(_ player: AVAudioPlayer, error: Error?) {
+        if let error = error {
+            print("Error decoding audio: \(error)")
+        }
+    }
+}
+
+extension AddExperienceViewController: AVAudioRecorderDelegate {
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        if let recordingURL = recordingURL {
+            audioPlayer = try? AVAudioPlayer(contentsOf: recordingURL)
+            do {
+                audioData = try Data(contentsOf: recordingURL)
+                play()
+            } catch {
+                print("Error getting audio data: \(error)")
+            }
+        }
+        audioRecorder = nil
+    }
+    
+    func audioRecorderEncodeErrorDidOccur(_ recorder: AVAudioRecorder, error: Error?) {
+        if let error = error {
+            print("Audio recorder error: \(error)")
+        }
+    }
 }
