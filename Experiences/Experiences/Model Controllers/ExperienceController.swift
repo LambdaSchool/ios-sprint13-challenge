86f1fc0794e0ec09269a88c8baa81b1b3fb59a63
dillonp23 <dillonp.lambda@gmail.com>
2020-03-22 21:49:43 -0700

    Added experience controller to manage experiences

---
+++ Experiences/Experiences/Model Controllers/ExperienceController.swift
@@ -0,0 +1,21 @@
+//
+//  ExperienceController.swift
+//  Experiences
+//
+//  Created by Dillon P on 3/22/20.
+//  Copyright © 2020 Lambda iOSPT3. All rights reserved.
+//
+
+import Foundation
+import MapKit
+import CoreLocation
+
+class ExperienceController {
+    
+    var experiences: [Experience] = []
+    
+    
+    func addNewExperience(title: String, imageData: Data, audioData: Data, videoData: Data, location: CLLocation = CLLocation()) {
+        
+    }
+}
