562c6c80a5160da5b604a7423a183bfdc3bb9a55
dillonp23 <dillonp.lambda@gmail.com>
2020-03-22 21:50:43 -0700

    Update MapViewController to display users current location and to instantiate and pass a experience controller. Updated location privacy usage in info.plist

--- Experiences/Experiences/View Controllers/MapViewController.swift
+++ Experiences/Experiences/View Controllers/MapViewController.swift
@@ -7,24 +7,58 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
+
 
 class MapViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var newExperienceButton: UIButton!
+    
+    private var userTrackingButton: MKUserTrackingButton!
+    private let locationManager = CLLocationManager()
+    
+    var experienceController = ExperienceController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+         locationManager.requestWhenInUseAuthorization()
+               userTrackingButton = MKUserTrackingButton(mapView: mapView)
+               
+               userTrackingButton.translatesAutoresizingMaskIntoConstraints = false
+               
+               view.addSubview(userTrackingButton)
+               
+               userTrackingButton.leadingAnchor.constraint(equalTo: mapView.leadingAnchor, constant: 20).isActive = true
+               userTrackingButton.bottomAnchor.constraint(equalTo: mapView.bottomAnchor, constant: -20).isActive = true
     }
     
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "AddNewExperienceSegue" {
+            if let addExperienceVC = segue.destination as? AddExperienceViewController {
+                addExperienceVC.experienceController = experienceController
+            }
+        }
     }
-    */
+    
+    @IBAction func newExperienceTapped(_ sender: Any) {
+        self.performSegue(withIdentifier: "AddNewExperienceSegue", sender: self)
+    }
+    
+}
 
+
+extension MapViewController: MKMapViewDelegate {
+    
+}
+
+extension MapViewController: CLLocationManagerDelegate {
+    
 }
