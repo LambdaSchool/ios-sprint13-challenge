08a32ffd9a4037cf7cef9dfddac09542973c8f6a
dillonp23 <dillonp.lambda@gmail.com>
2020-03-22 20:21:22 -0700

    Add camera view class for video capture and player layer

---
+++ Experiences/Experiences/View Controllers/CameraViewController.swift
@@ -0,0 +1,39 @@
+//
+//  CameraViewController.swift
+//  Experiences
+//
+//  Created by Dillon P on 3/22/20.
+//  Copyright © 2020 Lambda iOSPT3. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class CameraViewController: UIViewController {
+    
+    
+    // MARK: - Model Object Properties
+    
+    var imageData: Data?
+    var audioData: Data?
+    var videoData: Data?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
