b2c0f2d9ed606f6cfca98a01a3e44fa982c850d5
dillonp23 <dillonp.lambda@gmail.com>
2020-03-22 21:03:12 -0700

    Add experience Model.

---
+++ Experiences/Experiences/Models/Experience.swift
@@ -0,0 +1,17 @@
+//
+//  Experience.swift
+//  Experiences
+//
+//  Created by Dillon P on 3/22/20.
+//  Copyright © 2020 Lambda iOSPT3. All rights reserved.
+//
+
+import Foundation
+
+struct Experience {
+    let title: String
+    let imageData: Data?
+    let audioData: Data?
+    let videoData: Data?
+}
+
