6d7cdf1738d83109243f4ac40d67cc0b8660f9ea
Enrique Gongora <EGTech24@gmail.com>
2020-04-10 10:05:22 -0700

    Map View Controller working.

--- Experiences/Experiences/View Controller/MapKitViewController.swift
+++ Experiences/Experiences/View Controller/MapKitViewController.swift
@@ -7,24 +7,100 @@
 //
 
 import UIKit
+import MapKit
 
 class MapKitViewController: UIViewController {
-
+    
+    // MARK: - Properties
+    let locationManager = CLLocationManager()
+    var coordinate = CLLocationCoordinate2D()
+    let regionRadius: CLLocationDistance = 4000
+    var experiences: [Experience] = [Experience(title: "Winds Howling", image: UIImage(named: "Geralt"), video: nil, audio: nil, coordinate: CLLocationCoordinate2D(latitude: 37.569470, longitude: -122.314700))]
+    // MARK: - IBOutlets
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var getLocationButton: UIButton!
+    
+    // MARK: - View LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        locationManager.delegate = self
+        mapView.delegate = self
+        mapView.register(MKMarkerAnnotationView.self, forAnnotationViewWithReuseIdentifier: "Experience")
+        NotificationCenter.default.addObserver(forName: .saveTapped, object: nil, queue: nil) { (catchNotification) in
+            let exp = catchNotification.userInfo?[experienceSaved]
+            if let savedExperience = exp as? Experience {
+                self.experiences.append(savedExperience)
+                self.mapView.addAnnotation(savedExperience)
+            }
+        }
+        self.mapView.addAnnotations(self.experiences)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        checkLocationAuthorizationStatus()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.distanceFilter = kCLDistanceFilterNone
+        locationManager.startUpdatingLocation()
+        if let coords = mapView.userLocation.location?.coordinate {
+            self.coordinate = coords
+            let initialLocation = CLLocation(latitude: coords.latitude, longitude: coords.longitude)
+            centerMapOnLocation(location: initialLocation)
+        }
     }
-    */
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    // MARK: - Functions
+    func checkLocationAuthorizationStatus() {
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            mapView.showsUserLocation = true
+            if let coords = mapView.userLocation.location?.coordinate {
+                mapView.setCenter(coords, animated: true)
+            }
+        } else {
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+    
+    @objc func addLitExperience(_ notification: Notification) {
+        let experience = notification.userInfo?["experience"]
+        experiences.append(experience as! Experience)
+    }
+    
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "AddExperienceSegue" {
+            guard let newExperienceVC = segue.destination as? NewExperienceViewController else { return }
+//            newExperienceVC.coordinate = coordinate
+        }
+    }
+}
 
+extension MapKitViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let experience = annotation as? Experience
+        guard let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "Experience", for: annotation) as? MKMarkerAnnotationView else {
+            fatalError("Missing a registered map annotation view")
+        }
+        annotationView.glyphImage = UIImage(systemName: "flame")
+        annotationView.glyphTintColor = UIColor.orange
+        annotationView.canShowCallout = true
+        let detailView = ExperienceDetailView()
+        detailView.experience = experience
+        annotationView.detailCalloutAccessoryView = detailView
+        
+        return annotationView
+    }
+}
+
+extension MapKitViewController: CLLocationManagerDelegate {
+    
 }
