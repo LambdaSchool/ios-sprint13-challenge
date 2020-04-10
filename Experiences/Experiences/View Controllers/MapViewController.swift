6e1698d94d1df532b452a6346d8fbbe40e344520
Keri Levesque <kerianne166@gmail.com>
2020-04-10 14:33:44 -0400

    pin drop working

--- Experiences/Experiences/View Controllers/MapViewController.swift
+++ Experiences/Experiences/View Controllers/MapViewController.swift
@@ -13,27 +13,107 @@ import CoreLocation
 class MapViewController: UIViewController {
 
     //MARK: Properties
-    
+    let locationManager = CLLocationManager()
+    var experienceController = ExperienceController()
+    var touchLocation: CLLocationCoordinate2D?
     
     //MARK: Outlets
     @IBOutlet weak var map: MKMapView!
     
     //MARK: View Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupMapView()
 
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        map.addAnnotations(experienceController.experiences)
+        map.showAnnotations(experienceController.experiences, animated: true)
     }
-    */
 
+    // MARK: Methods
+    private func centerMapOnLocation() {
+        if let mapCenter = locationManager.location?.coordinate {
+            let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+            let region = MKCoordinateRegion(center: mapCenter, span: span)
+            map.setRegion(region, animated: false)
+        }
+    }
+    private func setupMapView () {
+           map.delegate = self
+           map.register(MKMarkerAnnotationView.self, forAnnotationViewWithReuseIdentifier: "ExperienceView")
+           
+           locationManager.delegate = self
+           locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+           let status = CLLocationManager.authorizationStatus()
+           switch status {
+           case .notDetermined:
+               locationManager.requestWhenInUseAuthorization()
+           case .authorizedWhenInUse:
+               locationManager.requestLocation()
+               centerMapOnLocation()
+           default:
+               break
+           }
+       }
+    
+    //MARK: Actions
+    @IBAction func dropPin(_ sender: UILongPressGestureRecognizer) {
+           if sender.state == .began {
+               let alert = UIAlertController(title: "Create an experience", message: "Do you want to drop a pin?", preferredStyle: .actionSheet)
+               let actionOK = UIAlertAction(title: "Yes", style: .default) { (_) in
+                   let touchLocation = sender.location(in: self.map)
+                   let locationCoordinate = self.map.convert(touchLocation, toCoordinateFrom: self.map)
+                   self.touchLocation = locationCoordinate
+                   self.performSegue(withIdentifier: "AddExperienceSegue", sender: self)
+               }
+               let actionCancel = UIAlertAction(title: "No", style: .destructive, handler: nil)
+               alert.addAction(actionOK)
+               alert.addAction(actionCancel)
+               present(alert, animated: true, completion: nil)
+           }
+       }
+    
+    
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let vc = segue.destination as? ExperienceTableViewController else { return }
+           vc.experienceController = self.experienceController
+           switch segue.identifier {
+           case "AddExperienceSegue":
+               vc.coordinate =  touchLocation 
+           case "ViewExperienceSegue":
+               guard let experience = sender as? Experience else { return }
+               vc.experience = experience
+               break
+           default:
+               break
+           }
+       }
+}
+// MARK: Extensions
+
+extension MapViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        guard let experience = view.annotation as? Experience else { return }
+        self.performSegue(withIdentifier: "ViewExperienceSegue", sender: experience)
+    }
+}
+
+extension MapViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        NSLog("Location manager failed with error: \(error)")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        guard status == .authorizedWhenInUse else { return }
+        locationManager.requestLocation()
+    }
 }
