b688e016325b74dc822cad99311252ef6099f151
Chris <chris.gonzales.82@gmail.com>
2020-04-10 09:10:36 -0700

    created VideoRecordingViewController and scene, wired up, and constrained

---
+++ Experiences/Experiences/View Controllers/VideoRecordingViewController.swift
@@ -0,0 +1,26 @@
+//
+//  VideoRecordingViewController.swift
+//  Experiences
+//
+//  Created by Chris Gonzales on 4/10/20.
+//  Copyright © 2020 Chris Gonzales. All rights reserved.
+//
+
+import UIKit
+
+class VideoRecordingViewController: UIViewController {
+
+    @IBOutlet weak var recordButton: UIButton!
+    
+    @IBAction func saveTapped(_ sender: Any) {
+    }
+    @IBAction func recordTapped(_ sender: UIButton) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
