dd9951cc8cf81ff3ee8635b2edbe33ddbcecd1fd
Chris <chris.gonzales.82@gmail.com>
2020-04-10 09:01:00 -0700

    Created PhotoViewController file and scene, wired up

---
+++ Experiences/Experiences/View Controllers/PhotoViewController.swift
@@ -0,0 +1,38 @@
+//
+//  PhotoViewController.swift
+//  Experiences
+//
+//  Created by Chris Gonzales on 4/10/20.
+//  Copyright © 2020 Chris Gonzales. All rights reserved.
+//
+
+import UIKit
+
+class PhotoViewController: UIViewController {
+
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func addPhotoTapped(_ sender: UIButton) {
+    }
+    @IBAction func saveTapped(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
