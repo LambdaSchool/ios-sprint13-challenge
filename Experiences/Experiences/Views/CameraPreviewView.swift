2fa415caf97fc691ced710bacd9487c50ed295c7
Enrique Gongora <EGTech24@gmail.com>
2020-04-10 09:13:47 -0700

    Added missing views to storyboard

---
+++ Experiences/Experiences/Views/CameraPreviewView.swift
@@ -0,0 +1,21 @@
+//
+//  CameraPreviewView.swift
+//  Experiences
+//
+//  Created by Enrique Gongora on 4/10/20.
+//  Copyright © 2020 Enrique Gongora. All rights reserved.
+//
+
+import UIKit
+
+class CameraPreviewView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Experiences/Experiences/Views/ExperienceDetailView.swift
@@ -0,0 +1,21 @@
+//
+//  ExperienceDetailView.swift
+//  Experiences
+//
+//  Created by Enrique Gongora on 4/10/20.
+//  Copyright © 2020 Enrique Gongora. All rights reserved.
+//
+
+import UIKit
+
+class ExperienceDetailView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
