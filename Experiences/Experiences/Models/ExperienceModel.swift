53bcd16d90d4e0372f98099431e9a3fe9790bce5
knopptimism <56057392+knopptimism@users.noreply.github.com>
2020-05-08 09:33:03 -0700

    Experience made and MKNotation understood

---
+++ Experiences/Experiences/Models/ExperienceModel.swift
@@ -0,0 +1,38 @@
+//
+//  ExperienceModel.swift
+//  Experiences
+//
+//  Created by Lambda_School_Loaner_268 on 5/8/20.
+//  Copyright © 2020 Lambda. All rights reserved.
+//
+
+import Foundation
+import MapKit
+enum MediaType: String {
+    case image
+    case audio
+    case geotag
+    case video
+}
+
+class Experience {
+    
+    init(title: String, description: String? = "", imageData: Data?, audioURL: URL?, videoURL: URL?, timestamp: Date = Date()) {
+
+     
+        self.title = title
+        self.description = description
+        self.imageData = imageData
+        self.audioURL = audioURL
+        self.videoURL = videoURL
+        self.timestamp = timestamp
+    }
+    let title: String?
+    var description: String? 
+    var imageData: Data?
+    var audioURL: URL?
+    var videoURL: URL?
+    let timestamp: Date
+    
+    
+}
