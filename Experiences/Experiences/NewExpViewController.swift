58558ed1296ab1e98efddea4dbe086fae4e30d64
Hector L <hectoremil21@hotmail.com>
2020-05-08 13:38:44 -0400

    Add view for audio recording. Clicking record prompts for either auddio or video recording.

--- Experiences/Experiences/NewExpViewController.swift
+++ Experiences/Experiences/NewExpViewController.swift
@@ -69,11 +69,11 @@ class NewExpViewController: UIViewController {
         let filter = CIFilter.vignetteEffect()
 
         // Set values
-        let size = photoView.bounds.size
+//        let size = photoView.bounds.size
         filter.inputImage = ciImage
-        filter.intensity = 0.3
-        filter.radius = 500
-        filter.center = CGPoint(x: size.width / 2, y: size.height / 2)
+        filter.intensity = 0.5
+        filter.radius = 100
+//        filter.center = CGPoint(x: size.width / 2, y: size.height / 2)
 
         // CI -> CG -> UI
         guard let outputCIImage = filter.outputImage else { return nil }
@@ -88,6 +88,23 @@ class NewExpViewController: UIViewController {
         presentImagePickerController()
     }
     @IBAction func recordButtonTapped(_ sender: Any) {
+        let alert = UIAlertController(title: "Record", message: "What kind of recording would you like to add:", preferredStyle: .actionSheet)
+
+        let videoExpAction = UIAlertAction(title: "Video", style: .default) { (_) in
+            self.performSegue(withIdentifier: "VideoRecordShowSegue", sender: self)
+        }
+
+        let audioExpAction = UIAlertAction(title: "Audio", style: .default) { (_) in
+            self.performSegue(withIdentifier: "AudioRecordShowSegue", sender: self)
+        }
+
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+
+        alert.addAction(videoExpAction)
+        alert.addAction(audioExpAction)
+        alert.addAction(cancelAction)
+
+        self.present(alert, animated: true, completion: nil)
     }
     @IBAction func saveButtonTapped(_ sender: Any) {
         // I'd usually make locationManager private and only control it through built methods but... time
