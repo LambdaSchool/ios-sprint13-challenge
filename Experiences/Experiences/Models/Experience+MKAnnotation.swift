6025067927336c21a135194f703707d7c6240e24
Wyatt Harrell <wyattaharrell@gmail.com>
2020-05-08 11:13:09 -0400

    Created Experience model and conformed it to MKAnnotation

---
+++ Experiences/Experiences/Models/Experience+MKAnnotation.swift
@@ -0,0 +1,20 @@
+//
+//  Experience+MKAnnotation.swift
+//  Experiences
+//
+//  Created by Wyatt Harrell on 5/8/20.
+//  Copyright © 2020 Wyatt Harrell. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+extension Experience: MKAnnotation {
+    var coordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+    
+    var title: String? {
+        name
+    }
+}
---
+++ Experiences/Experiences/Models/Experience.swift
@@ -0,0 +1,29 @@
+//
+//  Experience.swift
+//  Experiences
+//
+//  Created by Wyatt Harrell on 5/8/20.
+//  Copyright © 2020 Wyatt Harrell. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Experience: NSObject {
+    let name: String
+    let image: UIImage?
+    let audioURL: URL?
+    let videoURL: URL?
+    let longitude: Double
+    let latitude: Double
+    
+    
+    internal init(name: String, image: UIImage?, audioURL: URL?, videoURL: URL?, longitude: Double, latitude: Double) {
+        self.name = name
+        self.image = image
+        self.audioURL = audioURL
+        self.videoURL = videoURL
+        self.longitude = longitude
+        self.latitude = latitude
+    }
+}
