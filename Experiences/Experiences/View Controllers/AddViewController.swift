a8695936760ccd2375c71c5c9484723bc653377d
Mark Gerrior <github@gerrior.com>
2020-05-08 09:07:54 -0700

    Wired up Save button in Add View Controller

--- Experiences/Experiences/View Controllers/AddViewController.swift
+++ Experiences/Experiences/View Controllers/AddViewController.swift
@@ -10,21 +10,24 @@ import UIKit
 
 class AddViewController: UIViewController {
 
+    // MARK: - Properites
+
+
+    // MARK: - Actions
+    @IBAction func saveButton(_ sender: UIBarButtonItem) {
+        navigationController?.popViewController(animated: true)
+    }
+
+    // MARK: - Outlets
+
+
+    // MARK: - View Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    // MARK: - Private
 }
