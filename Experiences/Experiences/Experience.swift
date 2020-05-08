ec31476afc6333b2b2f46da4f18fc5b7ae96a938
Hector L <hectoremil21@hotmail.com>
2020-05-08 12:08:37 -0400

    Conform Experience to MKAnnotation.

--- Experiences/Experiences/Experience.swift
+++ Experiences/Experiences/Experience.swift
@@ -7,18 +7,37 @@
 //
 
 import Foundation
-
+import MapKit
 class Experience: NSObject {
 
+    var expTitle: String
     var content: String
     var videoURL: String?
     var imageData: Data?
     var auddioURL: String?
+    var geoTag: CLLocationCoordinate2D
 
-    internal init(content: String, videoURL: String? = nil, imageData: Data? = nil, auddioURL: String? = nil) {
+    internal init(expTitle: String, content: String, geoTag: CLLocationCoordinate2D, videoURL: String? = nil, imageData: Data? = nil, auddioURL: String? = nil) {
+        self.expTitle = expTitle
         self.content = content
         self.videoURL = videoURL
         self.imageData = imageData
         self.auddioURL = auddioURL
+        self.geoTag = geoTag
     }
+}
+
+extension Experience: MKAnnotation {
+    var coordinate: CLLocationCoordinate2D {
+        geoTag
+    }
+
+    var title: String? {
+        expTitle
+    }
+
+    var subtitle: String? {
+        content
+    }
+
 }
