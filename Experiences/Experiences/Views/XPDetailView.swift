ba75997746825699e8fc11f81e544afa3836db69
Wyatt Harrell <wyattaharrell@gmail.com>
2020-05-08 13:55:22 -0400

    Second stretch. Added audio playback in annotation

--- Experiences/Experiences/Views/XPDetailView.swift
+++ Experiences/Experiences/Views/XPDetailView.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class XPDetailView: UIView {
 
@@ -17,6 +18,8 @@ class XPDetailView: UIView {
     }
     
     var imageView = UIImageView()
+    var sliderView = UISlider()
+    var playButton = UIButton()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -25,9 +28,27 @@ class XPDetailView: UIView {
         imageView.topAnchor.constraint(equalTo: topAnchor, constant: 8).isActive = true
         imageView.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
         imageView.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
-        imageView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+//        imageView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
         imageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
         imageView.contentMode = .scaleAspectFit
+        
+        playButton.setImage(UIImage(systemName: "play.fill"), for: .normal)
+        playButton.setImage(UIImage(systemName: "stop.fill"), for: .selected)
+        playButton.setTitle("", for: .normal)
+        playButton.addTarget(self, action: #selector(togglePlayback(_:)), for: .touchUpInside)
+        playButton.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(playButton)
+        
+        sliderView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(sliderView)
+        playButton.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        playButton.topAnchor.constraint(equalTo: imageView.bottomAnchor).isActive = true
+        playButton.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        sliderView.leadingAnchor.constraint(equalTo: playButton.trailingAnchor, constant: 8).isActive = true
+        sliderView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        sliderView.topAnchor.constraint(equalTo: imageView.bottomAnchor).isActive = true
+        sliderView.value = 0
+        
     }
     
     
@@ -39,6 +60,97 @@ class XPDetailView: UIView {
     
     private func updateSubviews() {
         imageView.image = experience?.image
+        
+        if let audioURL = experience?.audioURL {
+            audioPlayer = try? AVAudioPlayer(contentsOf: audioURL)
+        } else {
+            sliderView.isHidden = true
+            playButton.isHidden = true
+        }
+    }
+    
+    @objc func togglePlayback(_ sender: Any) {
+        if isPlaying {
+            pause()
+        } else {
+            play()
+        }
     }
 
+    var audioPlayer: AVAudioPlayer? {
+        didSet {
+            // Using a didSet allows us to make sure we don't forget to set the delegate
+            audioPlayer?.delegate = self
+            audioPlayer?.isMeteringEnabled = true
+        }
+    }
+    
+    
+    var isPlaying: Bool {
+        audioPlayer?.isPlaying ?? false
+    }
+
+
+
+
+    // Fixes bug for iPhone
+    func prepareAudioSession() throws {
+        let session = AVAudioSession.sharedInstance()
+        try session.setCategory(.playAndRecord, options: [.defaultToSpeaker])
+        try session.setActive(true, options: []) // can fail if on a phone call, for instance
+    }
+
+
+    func play() {
+        audioPlayer?.play()
+        startTimer()
+//        updateViews()
+    }
+
+    func pause() {
+        audioPlayer?.pause()
+        cancelTimer()
+        updateViews()
+    }
+
+
+
+    // MARK: - Timer
+    var timer: Timer?
+
+    func startTimer() {
+        timer?.invalidate() // Cancel a timeer before you start a new one
+
+        timer = Timer.scheduledTimer(withTimeInterval: 0.030, repeats: true) { [weak self] (_) in
+            guard let self = self else { return }
+
+            self.updateViews()
+
+        }
+    }
+
+    func cancelTimer() {
+        timer?.invalidate()
+        timer = nil
+    }
+    
+    private func updateViews(){
+        playButton.isSelected = isPlaying
+    }
+
+
+}
+
+extension XPDetailView: AVAudioPlayerDelegate {
+    
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        updateViews()
+    }
+    
+    func audioPlayerDecodeErrorDidOccur(_ player: AVAudioPlayer, error: Error?) {
+        if let error = error {
+            print("⚠️ Audio Player Error: \(error)")
+        }
+        updateViews()
+    }
 }
