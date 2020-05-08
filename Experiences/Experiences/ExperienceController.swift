ac67784458ea83e64a05eec791b6247d09662048
Mark Gerrior <github@gerrior.com>
2020-05-08 09:25:08 -0700

    Created ExperienceController

---
+++ Experiences/Experiences/ExperienceController.swift
@@ -0,0 +1,25 @@
+//
+//  ExperienceController.swift
+//  Experiences
+//
+//  Created by Mark Gerrior on 5/8/20.
+//  Copyright © 2020 Mark Gerrior. All rights reserved.
+//
+
+import Foundation
+
+class ExperienceController {
+
+    var experiences: [Experience] = []
+
+    // MARK: - CRUD
+
+    // Create
+
+    // Read
+
+    // Update
+
+    // Delete
+
+}
