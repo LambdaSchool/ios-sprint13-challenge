8aaf33290198899fb8d99eaf741da8d4ba747654
Shawn Gee <shawngee@gmail.com>
2020-05-08 17:26:11 -0400

    Cleaned up cruft, added TODO

--- Experiences/Experiences/ViewControllers/MapViewController.swift
+++ Experiences/Experiences/ViewControllers/MapViewController.swift
@@ -55,37 +55,7 @@ class MapViewController: UIViewController {
 }
 
 extension MapViewController: MKMapViewDelegate {
-//    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-//        guard let quake = annotation as? Quake else {
-//            fatalError("Only quakes are supported")
-//        }
-//
-//        guard let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "QuakeView", for: annotation) as? MKMarkerAnnotationView else {
-//            fatalError("Unable to cast annotation view as \(MKMarkerAnnotationView.self)")
-//        }
-//
-//        annotationView.glyphImage = UIImage(named: "QuakeIcon")
-//
-//        if let magnitude = quake.magnitude {
-//            switch magnitude {
-//            case 5...:
-//                annotationView.markerTintColor = .red
-//            case 3..<5:
-//                annotationView.markerTintColor = .orange
-//            default:
-//                annotationView.markerTintColor = .yellow
-//            }
-//        } else {
-//            annotationView.markerTintColor = .white
-//        }
-//
-//        annotationView.canShowCallout = true
-//        let detailView = QuakeDetailView()
-//        detailView.quake = quake
-//        annotationView.detailCalloutAccessoryView = detailView
-//
-//        return annotationView
-//    }
+    // TODO: Implement custom annotation views
 }
 
 
