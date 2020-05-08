73e60486ec39f728935a7ed3a257612ed4f225c6
Wyatt Harrell <wyattaharrell@gmail.com>
2020-05-08 11:43:19 -0400

    Created RecordVideoViewController

---
+++ Experiences/Experiences/View Controllers/RecordVideoViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RecordVideoViewController.swift
+//  Experiences
+//
+//  Created by Wyatt Harrell on 5/8/20.
+//  Copyright © 2020 Wyatt Harrell. All rights reserved.
+//
+
+import UIKit
+
+class RecordVideoViewController: UIViewController {
+
+    // MARK: - IBOutlets
+    
+    // MARK: - Properties
+    
+    // MARK: - View Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
