8de6b09adcbde2ed40c69a273e597079ccc973e9
Hector L <hectoremil21@hotmail.com>
2020-05-08 13:49:15 -0400

    Create AudioRecordViewController

--- Experiences/Experiences/NewExpViewController.swift
+++ Experiences/Experiences/NewExpViewController.swift
@@ -46,15 +46,15 @@ class NewExpViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    /*
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "VideoRecordShowSegue" {
+
+        } else if segue.identifier == "AudioRecordShowSegue" {
+
+        }
     }
-    */
 
     // MARK: - Methods
 
