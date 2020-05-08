bc0bacdbdf14d4ee9ad6c5fab5d5a1393dab9c36
Wyatt Harrell <wyattaharrell@gmail.com>
2020-05-08 13:11:23 -0400

    Audio recording completed

--- Experiences/Experiences/View Controllers/AddExperienceViewController.swift
+++ Experiences/Experiences/View Controllers/AddExperienceViewController.swift
@@ -45,6 +45,9 @@ class AddExperienceViewController: UIViewController {
     let locationManager = CLLocationManager()
     let context = CIContext(options: nil)
     var recordedVideoURL: URL?
+    var recordedAudioURL: URL?
+    var latitude: Double = 0
+    var longitude: Double = 0
     
     var originalImage: UIImage? {
         didSet {
@@ -66,6 +69,12 @@ class AddExperienceViewController: UIViewController {
         titleTextField.layer.cornerRadius = 8
         saveButton.layer.cornerRadius = 8
         titleTextField.setLeftPaddingPoints(10)
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        }
     }
     
     private func requestPermissionAndShowCamera() {
@@ -102,6 +111,11 @@ class AddExperienceViewController: UIViewController {
     // MARK: - IBActions
     
     @IBAction func recordAudioButtoTapped(_ sender: Any) {
+        if isRecording {
+            stopRecording()
+        } else {
+            startRecording()
+        }
     }
     
     @IBAction func selectPhotoButtonTapped(_ sender: Any) {
@@ -114,8 +128,9 @@ class AddExperienceViewController: UIViewController {
     
     @IBAction func saveButtonTapped(_ sender: Any) {
         guard let title = titleTextField.text, !title.isEmpty else { return }
-        //TODO: FINISH THIS ACTION
-        experienceController?.createExperience(name: title, image: imageView.image, audioURL: nil, videoURL: recordedVideoURL, longitude: 0, latitude: 0)
+        //TODO: Dont forget about location
+        experienceController?.createExperience(name: title, image: imageView.image, audioURL: recordedAudioURL, videoURL: recordedVideoURL, longitude: 0, latitude: 0)
+        locationManager.stopUpdatingLocation()
         navigationController?.popViewController(animated: true)
     }
 
@@ -127,15 +142,100 @@ class AddExperienceViewController: UIViewController {
         }
     }
     
+    // MARK: - Audio Experience
+    var audioRecorder: AVAudioRecorder?
+    var recordingURL: URL?
     
+    var isRecording: Bool {
+        audioRecorder?.isRecording ?? false
+    }
     
+    private func updateAudioViews() {
+        recordAudioButton.isSelected = isRecording
+    }
     
+    func createNewRecordingURL() -> URL {
+        let documents = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        
+        let name = ISO8601DateFormatter.string(from: Date(), timeZone: .current, formatOptions: .withInternetDateTime)
+        let file = documents.appendingPathComponent(name, isDirectory: false).appendingPathExtension("caf")
+        
+        print("recording URL: \(file)")
+        
+        return file
+    }
     
+    func requestPermissionOrStartRecording() {
+        switch AVAudioSession.sharedInstance().recordPermission {
+        case .undetermined:
+            AVAudioSession.sharedInstance().requestRecordPermission { granted in
+                guard granted == true else {
+                    print("We need microphone access")
+                    return
+                }
+                
+                print("Recording permission has been granted!")
+                // NOTE: Invite the user to tap record again, since we just interrupted them, and they may not have been ready to record
+            }
+        case .denied:
+            print("Microphone access has been blocked.")
+            
+            let alertController = UIAlertController(title: "Microphone Access Denied", message: "Please allow this app to access your Microphone.", preferredStyle: .alert)
+            
+            alertController.addAction(UIAlertAction(title: "Open Settings", style: .default) { (_) in
+                UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
+            })
+            
+            alertController.addAction(UIAlertAction(title: "Cancel", style: .default, handler: nil))
+            
+            present(alertController, animated: true, completion: nil)
+        case .granted:
+            startRecording()
+        @unknown default:
+            break
+        }
+    }
     
+    func startRecording() {
+        let recordingURL = createNewRecordingURL()
+        
+        let format = AVAudioFormat(standardFormatWithSampleRate: 44_100, channels: 1)!
+        audioRecorder = try? AVAudioRecorder(url: recordingURL, format: format)
+        audioRecorder?.delegate = self
+        audioRecorder?.isMeteringEnabled = true
+        audioRecorder?.record()
+        self.recordingURL = recordingURL
+        updateAudioViews()
+        startTimer()
+    }
     
+    func stopRecording() {
+        audioRecorder?.stop()
+        updateAudioViews()
+        cancelTimer()
+    }
     
+    // MARK: - Timer
+    var timer: Timer?
     
+    func startTimer() {
+        timer?.invalidate() // Cancel a timeer before you start a new one
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 0.030, repeats: true) { [weak self] (_) in
+            guard let self = self else { return }
+            
+            self.updateAudioViews()
+            
+            if let _ = self.audioRecorder,
+                self.isRecording == true {
+            }
+        }
+    }
     
+    func cancelTimer() {
+        timer?.invalidate()
+        timer = nil
+    }
     
     // MARK: - Photo Experience
     private func presentImagePickerController() {
@@ -197,7 +297,32 @@ extension AddExperienceViewController: UIImagePickerControllerDelegate, UINaviga
         
         picker.dismiss(animated: true)
     }
+    
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         picker.dismiss(animated: true)
+    }
+}
+
+
+extension AddExperienceViewController: AVAudioRecorderDelegate {
+    func audioRecorderEncodeErrorDidOccur(_ recorder: AVAudioRecorder, error: Error?) {
+        if let error = error {
+            print("⚠️ Audio Recorder Error: \(error)")
+        }
+        updateAudioViews()
+    }
+    
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        recordedAudioURL = recordingURL
+        recordAudioButton.setImage(UIImage(systemName: "checkmark"), for: .normal)
+        updateAudioViews()
+    }
+}
+
+extension AddExperienceViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
+        latitude = locValue.latitude
+        longitude = locValue.longitude
     }
 }
