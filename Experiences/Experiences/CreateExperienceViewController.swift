724d11cd2fc01367fe83eceff85c0b55afafe4d8
Tobi Tselios <tobi.tselios@gmail.com>
2020-05-08 20:29:14 +0100

    ImagePicker Setup

--- Experiences/Experiences/CreateExperienceViewController.swift
+++ Experiences/Experiences/CreateExperienceViewController.swift
@@ -33,12 +33,43 @@ class CreateExperienceViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    }
+
+    // MARK: - Actions
+
+    private func presentImagePickerController() {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+
+        let alert = UIAlertController(title: "Select Source", message: nil, preferredStyle: .actionSheet)
+
+        let photoLibrary = UIAlertAction(title: "Photo Library", style: .default) { action in
+            imagePicker.sourceType = .photoLibrary
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+
+        let camera = UIAlertAction(title: "Camera", style: .default) { action in
+            if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                imagePicker.sourceType = .camera
+                self.present(imagePicker, animated: true, completion: nil)
+            } else {
+                fatalError("No camera available. Are you on a simulator?")
+            }
+        }
+
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+
+        alert.addAction(photoLibrary)
+        alert.addAction(camera)
+        alert.addAction(cancel)
+
+        self.present(alert, animated: true, completion: nil)
     }
 
     // MARK: - IBActions
 
     @IBAction func addImageTapped(_ sender: Any) {
+        presentImagePickerController()
     }
 
 
@@ -54,5 +85,13 @@ class CreateExperienceViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+
+}
+
+extension CreateExperienceViewController: UIImagePickerControllerDelegate {
+
+}
+
+extension CreateExperienceViewController: UINavigationControllerDelegate {
 
 }
