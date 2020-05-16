5e140c061c2e2e269f72287a7d4390dcdd127ff7
Jessie <jessie.griffin@icloud.com>
2020-05-15 23:24:24 -0400

    Adds image post functionality (not complete)

--- Experiences/Experiences/Helpers/Extensions/UIViewController+MediaChoiceAlert.swift
+++ Experiences/Experiences/Helpers/Extensions/UIViewController+MediaChoiceAlert.swift
@@ -9,23 +9,26 @@
 import UIKit
 
 extension UIViewController {
-    
     func presentMediaOptionsAlertController(title: String? = "Choose Media Type",
                                             message: String? = "How would you like to record your experience?",
                                             dismissActionCompletion: ((UIAlertAction) -> Void)? = nil,
                                             completion: (() -> Void)? = nil) {
         
+        guard let imageVC = self.storyboard?.instantiateViewController(withIdentifier: "ImagePostViewController"),
+            let audioVC = self.storyboard?.instantiateViewController(withIdentifier: "AudioPostViewController"),
+            let videoVC = self.storyboard?.instantiateViewController(withIdentifier: "VideoPostViewController") else { return }
+        
         let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let dismissAction = UIAlertAction(title: "Dismiss", style: .cancel, handler: dismissActionCompletion)
         let imageAction = UIAlertAction(title: "Choose a photo.", style: .default) { action in
-            self.present(ImagePostViewController(), animated: true, completion: nil)
+            self.present(imageVC, animated: true)
         }
         let audioAction = UIAlertAction(title: "Record audio.", style: .default) { action in
-            self.present(AudioPostViewController(), animated: true, completion: nil)
+            self.present(audioVC, animated: true, completion: nil)
 
         }
         let videoAction = UIAlertAction(title: "Take a video.", style: .default) { action in
-            self.present(VideoPostViewController(), animated: true, completion: nil)
+            self.present(videoVC, animated: true, completion: nil)
 
         }
         alertController.addAction(dismissAction)
@@ -36,4 +39,6 @@ extension UIViewController {
         
         present(alertController, animated: true, completion: completion)
     }
+
 }
+
