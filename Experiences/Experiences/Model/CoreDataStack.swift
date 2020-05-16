ff2f0654be8d4962e6579ce9bf480ca45fceea0d
Denis Cedeno <denis.cedeno@gmail.com>
2020-05-15 21:49:09 -0400

    Basic file structure and UI set up

---
+++ Experiences/Experiences/Model/CoreDataStack.swift
@@ -0,0 +1,9 @@
+//
+//  CoreDataStack.swift
+//  Experiences
+//
+//  Created by denis cedeno on 5/15/20.
+//  Copyright © 2020 DenCedeno Co. All rights reserved.
+//
+
+import Foundation
---
+++ Experiences/Experiences/View/JournalTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  JournalTableViewCell.swift
+//  Experiences
+//
+//  Created by denis cedeno on 5/15/20.
+//  Copyright © 2020 DenCedeno Co. All rights reserved.
+//
+
+import UIKit
+
+class JournalTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
