247d98feaf83d6600ca9f47242fc935a9a844c9c
Alex Thompson <guitarman226@gmail.com>
2020-05-16 11:07:16 -0500

    Setup views for the most part, created viwController files, connected outlets

---
+++ Experiences/Experiences/View Controllers/MapViewController.swift
@@ -0,0 +1,39 @@
+//
+//  MapViewController.swift
+//  Experiences
+//
+//  Created by Alex Thompson on 5/16/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+    
+    let locationManager = CLLocationManager()
+    
+    @IBOutlet var mapView: MKMapView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        locationManager.requestWhenInUseAuthorization()
+
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Experiences/Experiences/View Controllers/NewExperienceViewController.swift
@@ -0,0 +1,36 @@
+//
+//  NewExperienceViewController.swift
+//  Experiences
+//
+//  Created by Alex Thompson on 5/16/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class NewExperienceViewController: UIViewController {
+    
+    @IBOutlet var titleTextField: UITextField!
+    @IBOutlet var photoImage: UIImageView!
+    @IBOutlet var addPosterButton: UIButton!
+    @IBOutlet var recordButton: UIButton!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
