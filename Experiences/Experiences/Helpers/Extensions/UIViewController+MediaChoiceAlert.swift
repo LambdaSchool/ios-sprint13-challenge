adc1061ce25134630f1aeadafae38eaa73fb62ba
Jessie <jessie.griffin@icloud.com>
2020-05-15 21:54:13 -0400

    Moves and organizes files

---
+++ Experiences/Experiences/Helpers/Extensions/UIViewController+MediaChoiceAlert.swift
@@ -0,0 +1,9 @@
+//
+//  UIViewController+MediaChoiceAlert.swift
+//  Experiences
+//
+//  Created by Jessie Ann Griffin on 5/15/20.
+//  Copyright © 2020 Jessie Griffin. All rights reserved.
+//
+
+import Foundation
---
+++ Experiences/Experiences/Models/Experience.swift
@@ -0,0 +1,9 @@
+//
+//  Experience.swift
+//  Experiences
+//
+//  Created by Jessie Ann Griffin on 5/15/20.
+//  Copyright © 2020 Jessie Griffin. All rights reserved.
+//
+
+import Foundation
