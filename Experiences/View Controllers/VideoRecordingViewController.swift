f017e8018922ae8d05f12ce39a529321ff339029
Christian Lorenzo <clbmiami2004@gmail.com>
2020-05-17 12:35:46 -0700

    Created function to start/stop audio recording. Created function to get audioRecordingURL. Set permissions for audio and video recording.

---
+++ Experiences/View Controllers/VideoRecordingViewController.swift
@@ -0,0 +1,43 @@
+//
+//  VideoRecordingViewController.swift
+//  Experiences
+//
+//  Created by Christian Lorenzo on 5/17/20.
+//  Copyright © 2020 Christian Lorenzo. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import MapKit
+
+class VideoRecordingViewController: UIViewController {
+    
+    var userLocation: CLLocationCoordinate2D?
+    var picture: Experience.Picture?
+    var experienceTitle: String?
+    var recordingURL: Experience.Audio?
+    
+    lazy  private var captureSession = AVCaptureSession()
+    lazy private var fileOutput = AVCaptureMovieFileOutput()
+    var videoURL: URL?
+    var player: AVPlayer!
+    var mapViewController: MapViewController?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
