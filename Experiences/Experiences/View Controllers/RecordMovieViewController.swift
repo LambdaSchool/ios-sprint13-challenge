c9715f7b214af0aeb804b9f5e3515272d1b0ce0b
Joshua Rutkowski <jr.rtkski@gmail.com>
2020-05-17 22:38:37 -0400

    Wired up RecordMovieViewController. Navigation through app complete.

--- Experiences/Experiences/View Controllers/RecordMovieViewController.swift
+++ Experiences/Experiences/View Controllers/RecordMovieViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class RecordMovieViewController: UIViewController {
-
+    
+    @IBOutlet var cameraView: CameraPreviewView!
+    @IBOutlet var recordButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,14 +20,9 @@ class RecordMovieViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func recordButtonTapped(_ sender: Any) {
     }
-    */
-
+    @IBAction func saveButtonTapped(_ sender: Any) {
+    }
+    
 }
