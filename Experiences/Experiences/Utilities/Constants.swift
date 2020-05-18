942dca46891a5169a4bbbee5e0e3a0626af1b869
David Wright <davidtwright89@gmail.com>
2020-05-17 23:44:24 -0400

    Added segue identifiers to the storyboard and created and added them to a Constants.swift file

---
+++ Experiences/Experiences/Utilities/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  Experiences
+//
+//  Created by David Wright on 5/17/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+import UIKit
+
+struct DW {
+    static let buttonCornerRadius: CGFloat = 8.0
+}
+
+struct SegueIdentifiers {
+    static let addExperienceSegue: String = "AddExperienceSegue"
+    static let addImageSegue: String = "AddImageSegue"
+    static let addAudioSegue: String = "AddAudioSegue"
+    static let addVideoSegue: String = "AddVideoSegue"
+}
