3850b8489dcbca5d8c510bd1b3cebc6581a06329
David Wright <davidtwright89@gmail.com>
2020-05-18 07:56:43 -0400

    Fixed issue where location was not updating correctly

--- Experiences/Experiences/Model Controller/ExperienceController.swift
+++ Experiences/Experiences/Model Controller/ExperienceController.swift
@@ -9,19 +9,43 @@
 import Foundation
 import MapKit
 
-class ExperienceController {
+class ExperienceController: NSObject {
     
     var experiences: [Experience] = []
+    
+    private let locationManager = CLLocationManager()
+    private var location: CLLocationCoordinate2D?
     
     @discardableResult
     func createExperience(title: String, imageURL: URL? = nil, audioURL: URL? = nil, videoURL: URL? = nil) -> Experience {
         
-        let experience = Experience(title: title, imageURL: imageURL, audioURL: audioURL, videoURL: videoURL)
+        locationManager.requestLocation()
+        location = locationManager.location?.coordinate
+        
+        let experience = Experience(title: title, imageURL: imageURL, audioURL: audioURL, videoURL: videoURL, geotag: location)
         
         experiences.append(experience)
         
         return experience
     }
     
+    override init() {
+        super.init()
+        
+        locationManager.delegate = self
+    }
+}
+
+extension ExperienceController: CLLocationManagerDelegate {
     
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            print("Found user's location: \(location)")
+            self.location = location.coordinate
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Failed to find user's location: \(error.localizedDescription)")
+    }
 }
