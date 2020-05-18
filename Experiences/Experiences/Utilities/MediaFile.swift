3558c9452d5a4df34da705107cbccada4e735722
David Wright <davidtwright89@gmail.com>
2020-05-18 07:39:43 -0400

    Implemented the AddVideoViewController to allow users to create and add videos

---
+++ Experiences/Experiences/Utilities/MediaFile.swift
@@ -0,0 +1,36 @@
+//
+//  MediaFile.swift
+//  Experiences
+//
+//  Created by David Wright on 5/18/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+import Foundation
+
+class MediaFile {
+    
+    static func newURL(forType mediaType: MediaType) -> URL {
+        let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+
+        let formatter = ISO8601DateFormatter()
+        formatter.formatOptions = [.withInternetDateTime]
+
+        let name = formatter.string(from: Date())
+        
+        var fileExtension: String
+        
+        switch mediaType {
+        case .image:
+            fileExtension = "png"
+        case .audio:
+            fileExtension = "wav"
+        case .video:
+            fileExtension = "mov"
+        }
+        
+        let fileURL = documentsDirectory.appendingPathComponent(name).appendingPathExtension(fileExtension)
+        
+        return fileURL
+    }
+}
