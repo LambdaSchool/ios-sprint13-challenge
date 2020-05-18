b2b80c2d3b806e3059156e2bef3864f987138065
CrystalKnightCodes <CrystalKnightCodes@gmail.com>
2020-05-17 21:20:15 -0400

    Initial Commit

---
+++ Road Trip/Road Trip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Road Trip
+//
+//  Created by Christy Hicks on 5/17/20.
+//  Copyright © 2020 Knight Night. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
