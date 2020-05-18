2fbd707bcc329af7480e7ed295b96528dc818e5c
CrystalKnightCodes <CrystalKnightCodes@gmail.com>
2020-05-17 22:34:00 -0400

    Wired outlets and actions and set up wireframe of Visit Detail View Controller.

--- Road Trip/Car Trek/VisitDetailViewController.swift
+++ Road Trip/Car Trek/VisitDetailViewController.swift
@@ -9,22 +9,47 @@
 import UIKit
 
 class VisitDetailViewController: UIViewController {
-
+    // MARK: - Outlets
+    @IBOutlet var nameTextField: UITextField!
+    @IBOutlet var photoImageView: UIImageView!
+    @IBOutlet var audioElapsedTimeLabel: UILabel!
+    @IBOutlet var audioTotalTimeLabel: UILabel!
+    @IBOutlet var audioSlider: UISlider!
+    @IBOutlet var audioPlayButton: UIButton!
+    @IBOutlet var videoView: UIView!
+    
+    // MARK: - Properties
+//    var visit: Visit? {
+//        didSet {
+//            updateViews()
+//        }
+//    }
+    
+    // MARK: - Views
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateViews()
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func updateViews() {
+        
     }
-    */
-
+    
+    // MARK: - Actions
+    @IBAction func addPhoto(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func addAudioRecording(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func addVideoRecording(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func saveVisit(_ sender: UIBarButtonItem) {
+        
+    }
 }
