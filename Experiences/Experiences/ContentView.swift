3e030439cb0b01344b9d93636126852a688fb4f4
Alex Shillingford <alex.shillingford@icloud.com>
2020-05-17 19:22:43 -0600

    Create initial project in cloned local repository

---
+++ Experiences/Experiences/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Experiences
+//
+//  Created by Alex Shillingford on 5/17/20.
+//  Copyright © 2020 shillwil. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
