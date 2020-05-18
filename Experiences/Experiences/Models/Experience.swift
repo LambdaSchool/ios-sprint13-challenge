3f1de2fb314da0a56f32488a5b3b50bcdaffeafe
Alex Shillingford <alex.shillingford@icloud.com>
2020-05-17 21:51:51 -0600

    Create Experience model

---
+++ Experiences/Experiences/Models/Experience.swift
@@ -0,0 +1,19 @@
+//
+//  Experience.swift
+//  Experiences
+//
+//  Created by Alex Shillingford on 5/17/20.
+//  Copyright © 2020 shillwil. All rights reserved.
+//
+
+import Foundation
+import MapKit
+import AVKit
+
+struct Experience {
+    var title: String
+    var coordinate: CLLocationCoordinate2D?
+    var photo: UIImage?
+    var audioURL: URL?
+    var videoURL: URL?
+}
