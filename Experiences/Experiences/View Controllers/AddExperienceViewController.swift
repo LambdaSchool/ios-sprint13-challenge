ba24491d07d4fdf5003e3218f3e2e043af745b13
David Wright <davidtwright89@gmail.com>
2020-05-18 03:37:24 -0400

    Updated the AddExperienceViewController to allow the User to create and save a new experience (without adding any media data)

--- Experiences/Experiences/View Controllers/AddExperienceViewController.swift
+++ Experiences/Experiences/View Controllers/AddExperienceViewController.swift
@@ -10,13 +10,39 @@ import UIKit
 
 class AddExperienceViewController: UIViewController {
 
+    // MARK: - Properties
+
+    var experienceController: ExperienceController!
+    
+    var imageURL: URL?
+    var audioURL: URL?
+    var videoURL: URL?
+    
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    // MARK: - IBActions
+    
+    @IBAction func cancelButtonTapped(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func saveButtonTapped(_ sender: UIBarButtonItem) {
+        guard let title = titleTextField.text, !title.isEmpty else { return }
+        
+        experienceController.createExperience(title: title, imageURL: imageURL, audioURL: audioURL, videoURL: videoURL)
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
     /*
     // MARK: - Navigation
 
