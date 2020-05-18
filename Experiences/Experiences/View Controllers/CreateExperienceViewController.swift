839e1aee00d3db93451ac581316ae10e6d8cb85c
Joshua Rutkowski <jr.rtkski@gmail.com>
2020-05-17 21:41:36 -0400

    Created properties for Experience model in CreateExperienceViewController and setup actions

--- Experiences/Experiences/View Controllers/CreateExperienceViewController.swift
+++ Experiences/Experiences/View Controllers/CreateExperienceViewController.swift
@@ -10,23 +10,35 @@ import UIKit
 
 class CreateExperienceViewController: UIViewController {
     
+    //MARK: - IBOutlets
+    
+    @IBOutlet var titleTextField: UITextField!
+    @IBOutlet var experienceImageView: UIImageView!
+    @IBOutlet var recordAudioButton: UIButton!
+    
+    //Mark: - Properties
     var experienceController: ExperienceController?
+    var experience: Experience?
+    var experienceTitle: String?
+    var latitude: Double?
+    var longitude: Double?
+    var audioExtension: String?
+    var photoExtension: String?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func addPhotoButtonTapped(_ sender: Any) {
     }
-    */
-
+    
+    @IBAction func recordAudioButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func nextButtonTapped(_ sender: Any) {
+    }
+    
+    
 }
