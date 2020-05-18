def9916a140e16bf04dc134f277df28247e060c1
CrystalKnightCodes <CrystalKnightCodes@gmail.com>
2020-05-17 22:45:10 -0400

    Created Visit class

---
+++ Road Trip/Car Trek/Visit.swift
@@ -0,0 +1,25 @@
+//
+//  Visit.swift
+//  Car Trek
+//
+//  Created by Christy Hicks on 5/17/20.
+//  Copyright © 2020 Knight Night. All rights reserved.
+//
+
+import UIKit
+
+class Visit {
+    let name: String
+    var location: Int
+    var photo: UIImage?
+    var audioRecordingURL: URL?
+    var videoRecordingURL: URL?
+    
+    init(name: String, location: Int, photo: UIImage?, audioURL: URL?, videoURL: URL?) {
+        self.name = name
+        self.location = location
+        self.photo = photo
+        self.audioRecordingURL = audioURL
+        self.videoRecordingURL = videoURL
+    }
+}
