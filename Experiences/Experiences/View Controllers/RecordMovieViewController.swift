b830a79a10044aa0e3c0bf05d8a5f7589ca91530
Joshua Rutkowski <jr.rtkski@gmail.com>
2020-05-17 22:42:16 -0400

    Setup adding video/saving experience

--- Experiences/Experiences/View Controllers/RecordMovieViewController.swift
+++ Experiences/Experiences/View Controllers/RecordMovieViewController.swift
@@ -7,11 +7,23 @@
 //
 
 import UIKit
+import AVFoundation
 
 class RecordMovieViewController: UIViewController {
     
     @IBOutlet var cameraView: CameraPreviewView!
     @IBOutlet var recordButton: UIButton!
+    // MARK: - Properties
+    var experienceController: ExperienceController?
+    var experience: Experience?
+    var video = ""
+    lazy private var captureSession = AVCaptureSession()
+    lazy private var fileOutput = AVCaptureMovieFileOutput()
+    var experienceTitle: String?
+    var latitude: Double?
+    var longitude: Double?
+    var audioExtension: String?
+    var photoExtension: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,10 +31,22 @@ class RecordMovieViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    func addVideo() {
+        
+        guard let title = experienceTitle,
+            let lat = latitude,
+            let long = longitude else { return }
+        
+        experienceController?.createExperience(title: title, latitude: lat, longitude: long, videoExtension: video, audioExtension: audioExtension ?? "", photoExtension: photoExtension ?? "")
+        navigationController?.popToRootViewController(animated: true)
+    }
+    
 
     @IBAction func recordButtonTapped(_ sender: Any) {
     }
+    
     @IBAction func saveButtonTapped(_ sender: Any) {
+        addVideo()
     }
     
 }
