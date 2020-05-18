6fc5d8f0875e64038ebe8a37205e190181432b7d
Joshua Rutkowski <jr.rtkski@gmail.com>
2020-05-17 21:25:31 -0400

    Created remaining view controllers and views

---
+++ Experiences/Experiences/Views/CameraPreview.swift
@@ -0,0 +1,21 @@
+//
+//  CameraPreview.swift
+//  Experiences
+//
+//  Created by Joshua Rutkowski on 5/17/20.
+//  Copyright © 2020 Josh Rutkowski. All rights reserved.
+//
+
+import UIKit
+
+class CameraPreview: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
