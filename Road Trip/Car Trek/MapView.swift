21c1b3f6ab71135962a8cb6c280c75bcf49baf77
CrystalKnightCodes <CrystalKnightCodes@gmail.com>
2020-05-17 22:08:00 -0400

    Changed project name and created UI in main storyboard.

---
+++ Road Trip/Car Trek/MapView.swift
@@ -0,0 +1,22 @@
+//
+//  MapView.swift
+//  Road Trip
+//
+//  Created by Christy Hicks on 5/17/20.
+//  Copyright © 2020 Knight Night. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapView: MKMapView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
