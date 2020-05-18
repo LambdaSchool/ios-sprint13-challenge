540a231730f0b80035a12197c1e4334d1f216c9e
Alex Shillingford <alex.shillingford@icloud.com>
2020-05-17 19:32:20 -0600

    Create MapView component for ExperienceView

---
+++ Experiences/Experiences/Supporting Views/MapView.swift
@@ -0,0 +1,32 @@
+//
+//  MapView.swift
+//  Experiences
+//
+//  Created by Alex Shillingford on 5/17/20.
+//  Copyright © 2020 shillwil. All rights reserved.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: UIViewRepresentable {
+    var coordinate: CLLocationCoordinate2D
+
+    func makeUIView(context: Context) -> MKMapView {
+        MKMapView(frame: .zero)
+    }
+
+    func updateUIView(_ view: MKMapView, context: Context) {
+        let span = MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        view.setRegion(region, animated: true)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView(coordinate: displaycoordinate)
+    }
+}
+
+let displaycoordinate = CLLocationCoordinate2D(latitude: 40.898920, longitude: -111.851120)
