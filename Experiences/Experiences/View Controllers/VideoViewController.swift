5e8796a12429a367925cf88897a379cded5a58a8
Denis Cedeno <denis.cedeno@gmail.com>
2020-05-18 22:08:37 -0400

    added boilerplate code for audio and video controllers

--- Experiences/Experiences/View Controllers/VideoViewController.swift
+++ Experiences/Experiences/View Controllers/VideoViewController.swift
@@ -7,34 +7,178 @@
 //
 
 import UIKit
+import AVFoundation
+import AVKit
 
 class VideoViewController: UIViewController {
 
     var experience: Experience?
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
+       
+       lazy private var captureSession = AVCaptureSession()
+       lazy private var fileOutput = AVCaptureMovieFileOutput()
+       
+       private var player: AVPlayer!
 
-        // Do any additional setup after loading the view.
-    }
-    
-        private func presentSuccessfulSaveAlert() {
-            let alert = UIAlertController(title: "Photo Saved!", message: "The photo has been saved to your Photo Library!", preferredStyle: .alert)
-            
-            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-            navigationController?.popViewController(animated: true)
-            present(alert, animated: true, completion: nil)
-        }
-    
+       @IBOutlet var recordButton: UIButton!
+       @IBOutlet var cameraView: CameraPreviewView!
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+       override func viewDidLoad() {
+           super.viewDidLoad()
 
-}
+           // Resize camera preview to fill the entire screen
+           cameraView.videoPlayerLayer.videoGravity = .resizeAspectFill
+           setupCamera()
+           
+           let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTapGesture(_:)))
+           view.addGestureRecognizer(tapGesture)
+       }
+       
+       @IBAction func handleTapGesture(_ sender: UITapGestureRecognizer) {
+           if sender.state == .ended {
+               playRecording()
+           }
+       }
+       
+       func playMovie(url: URL) {
+           let playerVC = AVPlayerViewController()
+           playerVC.player = AVPlayer(url: url)
+           
+           self.present(playerVC, animated: true, completion: nil)
+           
+           
+   //        player = AVPlayer(url: url)
+   //
+   //        let playerView = MoviePlayerView()
+   //        playerView.player = player
+   //
+   //        var topRect = view.bounds
+   //        topRect.size.height /= 4
+   //        topRect.size.width /= 4
+   //        topRect.origin.y = view.layoutMargins.top
+   //
+   //        playerView.frame = topRect
+   //        view.addSubview(playerView)
+   //
+   //        player.play()
+       }
+       
+       func playRecording() {
+           if let player = player {
+               player.seek(to: CMTime(seconds: 0, preferredTimescale: 600)) // N/D, D = 600
+               player.play()
+           }
+       }
+       
+       private func setupCamera() {
+           let camera = bestCamera()
+           let microphone = bestMicrophone()
+           
+           captureSession.beginConfiguration()
+           
+           guard let cameraInput = try? AVCaptureDeviceInput(device: camera) else {
+               preconditionFailure("Can't create an input from the camera, but we should do something better than crashing!")
+           }
+           
+           guard let microphoneInput = try? AVCaptureDeviceInput(device: microphone) else {
+               preconditionFailure("Can't create an input from the microphone, but we should do something better than crashing!")
+           }
+           
+           guard captureSession.canAddInput(cameraInput) else {
+               preconditionFailure("This session can't handle this type of input: \(cameraInput)")
+           }
+           captureSession.addInput(cameraInput)
+           
+           guard captureSession.canAddInput(microphoneInput) else {
+               preconditionFailure("This session can't handle this type of input: \(microphoneInput)")
+           }
+           captureSession.addInput(microphoneInput)
+           
+           if captureSession.canSetSessionPreset(.hd1920x1080) {
+               captureSession.sessionPreset = .hd1920x1080
+           }
+           
+           guard captureSession.canAddOutput(fileOutput) else {
+               preconditionFailure("This session can't handle this type of output: \(fileOutput)")
+           }
+           captureSession.addOutput(fileOutput)
+           
+           captureSession.commitConfiguration()
+           
+           cameraView.session = captureSession
+       }
+       
+       private func bestCamera() -> AVCaptureDevice {
+           if let device = AVCaptureDevice.default(.builtInUltraWideCamera, for: .video, position: .back) {
+               return device
+           }
+           if let device = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) {
+               return device
+           }
+           
+           preconditionFailure("No cameras on device match the specs that we need")
+       }
+       
+       private func bestMicrophone() -> AVCaptureDevice {
+           if let device = AVCaptureDevice.default(for: .audio) {
+               return device
+           }
+           
+           preconditionFailure("No microphones on device match the specs that we need")
+       }
+       
+       override func viewDidAppear(_ animated: Bool) {
+           super.viewDidAppear(animated)
+           
+           captureSession.startRunning()
+       }
+       
+       override func viewDidDisappear(_ animated: Bool) {
+           super.viewDidDisappear(animated)
+           
+           captureSession.stopRunning()
+       }
+
+       @IBAction func recordButtonPressed(_ sender: Any) {
+           if fileOutput.isRecording {
+               fileOutput.stopRecording()
+           } else {
+               fileOutput.startRecording(to: newRecordingURL(), recordingDelegate: self)
+           }
+       }
+       
+       /// Creates a new file URL in the documents directory
+       private func newRecordingURL() -> URL {
+           let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+
+           let formatter = ISO8601DateFormatter()
+           formatter.formatOptions = [.withInternetDateTime]
+
+           let name = formatter.string(from: Date())
+           let fileURL = documentsDirectory.appendingPathComponent(name).appendingPathExtension("mov")
+           return fileURL
+       }
+       
+       func updateViews() {
+           recordButton.isSelected = fileOutput.isRecording
+       }
+   }
+
+
+   extension VideoViewController: AVCaptureFileOutputRecordingDelegate {
+       
+       func fileOutput(_ output: AVCaptureFileOutput, didStartRecordingTo fileURL: URL, from connections: [AVCaptureConnection]) {
+           updateViews()
+       }
+       
+       func fileOutput(_ output: AVCaptureFileOutput, didFinishRecordingTo outputFileURL: URL, from connections: [AVCaptureConnection], error: Error?) {
+           if let error = error {
+               print("Error saving video: \(error)")
+           }
+           
+           print("Video URL: \(outputFileURL)")
+           updateViews()
+           playMovie(url: outputFileURL)
+       }
+   }
+
---
+++ Experiences/Experiences/View/CameraPreviewView.swift
@@ -0,0 +1,26 @@
+//
+//  CameraPreviewView.swift
+//  Experiences
+//
+//  Created by denis cedeno on 5/18/20.
+//  Copyright © 2020 DenCedeno Co. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class CameraPreviewView: UIView {
+    
+    override class var layerClass: AnyClass {
+        return AVCaptureVideoPreviewLayer.self
+    }
+    
+    var videoPlayerLayer: AVCaptureVideoPreviewLayer {
+        return layer as! AVCaptureVideoPreviewLayer
+    }
+    
+    var session: AVCaptureSession? {
+        get { return videoPlayerLayer.session }
+        set { videoPlayerLayer.session = newValue }
+    }
+}
