b6f7ce13deff53e462472660443a7263e620dc6e
Gerardo <gahernandez02@gmail.com>
2020-05-20 18:44:08 -0700

    Added View Controllers and added privacy settingsg in the plist

---
+++ Experiences/Experiences/Models/Experience.swift
@@ -0,0 +1,9 @@
+//
+//  Experience.swift
+//  Experiences
+//
+//  Created by Gerardo Hernandez on 5/20/20.
+//  Copyright © 2020 Gerardo Hernandez. All rights reserved.
+//
+
+import Foundation
---
+++ Experiences/Experiences/View Controllers/ExperiencesMapViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ExperiencesMapViewController.swift
+//  Experiences
+//
+//  Created by Gerardo Hernandez on 5/20/20.
+//  Copyright © 2020 Gerardo Hernandez. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ExperiencesMapViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
