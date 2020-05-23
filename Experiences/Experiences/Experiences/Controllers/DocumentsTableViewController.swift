1fa905f7006392b0b0e87381ff36d9f27c12fafe
Aaron Haughton <dreamcast2000@outlook.com>
2020-05-23 15:49:35 -0400

    initial Commit; Tab Bar Controller for documents and experiences set up.

---
+++ Experiences/Experiences/Experiences/Controllers/DocumentsTableViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DocumentsTableViewController.swift
+//  Experiences
+//
+//  Created by Joe on 5/23/20.
+//  Copyright © 2020 AlphaGradeINC. All rights reserved.
+//
+
+import UIKit
+
+class DocumentsTableViewController: UITableViewController {
+    override func viewDidLoad() {
+        view.backgroundColor = .systemBackground
+    }
+}
---
+++ Experiences/Experiences/Experiences/Controllers/ExperiencesMapViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ExperiencesMapViewController.swift
+//  Experiences
+//
+//  Created by Joe on 5/23/20.
+//  Copyright © 2020 AlphaGradeINC. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ExperiencesMapViewController: UIViewController {
+    
+}
