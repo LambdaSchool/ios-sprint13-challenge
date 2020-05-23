86fbf72c818d1b9123a807576313f1bfb597c3c0
Aaron Haughton <dreamcast2000@outlook.com>
2020-05-23 17:04:45 -0400

    Added MKAnnotation Extension

---
+++ Experiences/Experiences/Experiences/Helper/Extensions/URL+Ext.swift
@@ -0,0 +1,43 @@
+//
+//  URL+Ext.swift
+//  Experiences
+//
+//  Created by Joe on 5/23/20.
+//  Copyright © 2020 AlphaGradeINC. All rights reserved.
+//
+
+import Foundation
+
+extension URL {
+    static func makeNewImageURL(with title: String) -> URL {
+        guard let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { fatalError("Could not load documents directory") }
+        let fileURL = documentsDirectory.appendingPathComponent(title).appendingPathExtension("png")
+        return fileURL
+    }
+    static func makeNewAudioURL(with title: String) -> URL {
+        guard let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { fatalError("Could not load documents directory") }
+        let fileURL = documentsDirectory.appendingPathComponent(title).appendingPathExtension("caf")
+        return fileURL
+    }
+    static func makeNewVideoURL(with title: String) -> URL {
+        guard let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { fatalError("Could not load documents directory") }
+        let fileURL = documentsDirectory.appendingPathComponent(title).appendingPathExtension("mp4")
+        return fileURL
+    }
+    static func fetchAudioFromDocumentsDirectory(name: String) -> URL? {
+        let documentDirectory = FileManager.SearchPathDirectory.documentDirectory
+        let userDomainMask = FileManager.SearchPathDomainMask.userDomainMask
+        let paths = NSSearchPathForDirectoriesInDomains(documentDirectory, userDomainMask, true)
+        guard let dirPath = paths.first else { return nil }
+        let audioURL = URL(fileURLWithPath: dirPath).appendingPathComponent(name).appendingPathExtension("caf")
+        return audioURL
+    }
+    static func fetchVideoFromDocumentsDirectory(name: String) -> URL? {
+        let documentDirectory = FileManager.SearchPathDirectory.documentDirectory
+        let userDomainMask = FileManager.SearchPathDomainMask.userDomainMask
+        let paths = NSSearchPathForDirectoriesInDomains(documentDirectory, userDomainMask, true)
+        guard let dirPath = paths.first else { return nil }
+        let videoURL = URL(fileURLWithPath: dirPath).appendingPathComponent(name).appendingPathExtension("mp4")
+        return videoURL
+    }
+}
