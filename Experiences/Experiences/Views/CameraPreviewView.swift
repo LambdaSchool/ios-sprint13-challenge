b646fe7adb6461bf25ba6ca317ec6b4a5f3937e5
Gerardo <gahernandez02@gmail.com>
2020-05-26 16:11:46 -0700

    Added moviePlayerView in Video scene and added file.

---
+++ Experiences/Experiences/Views/CameraPreviewView.swift
@@ -0,0 +1,26 @@
+//
+//  CameraPreviewView.swift
+//  Experiences
+//
+//  Created by Gerardo Hernandez on 5/26/20.
+//  Copyright © 2020 Gerardo Hernandez. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class CameraPreviewView: UIView {
+    
+    override class var layerClass: AnyClass {
+           return AVPlayerLayer.self
+       }
+       
+       var videoPlayerLayer: AVPlayerLayer {
+           return layer as! AVPlayerLayer
+       }
+       
+       var player: AVPlayer? {
+           get { return videoPlayerLayer.player }
+           set { videoPlayerLayer.player = newValue }
+       }
+}
