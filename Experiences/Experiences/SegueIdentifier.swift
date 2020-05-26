65e53858cb1af55b2156c2d0ebf5c47190768ecd
Gerardo <gahernandez02@gmail.com>
2020-05-26 13:55:00 -0700

    Added Segue Identifiers in storyboard and created an enum in a utility file

---
+++ Experiences/Experiences/SegueIdentifier.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  Experiences
+//
+//  Created by Gerardo Hernandez on 5/26/20.
+//  Copyright © 2020 Gerardo Hernandez. All rights reserved.
+//
+
+import Foundation
+
+enum SegueIdentifiers: String {
+    case AddExperienceSegue
+    case AddImageSegue
+    case AddAudioSegue
+    case AddVideoSegue
+}
+
