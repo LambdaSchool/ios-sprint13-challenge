96f37e43b4509acf1bc79065621cdfdc6674ff9a
Gerardo <gahernandez02@gmail.com>
2020-05-26 13:46:56 -0700

    Added view controller files for adding audio, video, and images. Assigned the appropriate classes in storyboard.

---
+++ Experiences/Experiences/View Controllers/AddAudioViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddAudioViewController.swift
+//  Experiences
+//
+//  Created by Gerardo Hernandez on 5/26/20.
+//  Copyright © 2020 Gerardo Hernandez. All rights reserved.
+//
+
+import UIKit
+
+class AddAudioViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
