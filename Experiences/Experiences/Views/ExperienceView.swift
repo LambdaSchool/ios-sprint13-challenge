8f682abfa9f307dfdcbda850497a525bc1ae1370
Gerardo <gahernandez02@gmail.com>
2020-05-26 14:28:38 -0700

    Created audio visualizer file and created the view

---
+++ Experiences/Experiences/Views/ExperienceView.swift
@@ -0,0 +1,21 @@
+//
+//  ExperienceView.swift
+//  Experiences
+//
+//  Created by Gerardo Hernandez on 5/26/20.
+//  Copyright © 2020 Gerardo Hernandez. All rights reserved.
+//
+
+import UIKit
+
+class ExperienceView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
