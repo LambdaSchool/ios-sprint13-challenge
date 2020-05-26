601174a653642ec181bf5bba19ae8fac07895538
Gerardo <gahernandez02@gmail.com>
2020-05-26 13:27:36 -0700

    Created ExperienceController and assigned its delegate.

---
+++ Experiences/Experiences/Model Controller/ExperienceController.swift
@@ -0,0 +1,52 @@
+//
+//  ExperienceController.swift
+//  Experiences
+//
+//  Created by Gerardo Hernandez on 5/26/20.
+//  Copyright © 2020 Gerardo Hernandez. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+class ExperienceController: NSObject {
+    
+    var experiences: [Experience] = []
+    
+    private let locationManager = CLLocationManager()
+    private var location: CLLocationCoordinate2D?
+    
+    func createExperience(title: String, imageURL: URL? = nil, audioURL: URL? = nil, videoURL: URL? = nil) -> Experience {
+        
+        locationManager.requestLocation()
+        location = locationManager.location?.coordinate
+        
+        let experience = Experience(title: title,
+                                    imageURL: imageURL, videoURL: videoURL
+            , audioURL: audioURL, geotag: location)
+        
+        experiences.append(experience)
+        
+        return experience
+    }
+    
+    override init() {
+        super.init()
+        
+        locationManager.delegate = self
+    }
+}
+
+
+extension ExperienceController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            self.location = location.coordinate
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+       print("Failed to find user's location: \(error.localizedDescription)")
+    }
+}
--- Experiences/Experiences/Models/Experience.swift
+++ Experiences/Experiences/Models/Experience.swift
@@ -7,3 +7,68 @@
 //
 
 import Foundation
+import MapKit
+
+
+enum MediaType: String, CaseIterable {
+      case image, audio, video
+  }
+
+struct Location {
+    static let trickDog = CLLocationCoordinate2D(latitude: 37.7600297, longitude: 122.4109439)
+}
+
+class Experience: NSObject {
+    
+  
+    
+    var experienceTitle: String
+    var media: [Media]
+    //TODO: Add a
+    let geotag: CLLocationCoordinate2D!
+    let timestamp: Date
+    
+    init(title: String, imageURL: URL?, videoURL: URL?, audioURL: URL?, geotag: CLLocationCoordinate2D? = nil, timestamp: Date = Date()) {
+        self.experienceTitle = title
+        self.geotag = geotag ?? Location.trickDog
+        self.timestamp = timestamp
+        self.media = [Media(mediaType: .image, url: imageURL),
+                      Media(mediaType: .video, url: videoURL),
+                      Media(mediaType: .audio, url: audioURL )]
+        
+    }
+    
+    var storedMedia: [Media] {
+        media.filter { $0.url != nil}
+    }
+    
+    var storedMediaTypes: [String] {
+        storedMedia.compactMap { $0.mediaType.rawValue}
+    }
+    
+    struct Media {
+      
+        let mediaType: MediaType
+        let url: URL?
+    }
+}
+
+
+extension Experience: MKAnnotation {
+    
+    var coordinate: CLLocationCoordinate2D {
+        geotag
+    }
+    
+    var title: String? {
+        experienceTitle
+    }
+    
+    var subtitle: String? {
+        if storedMedia.count == 0 {
+           return "No Experiences."
+        } else {
+            return "Experience stored: \(storedMediaTypes.joined(separator: ", ."))"
+        }
+    }
+}
