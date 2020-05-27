b1042bbe2f099997a3cd80cfbf690df22e2b110b
Gerardo <gahernandez02@gmail.com>
2020-05-26 17:13:16 -0700

    Renamed file to the correct action intended.

--- Experiences/Experiences/View Controllers/AddExperienceViewController.swift
+++ Experiences/Experiences/View Controllers/AddExperienceViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class VideoPlaybackViewController: UIViewController {
+class AddExperienceViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
