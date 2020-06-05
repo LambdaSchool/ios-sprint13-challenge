fa8fec108b47fbfb348896fcd0f920725d208077
Bharat Kumar <bharatiosdeveloper@gmail.com>
2020-06-05 14:33:14 -0700

    Added required properties to the view controller file.

--- Experiences/Experiences/ViewControllers/ExperiencesViewController.swift
+++ Experiences/Experiences/ViewControllers/ExperiencesViewController.swift
@@ -13,17 +13,47 @@ import CoreImage.CIFilterBuiltins
 import Photos
 
 class ExperiencesViewController: UIViewController {
-
+    
     // MARK: - Properites
     var experienceController: ExperienceController?
     var delegate: MapViewController?
     private var inited = false
-
-    // If this object exists, it's a view/update situation.
-   
-
+    
+    private var audioClip: URL?
+    private var audioRecorder: AVAudioRecorder?
+    
+    var audioPlayer: AVAudioPlayer? {
+        didSet {
+            
+            audioPlayer?.delegate = self
+        }
+    }
+    lazy private var captureSession = AVCaptureSession()
+    lazy private var fileOutput = AVCaptureMovieFileOutput()
+    
+    private var videoClip: URL?
+    private var player: AVPlayer!
+    let context = CIContext(options: nil)
+    
+    var originalImage: UIImage? {
+           didSet {
+               // resize the scaledImage and set it view
+               guard let originalImage = originalImage else { return }
+               // Height and width
+               var scaledSize = imageView.bounds.size
+               let scale = UIScreen.main.scale  // Size of pixels on this device: 1x, 2x, or 3x
+               scaledSize = CGSize(width: scaledSize.width * scale, height: scaledSize.height * scale)
+               scaledImage = originalImage.imageByScaling(toSize: scaledSize)
+           }
+       }
+    
+    var scaledImage: UIImage? 
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-}
+    }
+    
     
 }
