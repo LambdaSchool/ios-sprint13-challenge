879036ae7bb084f429d0edc770ef267d425c3637
Mark Poggi <markp@mac.com>
2020-06-05 09:49:27 -0700

    Finished experiences model to include all components of an experience.

--- Experiences/Experiences/Models/Experience.swift
+++ Experiences/Experiences/Models/Experience.swift
@@ -12,9 +12,16 @@ import CoreLocation
 
 class Experience: NSObject, MKAnnotation {
     var coordinate: CLLocationCoordinate2D
+    var title: String?
+    var image: UIImage?
+    var audio: URL?
+    var video: URL?
 
-
-    init(coordinate: CLLocationCoordinate2D) {
+    init(coordinate: CLLocationCoordinate2D, title: String?, image: UIImage?, audio: URL?, video: URL?) {
         self.coordinate = coordinate
+        self.title = title
+        self.image = image
+        self.audio = audio
+        self.video = video
     }
 }
