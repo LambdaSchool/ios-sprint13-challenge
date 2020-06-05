9c6a0eb825b13968e5a43508548e9c2fad60ec36
Shawn James <shawn.james@me.com>
2020-06-05 17:38:13 -0400

    UI Updates
    MapView, UIBarButtonItems added

--- Experiences/Experiences/ViewControllers/ViewController.swift
+++ Experiences/Experiences/ViewControllers/ViewController.swift
@@ -9,12 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+        
+    @IBOutlet weak var plusButton: UIBarButtonItem!
+    
+    lazy var searchBar = UISearchBar(frame: CGRect(x: 0, y: 0, width: self.view.frame.width * 0.8 , height: 20))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupInitialViews()
     }
 
+    private func setupInitialViews() {
+        setupSearchBar()
+    }
 
+    private func setupSearchBar() {
+        searchBar.placeholder = "Search for a memory..."
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(customView: searchBar)
+    }
+    
+    @IBAction func plusButtonTapped(_ sender: UIBarButtonItem) {
+    }
+    
+    
 }
 
