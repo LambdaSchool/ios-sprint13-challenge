4c2780d2463a5a891337024d2e26262fea8ceadc
Kenny Dubroff <kenny.dubroff@kennydubroff.com>
2020-06-05 07:23:29 -0700

    Added Icons

--- Experiences/Experiences/Controller/View Controllers/MainMenuViewController.swift
+++ Experiences/Experiences/Controller/View Controllers/MainMenuViewController.swift
@@ -10,28 +10,40 @@ import UIKit
 import MapKit
 
 class MainMenuViewController: UIViewController {
-    @IBOutlet weak var mapView: MKMapView!
-    @IBOutlet weak var tableView: UITableView!
-
     // MARK: - Properties -
     private let cellID = "MenuCell"
     private let menuArray = [
-        "Video",
-        "Photo",
-        "Audio",
-        "Story"
+        UIImage.NamedImage.video,
+        UIImage.NamedImage.photo,
+        UIImage.NamedImage.story
     ]
 
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var tableView: UITableView!
+
+    // MARK: - View Lifecycle -
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        for cell in tableView.visibleCells {
+            cell.isSelected = false
+        }
+    }
+
 }
 
 extension MainMenuViewController: UITableViewDelegate {
-
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let cell = tableView.cellForRow(at: indexPath) as? MenuTableViewCell,
+            let title = cell.title
+        else { return }
+        performSegue(withIdentifier: "\(title)Segue", sender: nil)
+    }
 }
 
 extension MainMenuViewController: UITableViewDataSource {
@@ -43,7 +55,10 @@ extension MainMenuViewController: UITableViewDataSource {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: cellID) as? MenuTableViewCell else {
             fatalError("check cell ID stoobid")
         }
-        cell.title = menuArray[indexPath.row]
+        let backgroundView = UIView()
+        backgroundView.backgroundColor = .blue
+        cell.selectedBackgroundView = backgroundView
+        cell.title = menuArray[indexPath.row].rawValue.capitalized(with: .current)
         return cell
     }
 }
--- Experiences/Experiences/View/Custom Views/MenuTableViewCell.swift
+++ Experiences/Experiences/View/Custom Views/MenuTableViewCell.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class MenuTableViewCell: UITableViewCell {
-    @IBOutlet weak var titleLabel: UILabel!
+    // MARK: - Properties -
+
     ///add 4 points of spacing to the bottom of each cell
     lazy var newFrame = self.layer.frame.inset(by: UIEdgeInsets(top: 0, left: 0, bottom: 4, right: 0));
 
@@ -19,21 +20,32 @@ class MenuTableViewCell: UITableViewCell {
         }
     }
 
+    lazy var iconImage: UIImage? = {
+        guard let title = title,
+            let iconResourceName = UIImage.NamedImage(rawValue: title.lowercased())
+        else { return nil }
+        return UIImage.withName(iconResourceName)
+    }()
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var iconImageView: UIImageView!
+
+    // MARK: - View Lifecycle -
     private func updateViews() {
-        guard let title = title else { return }
         self.layer.cornerRadius = 8
         self.layer.borderColor = UIColor.black.cgColor
         self.layer.borderWidth = 2
+        guard let title = title else { return }
         titleLabel.text = title
+
+        guard let image = iconImage else { return }
+        iconImageView.image = image
+
     }
 
     override func layoutSubviews() {
         super.layoutSubviews()
         self.layer.frame = newFrame;
-    }
-
-    @IBAction func addExperienceButtonTapped(_ sender: Any) {
-        print(sender)
     }
 
 }
