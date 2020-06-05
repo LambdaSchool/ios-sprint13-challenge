c6b341cd76b3240a39955e84be337c6831cf7589
Bharat Kumar <bharatiosdeveloper@gmail.com>
2020-06-05 12:54:37 -0700

    Added experience model and controller

---
+++ Experiences/Experiences/ExperienceController.swift
@@ -0,0 +1,37 @@
+//
+//  ExperienceController.swift
+//  Experiences
+//
+//  Created by Bhawnish Kumar on 6/5/20.
+//  Copyright © 2020 Bhawnish Kumar. All rights reserved.
+//
+import Foundation
+import UIKit
+
+class ExperienceController {
+var experiences: [Experience] = []
+
+
+func createExperience(title: String?,
+                      audioClip: URL?,
+                      image: UIImage?,
+                      videoClip: URL?,
+                      latitude: Double = 0.0,
+                      longitude: Double = 0.0) {
+    
+    let newExperience = Experience()
+    
+    newExperience.title = title
+    newExperience.audioClip = audioClip
+    newExperience.image = image
+    newExperience.videoClip = videoClip
+    newExperience.latitude = latitude
+    newExperience.longitude = longitude
+    
+    experiences.append(newExperience)
+    
+}
+    
+}
+
+
