c049988c24121a8cd5c0248e8fc917bcd26679fe
Mark Poggi <markp@mac.com>
2020-06-05 09:42:44 -0700

    Created view controller files and associated them with view controllers in storyboard.

---
+++ Experiences/Experiences/View Controllers/VideoCameraViewController.swift
@@ -0,0 +1,30 @@
+//
+//  VideoCameraViewController.swift
+//  Experiences
+//
+//  Created by Mark Poggi on 6/5/20.
+//  Copyright © 2020 Mark Poggi. All rights reserved.
+//
+
+import UIKit
+
+class VideoCameraViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
