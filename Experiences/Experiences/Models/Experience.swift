0d6b6a7a207307f20591d828e684129fa2882525
Chris Dobek <dobek.christopher@gmail.com>
2020-06-07 09:03:58 -0400

    Add Experience struct

---
+++ Experiences/Experiences/Models/Experience.swift
@@ -0,0 +1,18 @@
+//
+//  Experience.swift
+//  Experiences
+//
+//  Created by Chris Dobek on 6/7/20.
+//  Copyright © 2020 Chris Dobek. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import AVFoundation
+
+struct Experience {
+    let title: String
+    let location: CLLocation
+    let image: UIImage?
+    let audio: URL?
+}
