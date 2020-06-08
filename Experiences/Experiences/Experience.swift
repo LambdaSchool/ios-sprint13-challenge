4a30dbc606cf7e79d3633f0b9725a64eea929a15
Hunter Oppel <hunter.oppel@gmail.com>
2020-06-08 04:42:14 -0400

    Added a lot of the functionality to add a new experience

--- Experiences/Experiences/Experience.swift
+++ Experiences/Experiences/Experience.swift
@@ -12,14 +12,16 @@ class Experience: NSObject {
     let title: String?
     let details: String?
     let audioURL: URL?
+    let image: Data?
     
     let longitude: Double
     let latitude: Double
     
-    init(title: String, details: String?, audioURL: URL?, longitude: Double, latitude: Double) {
+    init(title: String, details: String?, audioURL: URL?, image: Data?, longitude: Double, latitude: Double) {
         self.title = title
         self.details = details
         self.audioURL = audioURL
+        self.image = image
         self.longitude = longitude
         self.latitude = latitude
     }
--- Experiences/Experiences/NewExperienceViewController.swift
+++ Experiences/Experiences/NewExperienceViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import MapKit
+import AVFoundation
 
 protocol NewExperienceDelegate {
     func didAddNewExperience(_ experience: Experience) -> Void
@@ -14,11 +16,106 @@ protocol NewExperienceDelegate {
 
 class NewExperienceViewController: UIViewController {
     
+    let audioRecorderController = AudioRecorderController()
+    
     var delegate: NewExperienceDelegate?
+    var audioURL: URL?
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var detailsTextView: UITextView!
+    @IBOutlet weak var recordAudioButton: UIButton!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    
+    @IBAction func toggleRecording(_ sender: Any) {
+        toggleRecording()
+    }
+    
+    @IBAction func saveExperience(_ sender: Any) {
+        saveExperience()
+    }
+    
+    private func toggleRecording() {
+        audioRecorderController.toggleRecording()
+        audioRecorderController.audioRecorder?.delegate = self
+        updateViews()
+    }
+    
+    private func saveExperience() {
+        guard let title = titleTextField.text,
+            !title.isEmpty else {
+                
+                return
+        }
+        
+        let location = CLLocation()
+        let longitude = location.coordinate.longitude
+        let latitude = location.coordinate.latitude
+        
+        let experience = Experience(title: title,
+                                    details: detailsTextView.text,
+                                    audioURL: audioURL,
+                                    image: imageView.image?.pngData(),
+                                    longitude: longitude,
+                                    latitude: latitude)
+        
+        delegate?.didAddNewExperience(experience)
+        
+        dismiss(animated: true)
+    }
+    
+    private func updateViews() {
+        let audioRecorder = audioRecorderController.audioRecorder
+        
+        recordAudioButton.isSelected = audioRecorder?.isRecording ?? false
+        
+        switch recordAudioButton.isSelected {
+        case true:
+            saveButton.isEnabled = false
+        case false:
+            saveButton.isEnabled = true
+        }
+    }
+}
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+extension NewExperienceViewController: AVAudioRecorderDelegate {
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        if let recordingURL = audioRecorderController.recordingURL {
+            print("Finished recording: \(recordingURL.path)")
+            
+            audioURL = recordingURL
+        } else {
+            print("Did not successfully finish recording")
+        }
+        
+        DispatchQueue.main.async {
+            self.updateViews()
+        }
+    }
+    
+    func audioRecorderEncodeErrorDidOccur(_ recorder: AVAudioRecorder, error: Error?) {
+        if let error = error {
+            NSLog("Error occured during recording: \(error)")
+        }
+        
+        DispatchQueue.main.async {
+            self.updateViews()
+        }
+    }
+}
 
-        // Do any additional setup after loading the view.
+extension NewExperienceViewController: AudioRecorderControllerDelegate {
+    func didNotRecievePermission() {
+        print("Microphone access has been blocked.")
+        
+        let alertController = UIAlertController(title: "Microphone Access Denied", message: "Please allow this app to access your Microphone.", preferredStyle: .alert)
+        
+        alertController.addAction(UIAlertAction(title: "Open Settings", style: .default) { (_) in
+            UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
+        })
+        
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .default, handler: nil))
+        
+        present(alertController, animated: true)
     }
 }
