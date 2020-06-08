7b8f45606c63ef12ae431a137432cb2b01fd05c8
Ezra Black <dev.ezrablack@gmail.com>
2020-06-08 00:14:51 -0500

    Audio Implementation
    Added Audio Features. 
    
    Added a recording controller and audio controller to allow the audio to be passed easier. 
    added a Audio View controller that controls the audio functionality.

--- Truthly/Truthly/Views/AudioViewController.swift
+++ Truthly/Truthly/Views/AudioViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 protocol AudioDelegate {
     func audioButtonTapped()
@@ -14,24 +15,118 @@ protocol AudioDelegate {
 
 class AudioViewController: UIViewController {
     
-    var postController: PostController?
-    var delegate: AudioDelegate?
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
     
+    var delegate: AudioDelegate?
+    var postController: PostController?
+    let audioController = AudioController()
+    let recordingController = RecordingController()
+    var audioData: Data?
 
-    /*
-    // MARK: - Navigation
+        @IBOutlet weak var recordingAudioButton: UIButton!
+        @IBOutlet weak var playbackAudioButton: UIButton!
+        @IBOutlet weak var timeLabel: UILabel!
+        @IBOutlet weak var timeRemainingLabel: UILabel!
+        @IBOutlet weak var timeSlider: UISlider!
+        @IBOutlet weak var postButton: UIBarButtonItem!
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        private lazy var timeFormatter: DateComponentsFormatter = {
+            let formatting = DateComponentsFormatter()
+            formatting.unitsStyle = .positional // 00:00  mm:ss
+            // NOTE: DateComponentFormatter is good for minutes/hours/seconds
+            // DateComponentsFormatter not good for milliseconds, use DateFormatter instead)
+            formatting.zeroFormattingBehavior = .pad
+            formatting.allowedUnits = [.minute, .second]
+            return formatting
+        }()
+
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            postButton.isEnabled = false
+            postButton.tintColor = UIColor.gray
+
+            timeLabel.font = UIFont.monospacedDigitSystemFont(ofSize: timeLabel.font.pointSize,
+                                                              weight: .regular)
+            timeRemainingLabel.font = UIFont.monospacedDigitSystemFont(ofSize: timeRemainingLabel.font.pointSize,
+                                                                       weight: .regular)
+
+            updateViews()
+
+        }
+
+        private func updateViews() {
+            let playButtonTitle = audioController.isPlaying ? "Pause" : "Playback Recording"
+            playbackAudioButton.setTitle(playButtonTitle, for: .normal)
+
+            let elapsedTime = audioController.audioPlayer?.currentTime ?? 0
+            let duration = audioController.audioPlayer?.duration ?? 0
+            timeLabel.text = timeFormatter.string(from: elapsedTime)
+
+            timeSlider.minimumValue = 0
+            timeSlider.maximumValue = Float(duration)
+            timeSlider.value = Float(elapsedTime)
+
+            let recordButtonTitle = recordingController.isRecording ? "Stop" : "Start Recording"
+            recordingAudioButton.setTitle(recordButtonTitle, for: .normal)
+
+            let timeRemaining = duration - elapsedTime
+            timeRemainingLabel.text = timeFormatter.string(from: timeRemaining)
+        }
+
+        func playbackAudioRecording() {
+            guard let recordURL = recordingController.recordURL else { return }
+            do {
+                audioData = try Data(contentsOf: recordURL)
+                audioController.loadAudio(url: recordURL)
+                audioController.audioPlayer?.delegate = self
+                postButton.isEnabled = true
+                postButton.tintColor = UIColor.link
+            } catch {
+                print("Cant create audio data")
+                return
+            }
+            audioController.playPause()
+        }
+
+        private func postAudio() {
+            view.endEditing(true)
+
+            guard let _ = audioData else { return }
+            let title = "Audio Post"
+            self.postController?.createPost(with: title, ofType: .audio, location: nil)
+            delegate?.audioButtonTapped()
+            dismiss(animated: true, completion: nil)
+        }
+
+        @IBAction func playbackAudioTapped(_ sender: UIButton) {
+            playbackAudioRecording()
+            updateViews()
+        }
+
+        @IBAction func recordAudioTapped(_ sender: UIButton) {
+            recordingController.recordToggle()
+            updateViews()
+        }
+
+        @IBAction func postButtonTapped(_ sender: Any) {
+            postAudio()
+        }
+
+        @IBAction func cancelButtonTapped(_ sender: Any) {
+            dismiss(animated: true, completion: nil)
+        }
     }
-    */
 
-}
+
+extension AudioViewController: AVAudioPlayerDelegate {
+        
+        func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+            updateViews()
+            print("Finished Playing")
+        }
+
+        func audioPlayerDecodeErrorDidOccur(_ player: AVAudioPlayer, error: Error?) {
+            if let error = error {
+                print("Audio Player error: \(error)")
+            }
+        }
+    }
