8cb2b77be0d484606a2b979f497ae24365a11914
Dahna <dbuenrostro09@gmail.com>
2020-07-10 13:49:26 -0400

    Implementing recording

--- Experiences/Experiences/AddExperienceViewController.swift
+++ Experiences/Experiences/AddExperienceViewController.swift
@@ -12,6 +12,7 @@ import AVFoundation
 import CoreImage
 import CoreImage.CIFilterBuiltins
 import Photos
+import MapKit
 
 class AddExperienceViewController: UIViewController {
     
@@ -19,10 +20,27 @@ class AddExperienceViewController: UIViewController {
     
     private let context = CIContext(options: nil)
     
+    weak var timer: Timer?
+    var recordingURL: URL?
+    var audioRecorder: AVAudioRecorder?
+    
+    var isRecording: Bool {
+        audioRecorder?.isRecording ?? false
+    }
+    
+    var audioPlayer: AVAudioPlayer? {
+        didSet {
+            guard let audioPlayer = audioPlayer else { return }
+            audioPlayer.delegate = self
+            audioPlayer.isMeteringEnabled = true
+        }
+    }
+    
     // MARK: Outlets
     
     @IBOutlet var titleTextField: UITextField!
     @IBOutlet var imageView: UIImageView!
+    @IBOutlet var recordButton: UIButton!
     
     // MARK: Actions
     
@@ -32,7 +50,11 @@ class AddExperienceViewController: UIViewController {
     
     
     @IBAction func recordButtonTapped(_ sender: Any) {
-        
+        if isRecording {
+            stopRecording()
+        } else {
+            startRecording()
+        }
     }
     
     
@@ -40,27 +62,36 @@ class AddExperienceViewController: UIViewController {
         
     }
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func updateViews() {
+        
+        if isRecording {
+            recordButton.setTitle("Stop Recording", for: .normal)
+        } else {
+            recordButton.setTitle("Record", for: .normal)
+        }
     }
-    */
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
     
     // MARK: Image
     
-     func image(byFiltering image: UIImage) -> UIImage {
+    func image(byFiltering image: UIImage) -> UIImage {
         guard let cgImage = image.cgImage else { return image}
         
         let ciImage = CIImage(cgImage: cgImage)
@@ -76,6 +107,8 @@ class AddExperienceViewController: UIViewController {
         return UIImage(cgImage: outputCGImage)
     }
     
+    
+    
     func presentImagePicker() {
         guard UIImagePickerController.isSourceTypeAvailable(.photoLibrary) else { return }
         
@@ -85,6 +118,91 @@ class AddExperienceViewController: UIViewController {
         imagePicker.delegate = self
         
         present(imagePicker, animated: true, completion: nil)
+    }
+    
+    // MARK: - Recording
+    
+    // func createNewRecordingURL(with Name: String) -> URL {
+    func createNewRecordingURL() -> URL {
+        let documents = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        
+        let name = ISO8601DateFormatter.string(from: Date(), timeZone: .current, formatOptions: .withInternetDateTime)
+        let file = documents.appendingPathComponent(name, isDirectory: false).appendingPathExtension("caf")
+        
+        print("recording URL: \(file)")
+        
+        return file
+    }
+    
+    
+    func requestPermissionOrStartRecording() {
+        switch AVAudioSession.sharedInstance().recordPermission {
+        case .undetermined:
+            AVAudioSession.sharedInstance().requestRecordPermission { granted in
+                guard granted == true else {
+                    print("We need microphone access")
+                    return
+                }
+                
+                print("Recording permission has been granted!")
+                // NOTE: Invite the user to tap record again, since we just interrupted them, and they may not have been ready to record
+            }
+        case .denied:
+            print("Microphone access has been blocked.")
+            
+            let alertController = UIAlertController(title: "Microphone Access Denied", message: "Please allow this app to access your Microphone.", preferredStyle: .alert)
+            
+            alertController.addAction(UIAlertAction(title: "Open Settings", style: .default) { (_) in
+                UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
+            })
+            
+            alertController.addAction(UIAlertAction(title: "Cancel", style: .default, handler: nil))
+            
+            present(alertController, animated: true, completion: nil)
+        case .granted:
+            startRecording()
+        @unknown default:
+            break
+        }
+    }
+    
+    
+    func startRecording() {
+        // Grab the recording URL
+        recordingURL = createNewRecordingURL()
+        
+        // Check for permission
+        AVAudioSession.sharedInstance().requestRecordPermission { granted in
+            guard granted else {
+                NSLog("We need permission to access the microphone")
+                return
+            }
+            
+            // Set up the recorder (give it the settings we want, etc.)
+            guard let recordingURL = self.recordingURL else {
+                NSLog("No recording URL available")
+                return
+            }
+            
+            let format = AVAudioFormat(standardFormatWithSampleRate: 44_100, channels: 1)!
+            
+            do {
+                self.audioRecorder = try AVAudioRecorder(url: recordingURL, format: format)
+                // Start recording
+                self.audioRecorder?.delegate = self
+                self.audioRecorder?.isMeteringEnabled = true
+                self.audioRecorder?.record()
+                self.updateViews()
+            } catch {
+                NSLog("Error setting up audio recorder: \(error)")
+            }
+        }
+    }
+    
+    func stopRecording() {
+        audioRecorder?.stop()
+        audioRecorder = nil
+        self.updateViews()
     }
 }
 
@@ -100,5 +218,36 @@ extension AddExperienceViewController: UIImagePickerControllerDelegate, UINaviga
     
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         dismiss(animated: true, completion: nil)
+    }
+}
+
+extension AddExperienceViewController: AVAudioRecorderDelegate {
+    
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        if let recordingURL = recordingURL {
+            audioPlayer = try? AVAudioPlayer(contentsOf: recordingURL)
+            self.audioRecorder?.delegate = self
+            self.recordingURL = nil
+            updateViews()
+        }
+    }
+    
+    func audioRecorderEncodeErrorDidOccur(_ recorder: AVAudioRecorder, error: Error?) {
+        if let error = error {
+            NSLog("Error recording audio: \(error)")
+        }
+    }
+}
+
+extension AddExperienceViewController: AVAudioPlayerDelegate {
+    
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        updateViews()
+    }
+    
+    func audioPlayerDecodeErrorDidOccur(_ player: AVAudioPlayer, error: Error?) {
+        if let error = error {
+            NSLog("Error playing audio: \(error)")
+        }
     }
 }
---
+++ Experiences/Experiences/ExperienceController.swift
@@ -0,0 +1,17 @@
+//
+//  ExperienceController.swift
+//  Experiences
+//
+//  Created by Dahna on 7/10/20.
+//  Copyright © 2020 Dahna Buenrostro. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+class ExperienceController {
+     
+    var experiences = [Experience]()
+    
+    
+}
