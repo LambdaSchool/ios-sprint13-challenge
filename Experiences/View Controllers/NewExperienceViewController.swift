9e7ff6e24b8714d671383f070886c9bd7d2a56d7
Chad Parker <chad@chadparker.com>
2020-07-17 19:24:50 -0700

    Added UIImagePickerController on button/imageview

--- Experiences/View Controllers/NewExperienceViewController.swift
+++ Experiences/View Controllers/NewExperienceViewController.swift
@@ -13,6 +13,7 @@ class NewExperienceViewController: UIViewController {
     // MARK: - Properties
 
     @IBOutlet var titleTextField: UITextField!
+    @IBOutlet var imageView: UIImageView!
 
 
     // MARK: - View Lifecycle
@@ -20,11 +21,37 @@ class NewExperienceViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    
+
+
+    // MARK: - Actions
+
+    @IBAction func addPosterImage(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.modalPresentationStyle = .currentContext
+        imagePicker.delegate = self
+        self.present(imagePicker, animated: true, completion: nil)
+    }
+
 
     // MARK: - Navigation
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
 
+    }
+}
+
+
+// MARK: - ImagePicker
+
+extension NewExperienceViewController: UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+
+        dismiss(animated: true, completion: nil)
+    }
+
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
     }
 }
