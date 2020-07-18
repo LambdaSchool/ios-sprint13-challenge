ba000ee4e17fb5dffcaa1f538108cd419ce60635
Kevin Stewart <kistewart419@gmail.com>
2020-07-17 21:15:25 -0400

    Organized folders and created model for an experience

---
+++ Experiences/Experiences/Model/ExperienceModel.swift
@@ -0,0 +1,15 @@
+//
+//  ExperienceModel.swift
+//  Experiences
+//
+//  Created by Kevin Stewart on 7/17/20.
+//  Copyright © 2020 Kevin Stewart. All rights reserved.
+//
+
+import Foundation
+
+struct Experience {
+    var title: String
+    var image: URL
+    var audioRecording: URL
+}
