b609d6b3e7404f183a7bfd2c709a2bdf5c558cb9
Chad Parker <chad@chadparker.com>
2020-07-17 19:50:38 -0700

    Create basic Experience model object

---
+++ Experiences/Model/Experience.swift
@@ -0,0 +1,14 @@
+//
+//  Experience.swift
+//  Experiences
+//
+//  Created by Chad Parker on 7/17/20.
+//  Copyright © 2020 Chad Parker. All rights reserved.
+//
+
+import UIKit
+
+struct Experience {
+    let title: String
+    let image: UIImage?
+}
