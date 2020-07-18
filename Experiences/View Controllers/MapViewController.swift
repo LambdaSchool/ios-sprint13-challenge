517920947f130d459058110f0ce8927ec203f5fa
Chad Parker <chad@chadparker.com>
2020-07-17 19:52:31 -0700

    Add CLLocationManager to MapVC, authorize, get location, zoom map

--- Experiences/View Controllers/MapViewController.swift
+++ Experiences/View Controllers/MapViewController.swift
@@ -15,17 +15,45 @@ class MapViewController: UIViewController {
 
     @IBOutlet var mapView: MKMapView!
 
+    private let locationManager = CLLocationManager()
+
 
     // MARK: - View Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.delegate = self
+        locationManager.startUpdatingLocation()
     }
+
+
+    // MARK: - Location
+
+
 
 
     // MARK: - Navigation
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    }
+}
 
+
+// MARK: - Location Delegate
+
+extension MapViewController: CLLocationManagerDelegate {
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.last! as CLLocation
+        let currentLocation = location.coordinate
+        let coordinateRegion = MKCoordinateRegion(center: currentLocation, latitudinalMeters: 8000, longitudinalMeters: 8000)
+        mapView.setRegion(coordinateRegion, animated: true)
+        locationManager.stopUpdatingLocation()
+    }
+
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+       print(error.localizedDescription)
     }
 }
