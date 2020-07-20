57c10ef00ec8539981a757829f2afe90ee304e98
Kevin Stewart <kistewart419@gmail.com>
2020-07-20 16:28:57 -0400

    To Do: Add experiences to map

--- Experiences/Experiences/Views/ExperienceTableViewController.swift
+++ Experiences/Experiences/Views/ExperienceTableViewController.swift
@@ -8,93 +8,94 @@
 
 import UIKit
 
-    var experiences: [Experience] = []
+var experiences: [Experience] = []
 
 class ExperienceTableViewController: UITableViewController {
-
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         tableView.reloadData()
     }
-
+    
     // MARK: - Table view data source
-//
-//    override func numberOfSections(in tableView: UITableView) -> Int {
-//        // #warning Incomplete implementation, return the number of sections
-//        return
-//
-//    }
-
+    //
+    //    override func numberOfSections(in tableView: UITableView) -> Int {
+    //        // #warning Incomplete implementation, return the number of sections
+    //        return
+    //
+    //    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         print(experiences.count)
         return experiences.count
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ExperienceCell", for: indexPath)
-
+        
         let experience = experiences[indexPath.row]
         cell.textLabel?.text = experience.title
         cell.detailTextLabel?.text = "\(Date())"
-        print(cell.textLabel)
-
+        
         return cell
     }
     
-
+    
     /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
     /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
+     // Override to support editing the table view.
+     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+     if editingStyle == .delete {
+     // Delete the row from the data source
+     tableView.deleteRows(at: [indexPath], with: .fade)
+     } else if editingStyle == .insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
     /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
     /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
     
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "RecordingSegue" {
-        if let detailVC = segue.destination as? RecordingViewController,
-            let indexPath = tableView.indexPathForSelectedRow {
-            detailVC.delegate = experiences[indexPath.row] as? AddRecordingDelegate
+            if let detailVC = segue.destination as? RecordingViewController,
+                let indexPath = tableView.indexPathForSelectedRow {
+                detailVC.delegate = experiences[indexPath.row] as? AddRecordingDelegate
+            } else if segue.identifier == "ExperiencePinSegue" {
+                if let detailVC = segue.destination as? RecordingViewController,
+                    let indexPath = tableView.indexPathForSelectedRow {
+                    detailVC.delegate = experiences[indexPath.row] as? AddRecordingDelegate
+                }
+            }
         }
     }
-    
-
 }
-}
-
 extension ExperienceTableViewController: AddRecordingDelegate {
     func recordingeWasAdded(_ experience: Experience) {
         experiences.append(experience)
