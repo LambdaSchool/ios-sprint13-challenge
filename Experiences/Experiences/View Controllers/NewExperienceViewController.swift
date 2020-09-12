3da9d9fa868543a7875c3e63fc354038a7caaf42
Waseem Idelbi <idelbiw@gmail.com>
2020-09-11 22:21:20 -0400

    added a done button for the keyboard

--- Experiences/Experiences/View Controllers/NewExperienceViewController.swift
+++ Experiences/Experiences/View Controllers/NewExperienceViewController.swift
@@ -24,7 +24,7 @@ class NewExperienceViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        titleTextField.addDoneButtonOnKeyboard()
     }
     
     private func presentImagePickerController() {
@@ -102,4 +102,37 @@ extension NewExperienceViewController: UIImagePickerControllerDelegate, UINaviga
         picker.dismiss(animated: true, completion: nil)
     }
 
+}
+
+extension UITextField {
+    
+    @IBInspectable var doneAccessory: Bool {
+        
+        get {
+            return self.doneAccessory
+        }
+        set (hasDone) {
+            if hasDone{
+                addDoneButtonOnKeyboard()
+            }
+        }
+    }
+    
+    func addDoneButtonOnKeyboard() {
+        let doneToolbar: UIToolbar = UIToolbar(frame: CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+        doneToolbar.barStyle = .default
+        
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let done: UIBarButtonItem = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(self.doneButtonAction))
+        
+        let items = [flexSpace, done]
+        doneToolbar.items = items
+        doneToolbar.sizeToFit()
+        
+        self.inputAccessoryView = doneToolbar
+    }
+    
+    @objc func doneButtonAction() {
+        self.resignFirstResponder()
+    }
 }
