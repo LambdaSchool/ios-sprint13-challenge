7ac8b5afcbb0c58c0b4f0ae2f5cdc5a07e296bd8
Waseem Idelbi <idelbiw@gmail.com>
2020-09-11 23:34:34 -0400

    started working on the model

---
+++ Experiences/Experiences/Models/Experience.swift
@@ -0,0 +1,39 @@
+//
+//  Experience.swift
+//  Experiences
+//
+//  Created by Waseem Idelbi on 9/11/20.
+//
+
+import UIKit
+import MapKit
+
+//class Experience {
+//
+//    //This is whatever the user decides to call the experience
+//    let experienceTitle: String
+//
+//    //This is the image the user chooses for the experience
+//    let image: UIImage
+//
+//    //The URL will contain the audio recording
+//    let audioRecordingURL: URL
+//
+//    init(title: String, image: UIImage, audioRecordingURL: URL) {
+//        self.experienceTitle = title
+//        self.image = image
+//        self.audioRecordingURL = audioRecordingURL
+//    }
+//}
+//
+//extension Experience: MKAnnotation {
+//
+//    var coordinate: CLLocationCoordinate2D {
+//        CLLocationCoordinate2D(latitude: latitude, longitude: longitude )
+//    }
+//
+//    var title: String? { experienceTitle }
+//
+//    var subtitle: String? { "Magnitude: \(magnitude)" }
+//
+//}
