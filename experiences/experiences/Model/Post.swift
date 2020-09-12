67325482edcc724a0abdd074eac7155a4e02241f
Clayton Watkins <watkinclayton17@yahoo.com>
2020-09-11 18:35:15 -0700

    Created model object

---
+++ experiences/experiences/Model/Post.swift
@@ -0,0 +1,29 @@
+//
+//  Post.swift
+//  experiences
+//
+//  Created by Clayton Watkins on 9/11/20.
+//  Copyright © 2020 Clayton Watkins. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class Post: NSObject {
+    
+    let image: UIImage?
+    let author: String
+    let title: String?
+    let audioURL: URL?
+    let location: CLLocationCoordinate2D
+    let entry: String?
+    
+    init(title: String?, author: String, location: CLLocationCoordinate2D, image: UIImage?, entry: String?, audioURL: URL?) {
+        self.title = title ?? nil
+        self.author = author
+        self.location = location
+        self.image = image ?? nil
+        self.entry = entry ?? nil
+        self.audioURL = audioURL ?? nil
+    }
+}
