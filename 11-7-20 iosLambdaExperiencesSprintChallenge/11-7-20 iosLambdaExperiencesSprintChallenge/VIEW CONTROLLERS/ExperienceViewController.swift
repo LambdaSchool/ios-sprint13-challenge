d712977f0ffc1960781da5ca4e347de66ab6d408
brys <brys1saclaus2@gmail.com>
2020-11-07 11:26:51 -0800

    storyboard layed out

---
+++ 11-7-20 iosLambdaExperiencesSprintChallenge/11-7-20 iosLambdaExperiencesSprintChallenge/VIEW CONTROLLERS/ExperienceViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ExperienceViewController.swift
+//  11-7-20 iosLambdaExperiencesSprintChallenge
+//
+//  Created by BrysonSaclausa on 11/7/20.
+//
+
+import UIKit
+import MapKit
+
+class ExperienceViewController: UIViewController {
+    
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
