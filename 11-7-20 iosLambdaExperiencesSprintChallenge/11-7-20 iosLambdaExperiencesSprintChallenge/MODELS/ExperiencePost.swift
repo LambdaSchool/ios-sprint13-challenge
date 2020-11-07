e0266acb328b7175506c87879373021330b09972
brys <brys1saclaus2@gmail.com>
2020-11-07 12:25:29 -0800

    IB Outlets and Actions hooked up

--- 11-7-20 iosLambdaExperiencesSprintChallenge/11-7-20 iosLambdaExperiencesSprintChallenge/MODELS/ExperiencePost.swift
+++ 11-7-20 iosLambdaExperiencesSprintChallenge/11-7-20 iosLambdaExperiencesSprintChallenge/MODELS/ExperiencePost.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+import MapKit
+import CoreLocation
+
+enum MediaType {
+    case image(UIImage)
+}
+
+struct Locations {
+    static let randomLocation = CLLocationCoordinate2D(latitude: 36.1569, longitude: -115.3345)
+}
+
+class Post: NSObject {
+    let title: String?
+    let mediaType: MediaType
+    let timestamp: Date
+    var id: String?
+    let location: CLLocationCoordinate2D
+    
+    init(title: String?, mediaType: MediaType, timestamp: Date = Date(), location: CLLocationCoordinate2D? = nil) {
+        self.mediaType = mediaType
+        self.title = title
+        self.timestamp = timestamp
+        self.location = location ?? Locations.randomLocation
+    }
+    
+}
--- 11-7-20 iosLambdaExperiencesSprintChallenge/11-7-20 iosLambdaExperiencesSprintChallenge/VIEW CONTROLLERS/NewXperiencePostViewController.swift
+++ 11-7-20 iosLambdaExperiencesSprintChallenge/11-7-20 iosLambdaExperiencesSprintChallenge/VIEW CONTROLLERS/NewXperiencePostViewController.swift
@@ -8,7 +8,17 @@
 import UIKit
 
 class NewXperiencePostViewController: UIViewController {
-
+    
+    //MARK: - IBOUTLETS
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var slider1: UISlider!
+    @IBOutlet weak var slider2: UISlider!
+    @IBOutlet weak var slider3: UISlider!
+    @IBOutlet weak var slider4: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -25,5 +35,21 @@ class NewXperiencePostViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    
+    //MARK:- FUNCTIONS
+    
+    private func presentImagePickerController() {
+        
+   }
+    
+    //MARK: - IBACTIONS
+    
+    @IBAction func chooseAnImageTapped(_ sender: Any) {
+        presentImagePickerController()
+    }
+    @IBAction func addARecordingTapped(_ sender: Any) {
+    }
+    @IBAction func saveButtonTapped(_ sender: Any) {
+    }
+    
 }
