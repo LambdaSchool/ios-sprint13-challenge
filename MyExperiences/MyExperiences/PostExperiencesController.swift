8841930183e19907dc9f0b45332d62a242c2581a
Gladymir <gaethano815@gmail.com>
2020-11-08 14:11:28 -0700

    Created functionality for the users to create audio, create text, and create images for their experiences.

--- MyExperiences/MyExperiences/PostExperiencesController.swift
+++ MyExperiences/MyExperiences/PostExperiencesController.swift
@@ -10,4 +10,25 @@ import MapKit
 
 class PostExperiencesController {
     
-}
+    static let shared = PostExperiencesController()
+        var posts: [PostExperiences] = []
+        var user: String? {
+            UserDefaults.standard.string(forKey: "username")
+        }
+
+    func createText(with title: String, entry: String, location: CLLocationCoordinate2D) {
+        guard let user = user else { return }
+        let post = PostExperiences(title: title, author: user, location: location, image: nil, entry: nil, audioURL: nil)
+    }
+    
+    func createAudio(with title: String, audioURL: URL, location: CLLocationCoordinate2D) {
+        guard let user = user else { return }
+        let post = PostExperiences(title: title, author: user, location: location, image: nil, entry: nil, audioURL: nil)
+    }
+    
+    func createImage(with title: String, image: UIImage, location: CLLocationCoordinate2D) {
+        guard let user = user else { return }
+        let post = PostExperiences(title: title, author: user, location: location, image: image, entry: nil, audioURL: nil)
+    }
+
+    }
