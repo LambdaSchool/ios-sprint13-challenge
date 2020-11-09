4c04ed6ba5af8e98b77a2209266c6faa0c9f64e1
Gladymir <gaethano815@gmail.com>
2020-11-08 18:17:37 -0700

    Created setup pin to view local Post.

--- MyExperiences/MyExperiences/ViewControllers/MapViewController.swift
+++ MyExperiences/MyExperiences/ViewControllers/MapViewController.swift
@@ -6,24 +6,40 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var location: CLLocationCoordinate2D?
+    var postTitle: String
+    var postAuthor: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        mapView.mapType = .standard
+        mapView.register(MKAnnotationView.self, forAnnotationViewWithReuseIdentifier: .annotationReuseIdentifier)
+        setPinUsingMKPointAnnotation(location: location!)
+
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setPinUsingMKPointAnnotation(location: CLLocationCoordinate2D) {
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = location
+        annotation.title = postTitle
+        annotation.subtitle = postAuthor
+        let coordinateRegion = MKCoordinateRegion(center: annotation.coordinate, latitudinalMeters: 200, longitudinalMeters: 200)
+        mapView.setRegion(coordinateRegion, animated: true)
+        mapView.addAnnotation(annotation)
     }
-    */
 
+    
+
+}
+
+extension String {
+    static let annotationReuseIdentifier = "PostLocationView"
 }
