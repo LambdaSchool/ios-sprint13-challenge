b9d462ca37c962db5e8f5bdb5ccb68be036fd2fc
Gladymir <gaethano815@gmail.com>
2020-11-08 23:12:04 -0700

    a few bugs to fix

--- MyExperiences/MyExperiences/ViewControllers/LoginViewController.swift
+++ MyExperiences/MyExperiences/ViewControllers/LoginViewController.swift
@@ -6,24 +6,61 @@
 //
 
 import UIKit
+import AVFoundation
+import AVKit
 
 class LoginViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet var backgroundVideoView: UIView!
+    @IBOutlet var loginView: UIView!
+    @IBOutlet weak var usernameTextField: UITextField!
+    
+    let player = AVPlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "Login", ofType: "mov")!))
+    var newPlayer = AVPlayerLayer(player: nil)
+    let postExperiencesController = PostExperiencesController.shared
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        segueIfUsernameExists()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setupView()
+        view.addSubview(loginView)
     }
-    */
+    
+    override func viewDidLayoutSubviews() {
+        newPlayer.frame = self.backgroundVideoView.bounds
+    }
+    
+    private func setupView() {
+        newPlayer = AVPlayerLayer(player: player)
+        newPlayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        
+        self.backgroundVideoView.layer.addSublayer(newPlayer)
+        
+        player.play()
+        player.actionAtItemEnd = AVPlayer.ActionAtItemEnd.none
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(self.videoDidPlayToEnd(_: )), name: NSNotification.Name(rawValue: "AVPlayerItemDidPlayToEndTimeNotification"), object: player.currentItem)
+    }
+    
+    @objc private func videoDidPlayToEnd(_ notification: Notification) {
+            let player: AVPlayerItem = notification.object as! AVPlayerItem
+            player.seek(to: CMTime.zero, completionHandler: nil)
+        }
+    
+    private func segueIfUsernameExists() {
+        if UserDefaults.standard.string(forKey: "username") != nil {
+            performSegue(withIdentifier: "ExperienceCollectionSegue", sender: nil)
+        }
+    }
+    
+    @IBAction func loginTapped(_ sender: UIButton) {
+        guard let username = usernameTextField.text, !username.isEmpty else { return }
+        UserDefaults.standard.set(username, forKey: "username")
+        segueIfUsernameExists()
+    }
 
 }
