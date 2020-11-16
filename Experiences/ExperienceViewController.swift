d580144878cd0ef7abe1fe3f105adb5a5b3d93fd
Kenneth Jones <kennethjones1991@outlook.com>
2020-11-16 04:00:54 -0700

    Added image picker and filter functionality

--- Experiences/ExperienceViewController.swift
+++ Experiences/ExperienceViewController.swift
@@ -6,6 +6,9 @@
 //
 
 import UIKit
+import CoreImage
+import CoreImage.CIFilterBuiltins
+import Photos
 
 class ExperienceViewController: UIViewController {
     
@@ -17,8 +20,73 @@ class ExperienceViewController: UIViewController {
     
     var expController: ExperienceController!
     
+    private let context = CIContext()
+    private let vintageFilter = CIFilter.photoEffectInstant()
+    
+    var originalImage: UIImage? {
+        didSet {
+            guard let originalImage = originalImage else {
+                scaledImage = nil
+                return
+            }
+            
+            var scaledSize = imageView.bounds.size
+            let scale = imageView.contentScaleFactor
+            
+            scaledSize.width *= scale
+            scaledSize.height *= scale
+            
+            guard let scaledUIImage = originalImage.imageByScaling(toSize: scaledSize) else {
+                scaledImage = nil
+                return
+            }
+            
+            scaledImage = CIImage(image: scaledUIImage)
+        }
+    }
+    
+    var scaledImage: CIImage? {
+        didSet {
+            updateImage()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    private func presentImagePickerController() {
+        
+        guard UIImagePickerController.isSourceTypeAvailable(.photoLibrary) else {
+            presentInformationalAlertController(title: "Error", message: "The photo library is unavailable")
+            return
+        }
+        
+        DispatchQueue.main.async {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = .photoLibrary
+            
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    private func image(byFiltering inputImage: CIImage) -> UIImage? {
+        vintageFilter.inputImage = inputImage
+        
+        guard let outputImage = vintageFilter.outputImage else { return nil }
+        
+        guard let renderedCGImage = context.createCGImage(outputImage, from: inputImage.extent) else { return nil }
+        
+        return UIImage(cgImage: renderedCGImage)
+    }
+    
+    private func updateImage() {
+        if let scaledImage = scaledImage {
+            imageView.image = image(byFiltering: scaledImage)
+        } else {
+            imageView.image = nil
+        }
     }
     
     @IBAction func saveExperience(_ sender: Any) {
@@ -26,10 +94,56 @@ class ExperienceViewController: UIViewController {
     }
     
     @IBAction func addImage(_ sender: Any) {
+        let authorizationStatus = PHPhotoLibrary.authorizationStatus()
         
+        switch authorizationStatus {
+        case .authorized:
+            presentImagePickerController()
+        case .notDetermined:
+            
+            PHPhotoLibrary.requestAuthorization { (status) in
+                
+                guard status == .authorized else {
+                    NSLog("User did not authorize access to the photo library")
+                    self.presentInformationalAlertController(title: "Error", message: "In order to access the photo library, you must allow this application access to it.")
+                    return
+                }
+                
+                self.presentImagePickerController()
+            }
+            
+        case .denied:
+            self.presentInformationalAlertController(title: "Error", message: "In order to access the photo library, you must allow this application access to it.")
+        case .restricted:
+            self.presentInformationalAlertController(title: "Error", message: "Unable to access the photo library. Your device's restrictions do not allow access.")
+        default:
+            break
+        }
+        presentImagePickerController()
     }
     
     @IBAction func recordAudio(_ sender: Any) {
         
+    }
+}
+
+
+extension ExperienceViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+
+        addImageButton.setTitle("", for: [])
+        
+        picker.dismiss(animated: true, completion: nil)
+        
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else { return }
+        
+        imageView.image = image
+        
+        originalImage = imageView.image
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
     }
 }
